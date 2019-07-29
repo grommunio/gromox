eba982b40c91dec0e330c5d766b384b792930c53
steep-system <yangsongyi@ee-post.com>
2019-07-29 16:59:58 +0800

    modification of anti-spam

--- MTA_SYSTEM/as_plugins/property_017/main.c
+++ MTA_SYSTEM/as_plugins/property_017/main.c
@@ -2,42 +2,31 @@
 #include "util.h"
 #include "mem_file.h"
 #include "config_file.h"
-#include "mail_func.h"
-#include <ctype.h>
-#include <string.h>
 #include <stdio.h>
 
 #define SPAM_STATISTIC_PROPERTY_017          46
 
 typedef void (*SPAM_STATISTIC)(int);
-typedef BOOL (*CHECK_TAGGING)(const char*, MEM_FILE*);
-
 
 static SPAM_STATISTIC spam_statistic;
-static CHECK_TAGGING check_tagging;
 
 DECLARE_API;
 
 static char g_return_reason[1024];
 
-static int plain_filter(int action, int context_ID, MAIL_BLOCK* mail_blk, 
-	char* reason, int length);
+static int head_filter(int context_ID, MAIL_ENTITY *pmail,
+	CONNECTION *pconnection, char *reason, int length);
 
 int AS_LibMain(int reason, void **ppdata)
 {	
 	CONFIG_FILE *pconfig_file;
 	char file_name[256], temp_path[256];
 	char *str_value, *psearch;
-	
+
 	switch (reason) {
 	case PLUGIN_INIT:
 		LINK_API(ppdata);
 		spam_statistic = (SPAM_STATISTIC)query_service("spam_statistic");
-		check_tagging = (CHECK_TAGGING)query_service("check_tagging");
-		if (NULL == check_tagging) {
-			printf("[property_017]: fail to get \"check_tagging\" service\n");
-			return FALSE;
-		}
 		strcpy(file_name, get_plugin_name());
 		psearch = strrchr(file_name, '.');
 		if (NULL != psearch) {
@@ -57,8 +46,7 @@ int AS_LibMain(int reason, void **ppdata)
 		}
 		printf("[property_017]: return string is %s\n", g_return_reason);
 		config_file_free(pconfig_file);
-		/* invoke register_filter for registering all type of mime paragraph*/
-		if (FALSE == register_filter("text/plain" , plain_filter)) {
+		if (FALSE == register_auditor(head_filter)) {
 			return FALSE;
 		}
 		return TRUE;
@@ -68,112 +56,55 @@ int AS_LibMain(int reason, void **ppdata)
 	return TRUE;
 }
 
-static int plain_filter(int action, int context_ID, MAIL_BLOCK* mail_blk, 
-	char* reason, int length)
+static int head_filter(int context_ID, MAIL_ENTITY *pmail,
+	CONNECTION *pconnection, char *reason, int length)
 {
-	int score;
 	int out_len;
+	int tag_len;
+	int val_len;
 	char buff[1024];
-	MAIL_ENTITY mail_entity;
 
-	switch (action) {
-	case ACTION_BLOCK_NEW:
+	if (TRUE == pmail->penvelop->is_outbound ||
+			TRUE == pmail->penvelop->is_relay) {
+			return MESSAGE_ACCEPT;
+	}
+	if (SINGLE_PART_MAIL != pmail->phead->mail_part) {
 		return MESSAGE_ACCEPT;
-	case ACTION_BLOCK_PROCESSING:
-		mail_entity	= get_mail_entity(context_ID);
-		if (TRUE == mail_entity.penvelop->is_outbound ||
-			TRUE == mail_entity.penvelop->is_relay) {
-			return MESSAGE_ACCEPT;
-		}
-
-		if (SINGLE_PART_MAIL != mail_entity.phead->mail_part) {
-			return MESSAGE_ACCEPT;
-		}
-
-		out_len = mem_file_read(&mail_entity.phead->f_xmailer, buff, 1024);
-		if (MEM_END_OF_FILE == out_len) {
-			return MESSAGE_ACCEPT;
-		}
-
-		if (0 != strncasecmp(buff, "Microsoft Outlook Express 6", 27)) {
-			return MESSAGE_ACCEPT;
-		}
-		
-		if (TRUE == mail_blk->is_parsed || mail_blk->original_length > 400) {
-			return MESSAGE_ACCEPT;
-		}
-		
-		score = 0;
-
-		if (NULL != search_string(mail_blk->original_buff, "target:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "price:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "Target Price:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "sym:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "symbol:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "company:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "date:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "trade:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "traded:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (NULL != search_string(mail_blk->original_buff, "ticker:",
-			mail_blk->original_length)) {
-			score ++;
-		}
-
-		if (score < 3) {
-			return MESSAGE_ACCEPT;
-		}
-
-		if (TRUE == check_tagging(mail_entity.penvelop->from,
-			&mail_entity.penvelop->f_rcpt_to)) {
-			mark_context_spam(context_ID);
-			return MESSAGE_ACCEPT;
-		} else {
-			if (NULL != spam_statistic) {
-				spam_statistic(SPAM_STATISTIC_PROPERTY_017);
+	}
+	out_len = mem_file_read(&pmail->phead->f_xmailer, buff, 1024);
+	if (MEM_END_OF_FILE == out_len) {
+		return MESSAGE_ACCEPT;
+	}
+	if (0 != strncasecmp(buff, "Foxmail 6", 9) &&
+		0 != strncasecmp(buff, "Foxmail 7", 9)) {
+		return MESSAGE_ACCEPT;
+	}
+	while (MEM_END_OF_FILE != mem_file_read(
+		&pmail->phead->f_others, &tag_len, sizeof(int))) {
+		if (25 == tag_len) {
+			mem_file_read(&pmail->phead->f_others, buff, tag_len);
+			if (0 == strncasecmp(buff, "Content-Transfer-Encoding", 25)) {
+				mem_file_read(&pmail->phead->f_others, &val_len, sizeof(int));
+				if (4 != val_len) {
+					return MESSAGE_ACCEPT;
+				}
+				mem_file_read(&pmail->phead->f_others, buff, 4);
+				if (0 != strncasecmp(buff, "8bit", 4)) {
+					return MESSAGE_ACCEPT;
+				}
+				if (NULL != spam_statistic) {
+					spam_statistic(SPAM_STATISTIC_PROPERTY_017);
+				}
+				strncpy(reason, g_return_reason, length);
+				return MESSAGE_REJECT;
 			}
-			strncpy(reason, g_return_reason, length);
-			return MESSAGE_REJECT;
+		} else {
+			mem_file_seek(&pmail->phead->f_others,
+				MEM_FILE_READ_PTR, tag_len, MEM_FILE_SEEK_CUR);
 		}
-		return MESSAGE_ACCEPT;
-	case ACTION_BLOCK_FREE:
-		return MESSAGE_ACCEPT;
+		mem_file_read(&pmail->phead->f_others, &val_len, sizeof(int));
+		mem_file_seek(&pmail->phead->f_others,
+			MEM_FILE_READ_PTR, val_len, MEM_FILE_SEEK_CUR);
 	}
 	return MESSAGE_ACCEPT;
 }
-
