f7287abaa3f5df86035604344fbbf9936c28ba1d
steep-system <yangsongyi@ee-post.com>
2019-07-30 13:10:15 +0800

    modification of anti-spam

--- MTA_SYSTEM/as_plugins/property_018/main.c
+++ MTA_SYSTEM/as_plugins/property_018/main.c
@@ -1,38 +1,32 @@
 #include "as_common.h"
+#include "mem_file.h"
 #include "config_file.h"
 #include "util.h"
-#include <ctype.h>
 #include <stdio.h>
 
-#define SPAM_STATISTIC_PROPERTY_018		49
+#define SPAM_STATISTIC_PROPERTY_018          49
 
 typedef void (*SPAM_STATISTIC)(int);
-typedef BOOL (*CHECK_TAGGING)(const char*, MEM_FILE*);
 
-static int head_filter(int context_ID, MAIL_ENTITY *pmail,
-	CONNECTION *pconnection, char *reason, int length);
+static SPAM_STATISTIC spam_statistic;
 
 DECLARE_API;
 
-static SPAM_STATISTIC spam_statistic;
-static CHECK_TAGGING check_tagging;
+static char g_return_reason[1024];
 
-static char g_return_string[1024];
+
+static int xmailer_filter(int action, int context_ID,
+	MAIL_BLOCK* mail_blk,  char* reason, int length);
 
 int AS_LibMain(int reason, void **ppdata)
-{
+{	
 	CONFIG_FILE *pconfig_file;
 	char file_name[256], temp_path[256];
 	char *str_value, *psearch;
-
-    switch (reason) {
-    case PLUGIN_INIT:
+	
+	switch (reason) {
+	case PLUGIN_INIT:
 		LINK_API(ppdata);
-		check_tagging = (CHECK_TAGGING)query_service("check_tagging");
-		if (NULL == check_tagging) {
-			printf("[property_018]: fail to get \"check_tagging\" service\n");
-			return FALSE;
-		}
 		spam_statistic = (SPAM_STATISTIC)query_service("spam_statistic");
 		strcpy(file_name, get_plugin_name());
 		psearch = strrchr(file_name, '.');
@@ -47,101 +41,104 @@ int AS_LibMain(int reason, void **ppdata)
 		}
 		str_value = config_file_get_value(pconfig_file, "RETURN_STRING");
 		if (NULL == str_value) {
-			strcpy(g_return_string, "000049 you are now sending spam mail!");
+			strcpy(g_return_reason, "000049 you are now sending spam mail!");
 		} else {
-			strcpy(g_return_string, str_value);
+			strcpy(g_return_reason, str_value);
 		}
-		printf("[property_018]: return string is %s\n", g_return_string);
+		printf("[property_018]: return string is %s\n", g_return_reason);
 		config_file_free(pconfig_file);
-
-        /* invoke register_auditor for registering auditor of mime head */
-        if (FALSE == register_auditor(head_filter)) {
-			printf("[property_018]: fail to register auditor function\n");
-            return FALSE;
-        }
-        return TRUE;
-    case PLUGIN_FREE:
-        return TRUE;
-    }
-    return TRUE;
+		/* invoke register_filter for registering all type of mime paragraph*/
+		if (FALSE == register_filter("text/plain" , xmailer_filter) ||
+		if (FALSE == register_filter("text/html" , xmailer_filter)) {
+			return FALSE;
+		}
+		return TRUE;
+	case PLUGIN_FREE:
+		return TRUE;
+	}
+	return TRUE;
 }
 
-
-static int head_filter(int context_ID, MAIL_ENTITY *pmail,
-	CONNECTION *pconnection, char *reason, int length)
+static int xmailer_filter(int action, int context_ID,
+	MAIL_BLOCK* mail_blk,  char* reason, int length)
 {
-	int out_len;
 	int tag_len;
 	int val_len;
 	char buff[1024];
+	MEM_FILE *fp_mime_field;
+	MAIL_ENTITY mail_entity;
 
-	
-	if (TRUE == pmail->penvelop->is_relay ||
-		TRUE == pmail->penvelop->is_outbound) {
+	switch (action) {
+	case ACTION_BLOCK_NEW:
 		return MESSAGE_ACCEPT;
-	}
-
-	if (SINGLE_PART_MAIL != pmail->phead->mail_part) {
-		return MESSAGE_ACCEPT;
-	}
-
-	out_len = mem_file_read(&pmail->phead->f_xmailer, buff, 1024);
-	if (MEM_END_OF_FILE == out_len) {
-		return MESSAGE_ACCEPT;
-	}
-
-	if (0 != strncasecmp(buff, "Apple Mail", 10)) {
-		return MESSAGE_ACCEPT;
-	}
-
-	out_len = mem_file_read(&pmail->phead->f_content_type, buff, 1024);
-	if (MEM_END_OF_FILE == out_len) {
-		return MESSAGE_ACCEPT;
-	}
-
-	if (0 != strncmp(buff, "text/html; charset=\"us-ascii\"", 29)) {
-		return MESSAGE_ACCEPT;
-	}
-
-	while (MEM_END_OF_FILE != mem_file_read(&pmail->phead->f_others,
-		&tag_len, sizeof(int))) {
-		if (10 == tag_len) {
-			mem_file_read(&pmail->phead->f_others, buff, tag_len);
-			if (0 == strncasecmp("Precedence", buff, 10)) {
-				mem_file_read(&pmail->phead->f_others, &val_len, sizeof(int));
-				if (4 != val_len) {
+	case ACTION_BLOCK_PROCESSING:
+		mail_entity	= get_mail_entity(context_ID);
+		if (MULTI_PARTS_MAIL == mail_entity.phead->mail_part) {
+			return MESSAGE_ACCEPT;
+		}
+		if (TRUE == mail_entity.penvelop->is_outbound ||
+			TRUE == mail_entity.penvelop->is_relay) {
+			return MESSAGE_ACCEPT;
+		}
+		if (mail_blk->original_length > 4096) {
+			return MESSAGE_ACCEPT;
+		}
+		fp_mime_field = &mail_entity.phead->f_others;
+		while (MEM_END_OF_FILE != mem_file_read(
+			fp_mime_field, &tag_len, sizeof(int))) {
+			if (8 == tag_len) {
+				mem_file_read(fp_mime_field, buff, tag_len);
+				if (0 == strncasecmp("Received", buff, 8)) {
 					return MESSAGE_ACCEPT;
 				}
-				mem_file_read(&pmail->phead->f_others, buff, val_len);
-				buff[val_len] = '\0';
-
-				if (0 != strcasecmp(buff, "list") && 0 != strcasecmp(buff, "junk")) {
-					return MESSAGE_ACCEPT;
-				}
-
-				if (TRUE == check_tagging(pmail->penvelop->from,
-					&pmail->penvelop->f_rcpt_to)) {
-					mark_context_spam(context_ID);
-					return MESSAGE_ACCEPT;
-				} else {
+			} else {
+				mem_file_seek(fp_mime_field, MEM_FILE_READ_PTR,
+									tag_len, MEM_FILE_SEEK_CUR);
+			}
+			mem_file_read(fp_mime_field, &val_len, sizeof(int));
+			mem_file_seek(fp_mime_field, MEM_FILE_READ_PTR,
+								val_len, MEM_FILE_SEEK_CUR);
+		}
+		fp_mime_field = mail_blk->fp_mime_info;
+		while (MEM_END_OF_FILE != mem_file_read(
+			fp_mime_field, &tag_len, sizeof(int))) {
+			if (25 == tag_len) {
+				mem_file_read(fp_mime_field, buff, tag_len);
+				if (0 == strncasecmp(buff, "Content-Transfer-Encoding", 25)) {
+					mem_file_read(fp_mime_field, &val_len, sizeof(int));
+					if (4 != val_len) {
+						return MESSAGE_ACCEPT;
+					}
+					mem_file_read(fp_mime_field, buff, 4);
+					if (0 != strncasecmp(buff, "8bit", 4)) {
+						return MESSAGE_ACCEPT;
+					}
+					
+					if (NULL == search_string((char*)mail_blk->original_buff,
+							"Bitcoin", mail_blk->original_length) &&
+						NULL == search_string((char*)mail_blk->original_buff,
+							"BTC ", mail_blk->original_length) &&
+						NULL == search_string((char*)mail_blk->original_buff,
+							" BTC", mail_blk->original_length)) {
+						return MESSAGE_ACCEPT;
+					}
 					if (NULL != spam_statistic) {
 						spam_statistic(SPAM_STATISTIC_PROPERTY_018);
 					}
-					strncpy(reason, g_return_string, length);
+					strncpy(reason, g_return_reason, length);
 					return MESSAGE_REJECT;
 				}
-
+			} else {
+				mem_file_seek(fp_mime_field, MEM_FILE_READ_PTR,
+									tag_len, MEM_FILE_SEEK_CUR);
 			}
-		} else {
-			mem_file_seek(&pmail->phead->f_others, MEM_FILE_READ_PTR,
-				tag_len, MEM_FILE_SEEK_CUR);
+			mem_file_read(fp_mime_field, &val_len, sizeof(int));
+			mem_file_seek(fp_mime_field, MEM_FILE_READ_PTR,
+								val_len, MEM_FILE_SEEK_CUR);
 		}
-		mem_file_read(&pmail->phead->f_others, &val_len, sizeof(int));
-		mem_file_seek(&pmail->phead->f_others, MEM_FILE_READ_PTR, val_len,
-			MEM_FILE_SEEK_CUR);
+		return MESSAGE_ACCEPT;
+	case ACTION_BLOCK_FREE:
+		return MESSAGE_ACCEPT;
 	}
-	
 	return MESSAGE_ACCEPT;
-	
 }
-
