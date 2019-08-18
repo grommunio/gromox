07235c6383580009a1e6e3aedc6db5edf55ccf22
steep-system <yangsongyi@ee-post.com>
2019-08-18 16:39:01 +0800

    midification of anti-spam

--- MTA_SYSTEM/as_plugins/property_001/main.c
+++ MTA_SYSTEM/as_plugins/property_001/main.c
@@ -103,6 +103,7 @@ static int head_filter(int context_ID, MAIL_ENTITY *pmail,
 	}
 		
 	if (0 != strncasecmp(buff, "Microsoft Outlook Express", 25) &&
+		0 != strncasecmp(buff, "FoxMail 3", 9) &&
 		0 != strncasecmp(buff, "FoxMail 4", 9) &&
 		0 != strncasecmp(buff, "Foxmail 5", 9) &&
 		0 != strncasecmp(buff, "mailer", 6)) {
