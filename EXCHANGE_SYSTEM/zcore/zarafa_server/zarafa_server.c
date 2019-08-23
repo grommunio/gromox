01b7f4db5690346e84e5b9487f1ffff83bfdc272
steep-system <yangsongyi@ee-post.com>
2019-08-23 08:15:14 +0800

    modification of zcore

--- EXCHANGE_SYSTEM/zcore/zarafa_server/zarafa_server.c
+++ EXCHANGE_SYSTEM/zcore/zarafa_server/zarafa_server.c
@@ -4282,13 +4282,13 @@ uint32_t zarafa_server_modifyrecipients(GUID hsession,
 		MODRECIP_MODIFY != flags && MODRECIP_REMOVE != flags)) {
 		return EC_INVALID_PARAMETER;
 	}
-	for (i=0; i<prcpt_list->count; i++) {
-		common_util_replace_address_type((TPROPVAL_ARRAY*)
-							prcpt_list->pparray[i], FALSE);
-	}
 	pinfo = zarafa_server_query_session(hsession);
 	if (NULL == pinfo) {
 		return EC_ERROR;
+	}
+	for (i=0; i<prcpt_list->count; i++) {
+		common_util_replace_address_type((TPROPVAL_ARRAY*)
+							prcpt_list->pparray[i], FALSE);
 	}
 	pmessage = object_tree_get_object(
 		pinfo->ptree, hmessage, &mapi_type);
