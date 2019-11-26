c6bba1e0b8e6a97af23736b288e87dcebef745ae
C. M. Palmerston <cmp@gromox.com>
2019-11-26 04:37:29 +0100

    domain_archive: remove unused session_client functions

--- DOMAIN_ADMIN/domain_archive/session_client.h
+++ DOMAIN_ADMIN/domain_archive/session_client.h
@@ -5,11 +5,7 @@
 void session_client_init(const char *ip, int port);
 extern int session_client_run(void);
 extern int session_client_stop(void);
-BOOL session_client_produce(const char *domainname, char *session);
-
 BOOL session_client_check(const char *domainname, const char *session);
-
-BOOL session_client_remove(const char *domainname, const char *session);
 extern void session_client_free(void);
 
 #endif
