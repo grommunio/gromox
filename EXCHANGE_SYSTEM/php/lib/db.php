79a8a57e14ed59e04c0f425c6b717f871d6d0a60
Victor Tesla <victor.tesla@grammm.com>
2019-11-26 04:37:26 +0100

    ews-php: replace mysql_* for php 7 compatibility

--- EXCHANGE_SYSTEM/php/lib/db.php
+++ EXCHANGE_SYSTEM/php/lib/db.php
@@ -3,10 +3,10 @@
 
 function get_db_connection()
 {
-	static $dbconn = NULL;
+	static $db_conn = NULL;
 	
-	if ($dbconn) {
-		return $dbconn;
+	if ($db_conn) {
+		return $db_conn;
 	}
 	require_once "conf.php";
 	$config = get_app_config();
@@ -26,29 +26,26 @@ function get_db_connection()
 	if (!isset($db_config['dbname'])) {
 		die("cannot find dbname under [database] in config file");
 	}
-	$dbconn = mysql_connect($db_config['host'], $db_config['username'], $db_config['password']);
-	if (!$dbconn) {
-		die("fail to connect to database server: " . mysql_error());
+	$db_conn = mysqli_connect($db_config['host'], $db_config['username'], $db_config['password'], $db_config['dbname']);
+	if ($db_conn->connect_errno) {
+		die("fail to connect to database server: " . $db_conn->connect_error);
 	}
-	if (mysql_select_db($db_config['dbname'], $dbconn) == false) {
-		die("fail to select database");
-	}
-	return $dbconn;
+	return $db_conn;
 }
 
 function get_user_info_by_name($email_address)
 {
 	$db_conn = get_db_connection();
 	
-	$sql_string = "SELECT maildir, real_name, username, id, domain_id, timezone FROM users WHERE username='" . mysql_real_escape_string($email_address) . "'";
-	$results = mysql_query($sql_string, $db_conn);
+	$sql_string = "SELECT maildir, real_name, username, id, domain_id, timezone FROM users WHERE username='" . $db_conn->real_escape_string($email_address) . "'";
+	$results = $db_conn->query($sql_string);
 	if (!$results) {
-		die("fail to query database: " . mysql_error());
+		die("fail to query database: " . $db_conn->error);
 	}
-	if (1 != mysql_num_rows($results)) {
+	if (1 != mysqli_num_rows($results)) {
 		return NULL;
 	}
-	$row = mysql_fetch_row($results);
+	$row = $results->fetch_row();
 	$at_pos = strpos($row[2], '@');
 	$domain = substr($row[2], $at_pos + 1);
 	if (!$row[5]) {
@@ -68,14 +65,14 @@ function get_user_info_by_id($user_id)
 	$db_conn = get_db_connection();
 	
 	$sql_string = "SELECT maildir, real_name, username, id, domain_id, timezone FROM users WHERE id=" . $user_id;
-	$results = mysql_query($sql_string, $db_conn);
+	$results = $db_conn->query($sql_string);
 	if (!$results) {
-		die("fail to query database: " . mysql_error());
+		die("fail to query database: " . $db_conn->error);
 	}
-	if (1 != mysql_num_rows($results)) {
+	if (1 != mysqli_num_rows($results)) {
 		return NULL;
 	}
-	$row = mysql_fetch_row($results);
+	$row = $results->fetch_row();
 	$at_pos = strpos($row[2], '@');
 	$domain = substr($row[2], $at_pos + 1);
 	if (!$row[5]) {
@@ -94,15 +91,15 @@ function get_domain_info_by_name($domain)
 {
 	$db_conn = get_db_connection();
 	
-	$sql_string = "SELECT homedir, id, domainname FROM domains WHERE domainname='" . mysql_real_escape_string($domain) . "'";
-	$results = mysql_query($sql_string, $db_conn);
+	$sql_string = "SELECT homedir, id, domainname FROM domains WHERE domainname='" . $db_conn->real_escape_string($domain) . "'";
+	$results = $db_conn->query($sql_string);
 	if (!$results) {
-		die("fail to query database: " . mysql_error());
+		die("fail to query database: " . $db_conn->error);
 	}
-	if (1 != mysql_num_rows($results)) {
+	if (1 != mysqli_num_rows($results)) {
 		return NULL;
 	}
-	$row = mysql_fetch_row($results);
+	$row = $results->fetch_row();
 	return array('homedir'=>$row[0], 'did'=>$row[1], 'domain'=>$row[2]);
 }
 
@@ -111,14 +108,14 @@ function get_domain_info_by_id($domain_id)
 	$db_conn = get_db_connection();
 	
 	$sql_string = "SELECT homedir, id, domainname FROM domains WHERE id=" . $domain_id;
-	$results = mysql_query($sql_string, $db_conn);
+	$results = $db_conn->query($sql_string);
 	if (!$results) {
-		die("fail to query database: " . mysql_error());
+		die("fail to query database: " . $db_conn->error);
 	}
-	if (1 != mysql_num_rows($results)) {
+	if (1 != mysqli_num_rows($results)) {
 		return NULL;
 	}
-	$row = mysql_fetch_row($results);
+	$row = $results->fetch_row();
 	return array('homedir'=>$row[0], 'did'=>$row[1], 'domain'=>$row[2]);
 }
 
