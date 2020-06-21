c1dad758192d86f4fb961997d16490261cdd147a
C. M. Palmerston <cmp@gromox.com>
2020-06-21 13:11:37 +0100

    lib: add scope_exit

--- include/gromox/scope.hpp
+++ include/gromox/scope.hpp
@@ -28,9 +28,32 @@ template<typename F> class scope_success {
 	void operator=(scope_success &&) = delete;
 };
 
+template<typename F> class scope_exit {
+	private:
+	F m_func;
+	bool m_eod = false;
+
+	public:
+	explicit scope_exit(F &&f) : m_func(std::move(f)), m_eod(true) {}
+	scope_exit(scope_exit &&o) : m_func(std::move(o.m_func)), m_eod(o.m_eod) {
+		o.m_eod = false;
+	}
+	~scope_exit() try {
+		if (m_eod)
+			m_func();
+	} catch (...) {
+	}
+	void operator=(scope_exit &&) = delete;
+};
+
 template<typename F> scope_success<F> make_scope_success(F &&f)
 {
 	return scope_success<F>(std::move(f));
+}
+
+template<typename F> scope_exit<F> make_scope_exit(F &&f)
+{
+	return scope_exit<F>(std::move(f));
 }
 
 } /* namespace */
