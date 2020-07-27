c7723b9401d67d7c49b73ffbb06b64fe3937ed59
C. M. Palmerston <cmp@gromox.com>
2020-07-27 19:23:11 +0100

    domain_admin: remove synchronizer daemon

--- DOMAIN_ADMIN/synchronizer/processing_engine.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void processing_engine_init(const char *master_host, const char *data_path,
-	const char *config_path);
-extern int processing_engine_run(void);
-extern int processing_engine_stop(void);
-extern void processing_engine_free(void);
--- DOMAIN_ADMIN/synchronizer/url_downloader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "common_types.h"
-
-extern void url_downloader_init(void);
-extern int url_downloader_run(void);
-BOOL url_downloader_get(const char *url, const char *save_path);
-extern int url_downloader_stop(void);
-extern void url_downloader_free(void);
