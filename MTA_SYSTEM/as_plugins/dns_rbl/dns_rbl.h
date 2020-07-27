9bb7e6e2eb6343a695b746ca70027d18cd224e08
C. M. Palmerston <cmp@gromox.com>
2020-07-27 02:44:58 +0100

    mta_system: remove antispam plugins
    Antispam handling is performed via an external, more powerful,
    mechanism (postfix + spamassassin/rspamd).
    
    References: GX-22

--- MTA_SYSTEM/as_plugins/dns_rbl/dns_rbl.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "common_types.h"
-
-void dns_rbl_init(const char *path);
-extern void dns_rbl_free(void);
-extern int dns_rbl_run(void);
-extern int dns_rbl_stop(void);
-BOOL dns_rbl_judge(const char *ip, char *answer_buff, int answer_len);
-extern BOOL dns_rbl_refresh(void);
