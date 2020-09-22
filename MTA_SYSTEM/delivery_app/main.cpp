d7f8f7e90adc877d0606575acb56f927d2c270c2
C. M. Palmerston <cmp@gromox.com>
2020-09-22 19:46:19 +0100

    delivery: delete domain_monitor plugin

--- MTA_SYSTEM/delivery_app/main.cpp
+++ MTA_SYSTEM/delivery_app/main.cpp
@@ -36,7 +36,6 @@ static struct HXoption g_options_table[] = {
 static const char *const g_dfl_mpc_plugins[] = {
 	"libmtahook_alias_translator.so",
 	"libmtahook_archive_agent.so",
-	"libmtahook_domain_monitor.so",
 	"libmtahook_domain_sign.so",
 	"libmtahook_exmdb_local.so",
 	"libmtahook_from_replace.so",
