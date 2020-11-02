9fb02493ad44c20fbb8a79482eb28b7bd28857c0
C. M. Palmerston <cmp@gromox.com>
2020-11-02 09:12:37 +0100

    mapi_lib: add "extern C" guards

--- LIBRARY/mapi_lib/include/oxcical.h
+++ LIBRARY/mapi_lib/include/oxcical.h
@@ -3,6 +3,9 @@
 #include "ext_buffer.h"
 #include "ical.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 MESSAGE_CONTENT* oxcical_import(
 	const char *str_zone, const ICAL *pical,
@@ -14,3 +17,7 @@ BOOL oxcical_export(const MESSAGE_CONTENT *pmsg, ICAL *pical,
 	ENTRYID_TO_USERNAME entryid_to_username,
 	ESSDN_TO_USERNAME essdn_to_username,
 	LCID_TO_LTAG lcid_to_ltag);
+
+#ifdef __cplusplus
+}
+#endif
