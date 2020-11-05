9549ac373dc2e21ace5912b204691f92d604aee8
C. M. Palmerston <cmp@gromox.com>
2020-11-05 06:19:37 +0100

    system_admin: delete bl_delete program

--- SYSTEM_ADMIN/bl_delete/midb_client.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "common_types.h"
-
-void midb_client_init(const char *list_path);
-extern int midb_client_run(void);
-extern int midb_client_stop(void);
-extern void midb_client_free(void);
-BOOL midb_client_delete(const char *maildir, const char *folder,
-	const char *file);
