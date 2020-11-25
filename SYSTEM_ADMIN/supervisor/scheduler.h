7429936b7bd81847d131104c82c4863caa9c96b9
C. M. Palmerston <cmp@gromox.com>
2020-11-25 05:08:47 +0100

    system_admin: delete supervising daemon

--- SYSTEM_ADMIN/supervisor/scheduler.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void scheduler_init(const char *list_path, const char *failure_path,
-	const char *default_domain, const char *admin_mailbox, int max_interval);
-extern int scheduler_run(void);
-extern void scheduler_stop(void);
-extern void scheduler_free(void);
