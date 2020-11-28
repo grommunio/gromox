de481c0846db0a787945b442b11361e87069a0bb
C. M. Palmerston <cmp@gromox.com>
2020-11-28 21:39:21 +0100

    archive_system: delete cidb daemon

--- EXCHANGE_SYSTEM/midb/cmd_parser.h
+++ EXCHANGE_SYSTEM/midb/cmd_parser.h
@@ -1,1 +1,30 @@
-../../ARCHIVE_SYSTEM/cidb/cmd_parser.h
+#pragma once
+#include "double_list.h"
+#include <pthread.h>
+
+
+typedef struct _CONNECTION {
+	DOUBLE_LIST_NODE node;
+	int sockd;
+	BOOL is_selecting;
+	pthread_t thr_id;
+} CONNECTION;
+
+typedef int (*COMMAND_HANDLER)(int argc, char** argv, int sockd);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cmd_parser_init(int threads_num, int timeout);
+extern int cmd_parser_run(void);
+extern int cmd_parser_stop(void);
+extern void cmd_parser_free(void);
+extern CONNECTION *cmd_parser_get_connection(void);
+void cmd_parser_put_connection(CONNECTION *pconnection);
+
+void cmd_parser_register_command(const char *command, COMMAND_HANDLER handler);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
--- EXCHANGE_SYSTEM/midb/listener.h
+++ EXCHANGE_SYSTEM/midb/listener.h
@@ -1,1 +1,15 @@
-../../ARCHIVE_SYSTEM/cidb/listener.h
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void listener_init(const char *ip, int port, const char *list_path);
+extern int listener_run(void);
+extern int listener_trigger_accept(void);
+extern int listener_stop(void);
+extern void listener_free(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
