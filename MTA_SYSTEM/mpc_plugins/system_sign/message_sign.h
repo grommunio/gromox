1102d4111ccda8ea8a3b30cb5d5f67cda8bbc576
C. M. Palmerston <cmp@gromox.com>
2020-12-09 18:23:31 +0100

    mta_system: delete system_sign plugin

--- MTA_SYSTEM/mpc_plugins/system_sign/message_sign.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "mail.h"
-
-void message_sign_init(const char *path);
-extern int message_sign_run(void);
-void message_sign_mark(MAIL *pmail);
-extern int message_sign_stop(void);
-extern void message_sign_free(void);
-extern BOOL message_sign_refresh(void);
