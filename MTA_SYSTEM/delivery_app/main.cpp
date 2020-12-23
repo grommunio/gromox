a3ac18a753bed164f7ae0168e3d3179864c8ba34
C. M. Palmerston <cmp@gromox.com>
2020-12-23 12:52:32 +0100

    mta_system: delete message_insulatin plugin

--- MTA_SYSTEM/delivery_app/main.cpp
+++ MTA_SYSTEM/delivery_app/main.cpp
@@ -38,7 +38,6 @@ static const char *const g_dfl_mpc_plugins[] = {
 	"libmtahook_exmdb_local.so",
 	"libmtahook_from_replace.so",
 	"libmtahook_mail_forwarder.so",
-	"libmtahook_message_insulation.so",
 	"libmtahook_mlist_expand.so",
 	"libmtahook_os_inspection.so",
 	"libmtahook_user_forwarder.so",
