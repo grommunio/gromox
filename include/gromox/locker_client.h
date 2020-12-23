4847a848bd96826860fc20c5034a928ed476be09
C. M. Palmerston <cmp@gromox.com>
2020-12-23 13:42:18 +0100

    lib: delete unused locker_client

--- include/gromox/locker_client.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define		LOCKD	int
-
-void locker_client_init(const char *ip, int port, int max_interval);
-LOCKD locker_client_lock(const char *resource);
-
-void locker_client_unlock(LOCKD lockd);
