5fb6b6ae9f5c9c6fec7c7e752bbf2d7e381c513d
François Châteauneuf <fcn@gromox.com>
2020-12-28 21:12:18 +0100

    mta_system: delete inmail_frequency_audit service plugin
    References: GX-44

--- MTA_SYSTEM/smtp/main.cpp
+++ MTA_SYSTEM/smtp/main.cpp
@@ -48,7 +48,6 @@ static const char *const g_dfl_as_plugins[] = {
 static const char *const g_dfl_svc_plugins[] = {
 	"libmtasvc_boundary_list.so",
 	"libmtasvc_domain_list.so",
-	"libmtasvc_inmail_frequency_audit.so",
 	"libmtasvc_ip_container.so",
 	"libmtasvc_ip_filter.so",
 	"libmtasvc_log_plugin.so",
