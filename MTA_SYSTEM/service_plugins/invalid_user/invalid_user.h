8fec0919046cfb1325a704cc4c0b24682f5fc1c0
François Châteauneuf <fcn@gromox.com>
2021-01-06 19:06:46 +0100

    mta_system: delete invalid_user service plugin
    References: GX-44

--- MTA_SYSTEM/service_plugins/invalid_user/invalid_user.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "common_types.h"
-
-void invalid_user_init(const char *config_path, int size, int valid_intvl);
-extern int invalid_user_run(void);
-BOOL invalid_user_check(const char *rcpt_address);
-extern int invalid_user_stop(void);
-extern void invalid_user_free(void);
-void invalid_user_console_talk(int argc, char **argv, char *result, int length);
