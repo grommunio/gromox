aee724565bc6834322ca2207854bc8f4b03f7d4c
François Châteauneuf <fcn@gromox.com>
2021-01-07 11:31:26 +0100

    include: add compile-testing code that makes BOOL a non-convertible type
    This helps identifying places that mix BOOL and bool/int/anything-else.

--- include/gromox/common_types.hpp
+++ include/gromox/common_types.hpp
@@ -2,19 +2,33 @@
 #include <cstddef>
 #include <cstdint>
 #ifndef BOOL
-#define BOOL    int
+#if defined(__cplusplus) && defined(BOOLCHK)
+struct BOOL {
+	int v;
+#if 0
+	constexpr BOOL() noexcept = default;
+	constexpr BOOL(int z) noexcept : m_bool_value{z} {}
+	constexpr bool operator==(const BOOL &o) const noexcept { return m_bool_value == o.m_bool_value; }
+	constexpr bool operator!=(const BOOL &o) const noexcept { return m_bool_value != o.m_bool_value; }
+	constexpr bool operator!() const noexcept { return !m_bool_value; }
+	constexpr operator bool() const noexcept { return m_bool_value; }
+	constexpr operator int() const noexcept = delete;
+#endif
+};
+inline bool operator==(const BOOL &a, const BOOL &b) { return a.v == b.v; }
+inline bool operator!=(const BOOL &a, const BOOL &b) { return a.v != b.v; }
+inline bool operator!(const BOOL &a) { return !a.v; }
+static constexpr BOOL FALSE{0};
+static constexpr BOOL TRUE{-1};
+#else
+#define BOOL    long
+#define TRUE    (-1)
+#define FALSE   0
+#endif
 #endif
 
 #ifndef NULL
 #define NULL    0
-#endif
-
-#ifndef TRUE
-#define TRUE    (-1)
-#endif
-
-#ifndef FALSE
-#define FALSE   0
 #endif
 
 struct GUID {
