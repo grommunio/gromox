a1af3379bc335b88e531dfb2d0d1c78ff25a9973
François Châteauneuf <fcn@gromox.com>
2021-01-09 23:02:49 +0100

    exmdb_local: fix function signature mismatches

--- mda/exmdb_local/cache_queue.h
+++ mda/exmdb_local/cache_queue.h
@@ -16,9 +16,7 @@ void cache_queue_init(const char *path, int scan_interval, int retrying_times);
 extern int cache_queue_run(void);
 extern int cache_queue_stop(void);
 extern void cache_queue_free(void);
-BOOL cache_queue_put(MESSAGE_CONTEXT *pcontext, const char *rcpt_to,
-	time_t original_time);
-
+extern int cache_queue_put(MESSAGE_CONTEXT *, const char *rcpt, time_t orig_time);
 int cache_queue_get_param(int param);
 
 void cache_queue_set_param(int param, int val);
