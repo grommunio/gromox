be4b3249ce51d01d9d76ee012d2026216294e7ba
François Châteauneuf <fcn@gromox.com>
2021-01-10 12:37:20 +0100

    freebusy: move webkit_lib code into freebusy.cpp

--- include/gromox/cookie_parser.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <map>
-#include <string>
-#include <gromox/defs.h>
-
-using cookie_jar = std::map<std::string, std::string, std::less<>>;
-
-extern GX_EXPORT cookie_jar cookie_parser_init(const char *cookie_string);
-extern GX_EXPORT const char *cookie_parser_get(const cookie_jar &, const char *name);
