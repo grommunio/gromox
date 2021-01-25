38e77ff9a5ac5a203b427edd52e44ffcbbffa261
C. M. Palmerston <cmp@gromox.com>
2021-01-26 00:45:02 +0100

    str_table: merge main.cpp into str_table.cpp

--- exch/str_table/str_table.cpp
+++ exch/str_table/str_table.cpp
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: GPL-2.0-only WITH linking exception
 #include <cerrno>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
+#include <fcntl.h>
 #include <unistd.h>
 #include <libHX/defs.h>
 #include <libHX/string.h>
+#include <gromox/common_types.hpp>
+#include <gromox/config_file.hpp>
 #include <gromox/defs.h>
-#include "str_table.h"
 #include <gromox/str_hash.hpp>
+#include <gromox/svc_common.h>
 #include <gromox/util.hpp>
 #include <gromox/list_file.hpp>
 #include <pthread.h>
 #include <cstdarg>
+#include <unistd.h>
+#define DEF_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
 
 enum{
 	STR_TABLE_REFRESH_OK,
@@ -19,6 +25,7 @@ enum{
 	STR_TABLE_REFRESH_HASH_FAIL
 };
 
+static void str_table_echo(const char *, ...);
 static int str_table_refresh(void);
 
 static STR_HASH_TABLE *g_string_list_table;
@@ -28,10 +35,12 @@ static BOOL g_case_sensitive;
 static char g_module_name[256];
 static int g_growing_num;
 static int g_hash_cap;
+DECLARE_API;
+
 /*
  *	string table's construct function
  */
-void str_table_init(const char *module_name, BOOL case_sensitive,
+static void str_table_init(const char *module_name, BOOL case_sensitive,
 	const char *path, int growing_num)
 {
 	HX_strlcpy(g_module_name, module_name, GX_ARRAY_SIZE(g_module_name));
@@ -44,7 +53,7 @@ void str_table_init(const char *module_name, BOOL case_sensitive,
 /*
  *	string table's destruct function
  */
-void str_table_free()
+static void str_table_free()
 {
 	g_list_path[0] = '\0';
 	g_growing_num = 0;
@@ -57,7 +66,7 @@ void str_table_free()
  *		 0		success
  *		<>0		fail
  */
-int str_table_run()
+static int str_table_run()
 {
     pthread_rwlock_init(&g_refresh_lock, NULL);
     if (STR_TABLE_REFRESH_OK != str_table_refresh()) {
@@ -72,7 +81,7 @@ int str_table_run()
  *		 0		success
  *		<>0		fail
  */
-int str_table_stop()
+static int str_table_stop()
 {
     if (NULL != g_string_list_table) {
         str_hash_free(g_string_list_table);
@@ -92,7 +101,7 @@ int str_table_stop()
  *      TRUE        allow
  *      FALSE       disallow
  */
-BOOL str_table_query(const char* str)
+static BOOL str_table_query(const char* str)
 {
 	char temp_string[256];
 	
@@ -172,7 +181,7 @@ static int str_table_refresh()
  *		TRUE			OK
  *		FALSE			fail
  */
-BOOL str_table_add(const char* str)
+static BOOL str_table_add(const char* str)
 {
 	char temp_string[256];
 	char file_item[512];
@@ -255,7 +264,7 @@ BOOL str_table_add(const char* str)
  *		TRUE			OK
  *		FALSE			fail
  */
-BOOL str_table_remove(const char* str)
+static BOOL str_table_remove(const char* str)
 {
 	int i, j;
 	int fd, string_len;
@@ -318,7 +327,7 @@ BOOL str_table_remove(const char* str)
  *		result [out]	buffer for retrieving result
  *		length			result buffer length
  */
-void str_table_console_talk(int argc, char **argv, char *result, int length)
+static void str_table_console_talk(int argc, char **argv, char *result, int length)
 {
 	char help_string[] = "250 string table help information:\r\n"
 						 "\t%s reload\r\n"
@@ -383,7 +392,7 @@ void str_table_console_talk(int argc, char **argv, char *result, int length)
 	return;
 }
 
-void str_table_echo(const char *format, ...)
+static void str_table_echo(const char *format, ...)
 {
 	char msg[256];
 	va_list ap;
@@ -394,4 +403,102 @@ void str_table_echo(const char *format, ...)
 	va_end(ap);
 	printf("[%s]: %s\n", g_module_name, msg);
 
+}
+
+BOOL SVC_LibMain(int reason, void **ppdata)
+{
+	CONFIG_FILE *pfile;
+	char file_name[256], tmp_path[256], *str_value, *psearch;
+	char *query_name, *add_name, *remove_name;
+	BOOL case_sensitive;
+	int growing_num;
+
+	switch (reason) {
+	case PLUGIN_INIT:
+		LINK_API(ppdata);
+		HX_strlcpy(file_name, get_plugin_name(), GX_ARRAY_SIZE(file_name));
+		psearch = strrchr(file_name, '.');
+		if (psearch != nullptr)
+			*psearch = '\0';
+		if (!register_talk(str_table_console_talk)) {
+			printf("[%s]: failed to register console talk\n", file_name);
+			return false;
+		}
+		sprintf(tmp_path, "%s/%s.cfg", get_config_path(), file_name);
+		pfile = config_file_init2(NULL, tmp_path);
+		if (pfile == nullptr) {
+			printf("[%s]: config_file_init %s: %s\n", file_name,
+			       tmp_path, strerror(errno));
+			return false;
+		}
+		query_name = config_file_get_value(pfile, "QUERY_SERVICE_NAME");
+		add_name = config_file_get_value(pfile, "ADD_SERVICE_NAME");
+		remove_name = config_file_get_value(pfile, "REMOVE_SERVICE_NAME");
+
+		str_value = config_file_get_value(pfile, "GROWING_NUM");
+		if (str_value == nullptr) {
+			growing_num = 100;
+			config_file_set_value(pfile, "GROWING_NUM", "100");
+		} else {
+			growing_num = atoi(str_value);
+			if (growing_num <= 0) {
+				growing_num = 100;
+				config_file_set_value(pfile, "GROWING_NUM", "100");
+			}
+		}
+		printf("[%s]: table growing number is %d\n", file_name, growing_num);
+		str_value = config_file_get_value(pfile, "IS_CASE_SENSITIVE");
+		if (str_value == nullptr) {
+			case_sensitive = FALSE;
+			config_file_set_value(pfile, "IS_CASE_SENSITIVE", "FALSE");
+			printf("[%s]: case-insensitive\n", file_name);
+		} else {
+			if (strcasecmp(str_value, "FALSE") == 0) {
+				case_sensitive = FALSE;
+				printf("[%s]: case-insensitive\n", file_name);
+			} else if (strcasecmp(str_value, "TRUE") == 0) {
+				case_sensitive = TRUE;
+				printf("[%s]: case-sensitive\n", file_name);
+			} else {
+				case_sensitive = FALSE;
+				config_file_set_value(pfile, "IS_CASE_SENSITIVE", "FALSE");
+				printf("[%s]: case-insensitive\n", file_name);
+			}
+		}
+		sprintf(tmp_path, "%s/%s.txt", get_data_path(), file_name);
+		str_table_init(file_name, case_sensitive, tmp_path, growing_num);
+		if (str_table_run() != 0) {
+			printf("[%s]: failed to run the module\n", file_name);
+			config_file_free(pfile);
+			return FALSE;
+		}
+		if (query_name != nullptr && !register_service(query_name,
+		    reinterpret_cast<void *>(str_table_query))) {
+			printf("[%s]: failed to register \"%s\" service\n",
+			       file_name, query_name);
+			config_file_free(pfile);
+			return false;
+		}
+		if (add_name != nullptr && !register_service(add_name,
+		    reinterpret_cast<void *>(str_table_add))) {
+			printf("[%s]: failed to register \"%s\" service\n",
+			       file_name, add_name);
+			config_file_free(pfile);
+			return false;
+		}
+		if (remove_name != nullptr && !register_service(remove_name,
+		    reinterpret_cast<void *>(str_table_remove))) {
+			printf("[%s]: failed to register \"%s\" service\n",
+			       file_name, remove_name);
+			config_file_free(pfile);
+			return false;
+		}
+		config_file_free(pfile);
+		return TRUE;
+	case PLUGIN_FREE:
+		str_table_stop();
+		str_table_free();
+		return TRUE;
+	}
+	return false;
 }
