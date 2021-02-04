67653f30ec059b612f62f2608385786fe0773de2
C. M. Palmerston <cmp@gromox.com>
2021-02-04 11:18:30 +0100

    midb: fix load from misaligned addresses
    exmdb_client.cpp:109:16: runtime error: load of misaligned address ..
    for type "uint32_t", which requires 4 byte alignment
    exmdb_client.cpp:192:31: runtime error: load of misaligned address ..
    for type "uint32_t", which requires 4 byte alignment

--- exch/midb/exmdb_client.cpp
+++ exch/midb/exmdb_client.cpp
@@ -106,7 +106,9 @@ static BOOL exmdb_client_read_socket(int sockd, BINARY *pbin)
 				*(uint8_t*)pbin->pb = resp_buff[0];
 				return TRUE;
 			} else if (5 == read_len) {
-				pbin->cb = *(uint32_t*)(resp_buff + 1) + 5;
+				uint32_t cb;
+				memcpy(&cb, resp_buff + 1, sizeof(cb));
+				pbin->cb = cb + 5;
 				pbin->pv = common_util_alloc(pbin->cb);
 				if (pbin->pv == nullptr)
 					return FALSE;
@@ -189,7 +191,9 @@ static int exmdb_client_connect_exmdb(REMOTE_SVR *pserver, BOOL b_listen)
 	}
 	response_code = tmp_bin.pb[0];
 	if (response_code == exmdb_response::SUCCESS) {
-		if (5 != tmp_bin.cb || 0 != *(uint32_t*)(tmp_bin.pb + 1)) {
+		uint32_t cb;
+		memcpy(&cb, tmp_bin.pb + 1, sizeof(cb));
+		if (tmp_bin.cb != 5 || cb != 0) {
 			common_util_free_environment();
 			printf("[exmdb_client]: response format error "
 				"when connect to %s:%d for prefix \"%s\"\n",
