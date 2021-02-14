5cfef35332565a306f4f21d1fa7f079e4e52ef37
C. M. Palmerston <cmp@gromox.com>
2021-02-14 14:23:12 +0100

    mta_system: delete boundary_list service plugin

--- mda/smtp/main.cpp
+++ mda/smtp/main.cpp
@@ -48,7 +48,6 @@ static const char *const g_dfl_as_plugins[] = {
 };
 
 static const char *const g_dfl_svc_plugins[] = {
-	"libmtasvc_boundary_list.so",
 	"libmtasvc_domain_list.so",
 	"libgxsvc_ip6_container.so",
 	"libgxsvc_ip6_filter.so",
