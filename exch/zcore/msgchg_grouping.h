770f2b0369376b56ca1a0881b12e6ae8a46534d7
C. M. Palmerston <cmp@gromox.com>
2021-02-14 16:46:31 +0100

    exch: deduplicate msgchg_grouping.cpp

--- exch/zcore/msgchg_grouping.h
+++ exch/zcore/msgchg_grouping.h
@@ -1,11 +1,1 @@
-#pragma once
-#include <cstdint>
-#include <gromox/element_data.hpp>
-#include <gromox/mapi_types.hpp>
-
-void msgchg_grouping_init(const char *path);
-extern int msgchg_grouping_run(void);
-extern uint32_t msgchg_grouping_get_last_group_id(void);
-extern PROPERTY_GROUPINFO *msgchg_grouping_get_groupinfo(BOOL (*)(void *, BOOL, const PROPERTY_NAME *, uint16_t *), void *, uint32_t group_id);
-extern int msgchg_grouping_stop(void);
-extern void msgchg_grouping_free(void);
+../exchange_emsmdb/msgchg_grouping.h
