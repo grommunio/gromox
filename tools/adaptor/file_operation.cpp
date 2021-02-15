7c7642dde42b240cdda79db166ddf5d6c46d9243
François Châteauneuf <fcn@gromox.com>
2021-02-15 08:39:35 +0100

    adaptor: fix coverity TOCTOU report
    CID 1441616

--- tools/adaptor/file_operation.cpp
+++ tools/adaptor/file_operation.cpp
@@ -3,6 +3,7 @@
 #include <libHX/defs.h>
 #include <libHX/string.h>
 #include <gromox/defs.h>
+#include <gromox/scope.hpp>
 #include "file_operation.h"
 #include <gromox/list_file.hpp>
 #include <sys/types.h>
@@ -12,14 +13,22 @@
 #include <cstdlib>
 #include <dirent.h>
 
+using namespace gromox;
+
 int file_operation_compare(const char *file1, const char *file2)
 {
-	int fd1, fd2;
 	struct stat node_stat1, node_stat2;
 
-	if (0 != stat(file1, &node_stat1) || 0 != stat(file2, &node_stat2)) {
-		return FILE_COMPARE_DIFFERENT;
-	}
+	auto fd1 = open(file1, O_RDONLY);
+	if (fd1 < 0)
+		return errno == ENOENT ? FILE_COMPARE_DIFFERENT : FILE_COMPARE_FAIL;
+	auto cl_1 = make_scope_exit([&]() { close(fd1); });
+	auto fd2 = open(file2, O_RDONLY);
+	if (fd2 < 0)
+		return errno == ENOENT ? FILE_COMPARE_DIFFERENT : FILE_COMPARE_FAIL;
+	auto cl_2 = make_scope_exit([&]() { close(fd2); });
+	if (fstat(fd1, &node_stat1) != 0 || fstat(fd2, &node_stat2) != 0)
+		return FILE_COMPARE_FAIL;
 	if (node_stat1.st_size != node_stat2.st_size) {
 		return FILE_COMPARE_DIFFERENT;
 	}
@@ -27,27 +36,12 @@ int file_operation_compare(const char *file1, const char *file2)
 	if (NULL == ptr) {
 		return FILE_COMPARE_FAIL;
 	}
-	fd1 = open(file1, O_RDONLY);
-	if (-1 == fd1) {
-		free(ptr);
-		return FILE_COMPARE_FAIL;
-	}
-	fd2 = open(file2, O_RDONLY);
-	if (-1 == fd2) {
-		free(ptr);
-		close(fd1);
-		return FILE_COMPARE_FAIL;
-	}
 	if (node_stat1.st_size != read(fd1, ptr, node_stat1.st_size) ||
 		node_stat2.st_size != read(fd2, ptr + node_stat1.st_size,
 		node_stat2.st_size)) {
 		free(ptr);
-		close(fd1);
-		close(fd2);
 		return FILE_COMPARE_FAIL;
 	}
-	close(fd1);
-	close(fd2);
 	if (0 == memcmp(ptr, ptr + node_stat1.st_size, node_stat1.st_size)) {
 		free(ptr);
 		return FILE_COMPARE_SAME;
