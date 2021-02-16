85c3e7115bb5be299064e4f5f9c4a2a60d30f3f1
C. M. Palmerston <cmp@gromox.com>
2021-02-16 23:58:31 +0100

    mda: delete from_replace hook plugin

--- mda/delivery_app/main.cpp
+++ mda/delivery_app/main.cpp
@@ -38,7 +38,6 @@ static struct HXoption g_options_table[] = {
 static const char *const g_dfl_mpc_plugins[] = {
 	"libmtahook_alias_translator.so",
 	"libmtahook_exmdb_local.so",
-	"libmtahook_from_replace.so",
 	"libmtahook_mlist_expand.so",
 	NULL,
 };
