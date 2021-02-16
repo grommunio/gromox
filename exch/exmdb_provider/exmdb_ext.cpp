7ad17e02c06c94245ee4e746c48c173eeaaba31f
François Châteauneuf <fcn@gromox.com>
2021-02-16 14:48:12 +0100

    exmdb_provider: compact repeated ext_buffer_* error checking 2

--- exch/exmdb_provider/exmdb_ext.cpp
+++ exch/exmdb_provider/exmdb_ext.cpp
@@ -6,6 +6,7 @@
 #include <gromox/rop_util.hpp>
 #include <gromox/idset.hpp>
 #define TRY(expr) do { int v = (expr); if (v != EXT_ERR_SUCCESS) return v; } while (false)
+#define QYY(expr) do { int v = (expr); if (v != EXT_ERR_SUCCESS) return EXT_ERR_SUCCESS; } while (false) /* legit? */
 
 static int exmdb_ext_pull_connect_request(
 	EXT_PULL *pext, REQUEST_PAYLOAD *ppayload)
@@ -2106,37 +2107,17 @@ static int exmdb_ext_push_unlink_message_request(
 static int exmdb_ext_pull_rule_new_message_request(
 	EXT_PULL *pext, REQUEST_PAYLOAD *ppayload)
 {
-	int status;
 	uint8_t tmp_byte;
 	
-	status = ext_buffer_pull_uint8(pext, &tmp_byte);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
+	QYY(ext_buffer_pull_uint8(pext, &tmp_byte ));
 	if (0 == tmp_byte) {
 		ppayload->rule_new_message.username = NULL;
 	} else {
-		status = ext_buffer_pull_string(pext,
-			&ppayload->rule_new_message.username);
-		if (EXT_ERR_SUCCESS != status) {
-			return EXT_ERR_SUCCESS;
-		}
+		QYY(ext_buffer_pull_string(pext, &ppayload->rule_new_message.username));
 	}
-	status = ext_buffer_pull_string(pext,
-		&ppayload->rule_new_message.account);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
-	status = ext_buffer_pull_uint32(pext,
-		&ppayload->rule_new_message.cpid);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
-	status = ext_buffer_pull_uint64(pext,
-		&ppayload->rule_new_message.folder_id);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
+	QYY(ext_buffer_pull_string(pext, &ppayload->rule_new_message.account));
+	QYY(ext_buffer_pull_uint32(pext, &ppayload->rule_new_message.cpid));
+	QYY(ext_buffer_pull_uint64(pext, &ppayload->rule_new_message.folder_id));
 	return ext_buffer_pull_uint64(pext,
 		&ppayload->rule_new_message.message_id);
 }
@@ -2144,39 +2125,15 @@ static int exmdb_ext_pull_rule_new_message_request(
 static int exmdb_ext_push_rule_new_message_request(
 	EXT_PUSH *pext, const REQUEST_PAYLOAD *ppayload)
 {
-	int status;
-	
 	if (NULL == ppayload->rule_new_message.username) {
-		status = ext_buffer_push_uint8(pext, 0);
-		if (EXT_ERR_SUCCESS != status) {
-			return EXT_ERR_SUCCESS;
-		}
+		QYY(ext_buffer_push_uint8(pext, 0 ));
 	} else {
-		status = ext_buffer_push_uint8(pext, 1);
-		if (EXT_ERR_SUCCESS != status) {
-			return EXT_ERR_SUCCESS;
-		}
-		status = ext_buffer_push_string(pext,
-			ppayload->rule_new_message.username);
-		if (EXT_ERR_SUCCESS != status) {
-			return EXT_ERR_SUCCESS;
-		}
+		QYY(ext_buffer_push_uint8(pext, 1 ));
+		QYY(ext_buffer_push_string(pext, ppayload->rule_new_message.username));
 	}
-	status = ext_buffer_push_string(pext,
-		ppayload->rule_new_message.account);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
-	status = ext_buffer_push_uint32(pext,
-		ppayload->rule_new_message.cpid);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
-	status = ext_buffer_push_uint64(pext,
-		ppayload->rule_new_message.folder_id);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
+	QYY(ext_buffer_push_string(pext, ppayload->rule_new_message.account));
+	QYY(ext_buffer_push_uint32(pext, ppayload->rule_new_message.cpid));
+	QYY(ext_buffer_push_uint64(pext, ppayload->rule_new_message.folder_id));
 	return ext_buffer_push_uint64(pext,
 		ppayload->rule_new_message.message_id);
 }
@@ -2184,13 +2141,7 @@ static int exmdb_ext_push_rule_new_message_request(
 static int exmdb_ext_pull_set_message_timer_request(
 	EXT_PULL *pext, REQUEST_PAYLOAD *ppayload)
 {
-	int status;
-
-	status = ext_buffer_pull_uint64(pext,
-		&ppayload->set_message_timer.message_id);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
+	QYY(ext_buffer_pull_uint64(pext, &ppayload->set_message_timer.message_id));
 	return ext_buffer_pull_uint32(pext,
 		&ppayload->set_message_timer.timer_id);
 }
@@ -2198,13 +2149,7 @@ static int exmdb_ext_pull_set_message_timer_request(
 static int exmdb_ext_push_set_message_timer_request(
 	EXT_PUSH *pext, const REQUEST_PAYLOAD *ppayload)
 {
-	int status;
-	
-	status = ext_buffer_push_uint64(pext,
-		ppayload->set_message_timer.message_id);
-	if (EXT_ERR_SUCCESS != status) {
-		return EXT_ERR_SUCCESS;
-	}
+	QYY(ext_buffer_push_uint64(pext, ppayload->set_message_timer.message_id));
 	return ext_buffer_push_uint32(pext,
 		ppayload->set_message_timer.timer_id);
 }
