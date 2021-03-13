060a84222a9caad50b528c86296edec43beb2c6e
Jan Engelhardt <jengelh@inai.de>
2021-03-13 11:03:33 +0100

    http: remove unused functions

--- exch/http/console_cmd_handler.h
+++ exch/http/console_cmd_handler.h
@@ -5,9 +5,6 @@ BOOL cmd_handler_http_control(int argc, char** argv);
 
 BOOL cmd_handler_rpc_control(int argc, char** argv);
 BOOL cmd_handler_help(int argc, char** argv);
-
-BOOL cmd_handler_server_control(int argc, char** argv);
-
 BOOL cmd_handler_system_control(int argc, char** argv);
 
 BOOL cmd_handler_proc_plugins(int argc, char** argv);
--- exch/http/resource.h
+++ exch/http/resource.h
@@ -6,10 +6,4 @@
 #define resource_set_string(k, v) config_file_set_value(g_config_file, (k), (v))
 #define resource_get_integer(k, vp) config_file_get_int(g_config_file, (k), (vp))
 #define resource_set_integer(k, v) config_file_set_int(g_config_file, (k), (v))
-
-extern void resource_init();
-extern void resource_free();
-extern int resource_run();
-extern int resource_stop();
-
 extern std::shared_ptr<CONFIG_FILE> g_config_file;
