de0fcf590645a23aa5478da394ae64c842d31198
Jan Engelhardt <jengelh@inai.de>
2021-03-24 16:54:27 +0100

    lib: add wrapper for sqlite3_stmt
    This wrapper is planned to be stepwise be upgraded to function as RAII.

--- include/gromox/database.h
+++ include/gromox/database.h
@@ -3,13 +3,19 @@
 #include <cstdio>
 #include <sqlite3.h>
 
-static inline sqlite3_stmt *gx_sql_prep(sqlite3 *db, const char *query)
+struct xstmt {
+	void operator=(sqlite3_stmt *s) { m_ptr = s; }
+	operator sqlite3_stmt *() { return m_ptr; }
+	sqlite3_stmt *m_ptr = nullptr;
+};
+
+static inline xstmt gx_sql_prep(sqlite3 *db, const char *query)
 {
-	sqlite3_stmt *p = nullptr;
-	int ret = sqlite3_prepare_v2(db, query, -1, &p, nullptr);
+	xstmt out;
+	int ret = sqlite3_prepare_v2(db, query, -1, &out.m_ptr, nullptr);
 	if (ret != SQLITE_OK)
 		printf("sqlite3_prepare_v2 \"%s\": %s\n", query, sqlite3_errstr(ret));
-	return p;
+	return out;
 }
 
 static inline uint64_t gx_sql_col_uint64(sqlite3_stmt *s, int c)
