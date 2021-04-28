d784188dc9d1c81df7cad4a569742d726311fc0f
C. M. Palmerston <cmp@gromox.com>
2021-04-28 10:20:29 +0200

    mysql_adaptor: change default schema_upgrades action to "skip"

--- exch/mysql_adaptor/main.cpp
+++ exch/mysql_adaptor/main.cpp
@@ -98,7 +98,7 @@ static BOOL svc_mysql_adaptor(int reason, void** ppdata)
 		}
 
 		str_value = config_file_get_value(pfile, "schema_upgrades");
-		enum sql_schema_upgrade upg = S_ABORT;
+		enum sql_schema_upgrade upg = S_SKIP;
 		auto prog_id = get_prog_id();
 		if (str_value != nullptr && strncmp(str_value, "host:", 5) == 0 &&
 		    prog_id != nullptr && strcmp(prog_id, "http") == 0 &&
