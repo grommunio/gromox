55bcfc4beae937997358676ea1886c5f90eb55be
François Châteauneuf <fcn@gromox.com>
2021-07-07 10:54:05 +0200

    lib: add OOP-style interface/member functions to EXT_PULL class

--- include/gromox/ext_buffer.hpp
+++ include/gromox/ext_buffer.hpp
@@ -27,18 +27,7 @@ struct EXT_BUFFER_MGT {
 	void (*free)(void *);
 };
 
-struct EXT_PULL {
-	EXT_BUFFER_ALLOC alloc{};
-	template<typename T> inline T *anew() { return static_cast<T *>(alloc(sizeof(T))); }
-	template<typename T> inline T *anew(size_t elem) { return static_cast<T *>(alloc(sizeof(T) * elem)); }
-	union {
-		const uint8_t *data, *udata;
-		const char *cdata;
-		const void *vdata = nullptr;
-	};
-	uint32_t data_size = 0, offset = 0, flags = 0;
-};
-
+struct EXT_PULL;
 struct EXT_PUSH;
 /* bitmap RPC_HEADER_EXT flags */
 #define RHE_FLAG_COMPRESSED							0x0001
@@ -159,6 +148,88 @@ int ext_buffer_pull_appointmentrecurrencepattern(
 int ext_buffer_pull_globalobjectid(EXT_PULL *pext, GLOBALOBJECTID *r);
 int ext_buffer_pull_message_content(EXT_PULL *pext, MESSAGE_CONTENT *pmsg);
 
+struct EXT_PULL {
+	EXT_BUFFER_ALLOC alloc{};
+	inline void init(const void *d, uint32_t z, EXT_BUFFER_ALLOC a, uint32_t fl) { return ext_buffer_pull_init(this, d, z, a, fl); }
+	inline int advance(uint32_t n) { return ext_buffer_pull_advance(this, n); }
+	inline int g_rpc_header_ext(RPC_HEADER_EXT *r) { return ext_buffer_pull_rpc_header_ext(this, r); }
+	inline int g_int8(int8_t *v) { return ext_buffer_pull_int8(this, v); }
+	inline int g_uint8(uint8_t *v) { return ext_buffer_pull_uint8(this, v); }
+	inline int g_int16(int16_t *v) { return ext_buffer_pull_int16(this, v); }
+	inline int g_uint16(uint16_t *v) { return ext_buffer_pull_uint16(this, v); }
+	inline int g_int32(int32_t *v) { return ext_buffer_pull_int32(this, v); }
+	inline int g_uint32(uint32_t *v) { return ext_buffer_pull_uint32(this, v); }
+	inline int g_int64(int64_t *v) { return ext_buffer_pull_int64(this, v); }
+	inline int g_uint64(uint64_t *v) { return ext_buffer_pull_uint64(this, v); }
+	inline int g_float(float *v) { return ext_buffer_pull_float(this, v); }
+	inline int g_double(double *v) { return ext_buffer_pull_double(this, v); }
+	inline int g_bool(BOOL *v) { return ext_buffer_pull_bool(this, v); }
+	inline int g_bytes(void *d, uint32_t n) { return ext_buffer_pull_bytes(this, d, n); }
+	inline int g_guid(GUID *r) { return ext_buffer_pull_guid(this, r); }
+	inline int g_str(char **s) { return ext_buffer_pull_string(this, s); }
+	inline int g_wstr(char **s) { return ext_buffer_pull_wstring(this, s); }
+	inline int g_blob(DATA_BLOB *b) { return ext_buffer_pull_data_blob(this, b); }
+	inline int g_bin(BINARY *r) { return ext_buffer_pull_binary(this, r); }
+	inline int g_sbin(BINARY *r) { return ext_buffer_pull_sbinary(this, r); }
+	inline int g_exbin(BINARY *r) { return ext_buffer_pull_exbinary(this, r); }
+	inline int g_uint16_a(SHORT_ARRAY *r) { return ext_buffer_pull_short_array(this, r); }
+	inline int g_uint32_a(LONG_ARRAY *r) { return ext_buffer_pull_long_array(this, r); }
+	inline int g_uint64_a(LONGLONG_ARRAY *r) { return ext_buffer_pull_longlong_array(this, r); }
+	inline int g_uint64_sa(LONGLONG_ARRAY *r) { return ext_buffer_pull_slonglong_array(this, r); }
+	inline int g_bin_a(BINARY_ARRAY *r) { return ext_buffer_pull_binary_array(this, r); }
+	inline int g_str_a(STRING_ARRAY *r) { return ext_buffer_pull_string_array(this, r); }
+	inline int g_wstr_a(STRING_ARRAY *r) { return ext_buffer_pull_wstring_array(this, r); }
+	inline int g_guid_a(GUID_ARRAY *r) { return ext_buffer_pull_guid_array(this, r); }
+	inline int g_proptag_a(PROPTAG_ARRAY *r) { return ext_buffer_pull_proptag_array(this, r); }
+	inline int g_restriction(RESTRICTION *r) { return ext_buffer_pull_restriction(this, r); }
+	inline int g_svreid(SVREID *r) { return ext_buffer_pull_svreid(this, r); }
+	inline int g_store_eid(STORE_ENTRYID *r) { return ext_buffer_pull_store_entryid(this, r); }
+	inline int g_rule_actions(RULE_ACTIONS *r) { return ext_buffer_pull_rule_actions(this, r); }
+	inline int g_ext_rule_actions(EXT_RULE_ACTIONS *r) { return ext_buffer_pull_ext_rule_actions(this, r); }
+	inline int g_namedprop_info(NAMEDPROPERTY_INFOMATION *r) { return ext_buffer_pull_namedproperty_information(this, r); }
+	inline int g_longterm(LONG_TERM_ID *r) { return ext_buffer_pull_long_term_id(this, r); }
+	inline int g_longterm_rang(LONG_TERM_ID_RANGE *r) { return ext_buffer_pull_long_term_id_rang(this, r); }
+	inline int g_typed_pv(TYPED_PROPVAL *r) { return ext_buffer_pull_typed_propval(this, r); }
+	inline int g_propval(uint16_t type, void **v) { return ext_buffer_pull_propval(this, type, v); }
+	inline int g_tagged_pv(TAGGED_PROPVAL *r) { return ext_buffer_pull_tagged_propval(this, r); }
+	inline int g_flagged_pv(uint16_t type, FLAGGED_PROPVAL *r) { return ext_buffer_pull_flagged_propval(this, type, r); }
+	inline int g_proprow(const PROPTAG_ARRAY *cols, PROPERTY_ROW *r) { return ext_buffer_pull_property_row(this, cols, r); }
+	inline int g_propname(PROPERTY_NAME *r) { return ext_buffer_pull_property_name(this, r); }
+	inline int g_propname_a(PROPNAME_ARRAY *r) { return ext_buffer_pull_propname_array(this, r); }
+	inline int g_propid_a(PROPID_ARRAY *r) { return ext_buffer_pull_propid_array(this, r); }
+	inline int g_tpropval_a(TPROPVAL_ARRAY *r) { return ext_buffer_pull_tpropval_array(this, r); }
+	inline int g_tarray_set(TARRAY_SET *r) { return ext_buffer_pull_tarray_set(this, r); }
+	inline int g_problem_a(PROBLEM_ARRAY *r) { return ext_buffer_pull_problem_array(this, r); }
+	inline int g_xid(uint8_t size, XID *x) { return ext_buffer_pull_xid(this, size, x); }
+	inline int g_folder_eid(FOLDER_ENTRYID *r) { return ext_buffer_pull_folder_entryid(this, r); }
+	inline int g_msg_eid(MESSAGE_ENTRYID *r) { return ext_buffer_pull_message_entryid(this, r); }
+	inline int g_sortorder(SORT_ORDER *r) { return ext_buffer_pull_sort_order(this, r); }
+	inline int g_sortorder_set(SORTORDER_SET *r) { return ext_buffer_pull_sortorder_set(this, r); }
+	inline int g_recipient_row(const PROPTAG_ARRAY *tags, RECIPIENT_ROW *r) { return ext_buffer_pull_recipient_row(this, tags, r); }
+	inline int g_modrcpt_row(PROPTAG_ARRAY *tag, MODIFYRECIPIENT_ROW *r) { return ext_buffer_pull_modifyrecipient_row(this, tag, r); }
+	inline int g_permission_data(PERMISSION_DATA *r) { return ext_buffer_pull_permission_data(this, r); }
+	inline int g_rule_data(RULE_DATA *r) { return ext_buffer_pull_rule_data(this, r); }
+	inline int g_abk_eid(ADDRESSBOOK_ENTRYID *r) { return ext_buffer_pull_addressbook_entryid(this, r); }
+	inline int g_oneoff_eid(ONEOFF_ENTRYID *r) { return ext_buffer_pull_oneoff_entryid(this, r); }
+	inline int g_oneoff_a(ONEOFF_ARRAY *r) { return ext_buffer_pull_oneoff_array(this, r); }
+	inline int g_eid_a(EID_ARRAY *r) { return ext_buffer_pull_eid_array(this, r); }
+	inline int g_systime(SYSTEMTIME *r) { return ext_buffer_pull_systemtime(this, r); }
+	inline int g_tzstruct(TIMEZONESTRUCT *r) { return ext_buffer_pull_timezonestruct(this, r); }
+	inline int g_tzdef(TIMEZONEDEFINITION *r) { return ext_buffer_pull_timezonedefinition(this, r); }
+	inline int g_apptrecpat(APPOINTMENTRECURRENCEPATTERN *r) { return ext_buffer_pull_appointmentrecurrencepattern(this, r); }
+	inline int g_goid(GLOBALOBJECTID *r) { return ext_buffer_pull_globalobjectid(this, r); }
+	inline int g_msgctnt(MESSAGE_CONTENT *m) { return ext_buffer_pull_message_content(this, m); }
+
+	template<typename T> inline T *anew() { return static_cast<T *>(alloc(sizeof(T))); }
+	template<typename T> inline T *anew(size_t elem) { return static_cast<T *>(alloc(sizeof(T) * elem)); }
+	union {
+		const uint8_t *data, *udata;
+		const char *cdata;
+		const void *vdata = nullptr;
+	};
+	uint32_t data_size = 0, offset = 0, flags = 0;
+};
+
 struct EXT_PUSH {
 	~EXT_PUSH();
 	BOOL init(void *, uint32_t, uint32_t, const EXT_BUFFER_MGT * = nullptr);
@@ -167,13 +238,13 @@ struct EXT_PUSH {
 	int advance(uint32_t);
 	int p_bytes(const void *, uint32_t);
 	int p_uint8(uint8_t);
-	inline int p_int8(int8_t v) { return p_uint8(v); }
+	int p_int8(int8_t v) { return p_uint8(v); }
 	int p_uint16(uint16_t);
-	inline int p_int16(int16_t v) { return p_uint16(v); }
+	int p_int16(int16_t v) { return p_uint16(v); }
 	int p_uint32(uint32_t);
-	inline int p_int32(int32_t v) { return p_uint32(v); }
+	int p_int32(int32_t v) { return p_uint32(v); }
 	int p_uint64(uint64_t);
-	inline int p_int64(int64_t v) { return p_uint64(v); }
+	int p_int64(int64_t v) { return p_uint64(v); }
 	int p_float(float);
 	int p_double(double);
 	int p_bool(BOOL);
