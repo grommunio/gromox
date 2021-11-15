65194ec8e796d26734a4ea7f6847f3bb5d380701
Jan Engelhardt <jengelh@inai.de>
2021-11-15 14:31:16 +0100

    tests: add MJSON test routines

---
+++ tests/jsontest.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cstring>
+#include <gromox/mjson.hpp>
+
+static void enx(MJSON_MIME *mi, void *q)
+{
+	printf("this=%p type=%d id=%s\n", mi, mi->mime_type, mi->id);
+}
+
+static char tdata1[] =
+	"{\"file\":\"\",\"uid\":0,\"recent\":1,\"read\":0,\"replied\":0,"
+	"\"unsent\":0,\"forwarded\":0,\"flag\":0,\"priority\":3,"
+	"\"msgid\":\"PDg0YzA3MWFiM2ViOWUyNDU4YTg0MDRhYjhjMzBlMDAxQGFrby1kZXYtMDEuZ3JhbW1tLmNvbT4=\","
+	"\"from\":\"Ij0/dXRmLTg/Qj9VMmxuYldGeUlGTmphSExEdG1SbGNnPT0/PSIgPHRlc3QxQGdyYW1tbS5jb20+\","
+	"\"to\":\"Ij0/dXRmLTg/Qj81cldjNWJTTzVhQ0E1YkMrSUVoaGJXRnpZV3RwSUVodmNtbHY/PSIgPHRlc3RAZ3JhbW1tLmNvbT4=\","
+	"\"cc\":\"\",\"subject\":\"UQ==\","
+	"\"received\":\"RnJpLCAxMCBTZXAgMjAyMSAxMjoxMzowMiArMDIwMA==\","
+	"\"date\":\"RnJpLCAxMCBTZXAgMjAyMSAxMjoxMzowMiArMDIwMA==\","
+	"\"charset\":\"utf-8\",\"structure\":[{\"id\":\"\",\"ctype\":\"multipart/alternative\",\"head\":0,\"begin\":783, \"length\":539}],"
+	"\"mimes\":["
+	"{\"id\":\"1\",\"ctype\":\"text/plain\",\"encoding\":\"base64\",\"head\":875,\"begin\":957,\"length\":6,\"charset\":\"utf-8\"},"
+	"{\"id\":\"2\",\"ctype\":\"text/html\",\"encoding\":\"base64\",\"head\":1007,\"begin\":1088,\"length\":186,\"charset\":\"utf-8\"}],"
+	"\"size\":1322}";
+
+int main()
+{
+	alloc_limiter<MJSON_MIME> al(4096, "mjson");
+	MJSON m(&al);
+	if (!m.retrieve(tdata1, std::size(tdata1), "/tmp")) {
+		fprintf(stderr, "retrieve failed\n");
+		return EXIT_FAILURE;
+	}
+	m.enum_mime(enx, nullptr);
+	return EXIT_SUCCESS;
+}
