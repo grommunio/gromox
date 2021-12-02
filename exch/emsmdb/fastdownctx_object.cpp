d030a19e901a3517abafffd68140efa2d08535a3
Jan Engelhardt <jengelh@inai.de>
2021-12-02 14:03:05 +0100

    emsmdb: turn fastdownctx_object::flow_list into a stdlib container

--- exch/emsmdb/fastdownctx_object.cpp
+++ exch/emsmdb/fastdownctx_object.cpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only WITH linking exception
+#include <algorithm>
 #include <cstdint>
 #include <cstdio>
 #include <memory>
@@ -21,180 +22,97 @@ enum {
 	FUNC_ID_MESSAGE
 };
 
-namespace {
-
-struct FAST_FLOW_NODE {
-	DOUBLE_LIST_NODE node;
-	uint8_t func_id;
-	void *pparam;
-};
-
-}
-
-static BOOL fastdownctx_object_record_subfoldernodelprops(
-	DOUBLE_LIST *pflow_list, const FOLDER_CONTENT *pfldctnt);
-
-static BOOL fastdownctx_object_record_subfolder(
-	DOUBLE_LIST *pflow_list, const FOLDER_CONTENT *pfldctnt);
-
-static BOOL fastdownctx_object_record_flow_node(
-	DOUBLE_LIST *pflow_list, int func_id, void *pparam)
+bool fxdown_flow_list::record_node(uint8_t func_id, const void *param) try
 {
-	auto pflow = me_alloc<FAST_FLOW_NODE>();
-	if (NULL == pflow) {
-		return FALSE;
-	}
-	pflow->node.pdata = pflow;
-	pflow->func_id = func_id;
-	pflow->pparam = pparam;
-	double_list_append_as_tail(pflow_list, &pflow->node);
-	return TRUE;
+	emplace_back(func_id, param);
+	return true;
+} catch (const std::bad_alloc &) {
+	fprintf(stderr, "E-1599: ENOMEM\n");
+	return false;
 }
 
-static BOOL fastdownctx_object_record_messagelist(
-	DOUBLE_LIST *pflow_list, EID_ARRAY *pmsglst)
+bool fxdown_flow_list::record_tag(uint32_t tag)
+{
+	static_assert(sizeof(void *) >= sizeof(tag));
+	return record_node(FUNC_ID_UINT32, reinterpret_cast<void *>(static_cast<uintptr_t>(tag)));
+}
+
+bool fxdown_flow_list::record_messagelist(EID_ARRAY *pmsglst)
 {
 	for (size_t i = 0; i < pmsglst->count; ++i) {
-		if (FALSE == fastdownctx_object_record_flow_node(
-			pflow_list, FUNC_ID_MESSAGE, pmsglst->pids + i)) {
-			return FALSE;	
-		}
+		if (!record_node(FUNC_ID_MESSAGE, &pmsglst->pids[i]))
+			return false;
 	}
-	return TRUE;
+	return true;
 }
 
-static BOOL fastdownctx_object_record_foldermessages(
-	DOUBLE_LIST *pflow_list, const FOLDER_MESSAGES *pfldmsgs)
+bool fxdown_flow_list::record_foldermessages(const FOLDER_MESSAGES *pfldmsgs)
 {	
 	if (NULL != pfldmsgs->pfai_msglst) {
-		if (FALSE == fastdownctx_object_record_flow_node(
-			pflow_list, FUNC_ID_UINT32, (void*)META_TAG_FXDELPROP)) {
-			return FALSE;
-		}
-		if (FALSE == fastdownctx_object_record_flow_node(pflow_list,
-			FUNC_ID_UINT32, (void*)PROP_TAG_FOLDERASSOCIATEDCONTENTS)) {
-			return FALSE;
-		}
-		if (FALSE == fastdownctx_object_record_messagelist(
-			pflow_list, pfldmsgs->pfai_msglst)) {
-			return FALSE;
-		}
+		if (!record_tag(META_TAG_FXDELPROP) ||
+		    !record_tag(PROP_TAG_FOLDERASSOCIATEDCONTENTS) ||
+		    !record_messagelist(pfldmsgs->pfai_msglst))
+			return false;
 	}
 	if (NULL != pfldmsgs->pnormal_msglst) {
-		if (FALSE == fastdownctx_object_record_flow_node(
-			pflow_list, FUNC_ID_UINT32, (void*)META_TAG_FXDELPROP)) {
-			return FALSE;
-		}
-		if (FALSE == fastdownctx_object_record_flow_node(pflow_list,
-			FUNC_ID_UINT32, (void*)PROP_TAG_CONTAINERCONTENTS)) {
-			return FALSE;
-		}
-		if (FALSE == fastdownctx_object_record_messagelist(
-			pflow_list, pfldmsgs->pnormal_msglst)) {
-			return FALSE;
-		}
+		if (!record_tag(META_TAG_FXDELPROP) ||
+		    !record_tag(PROP_TAG_CONTAINERCONTENTS) ||
+		    !record_messagelist(pfldmsgs->pnormal_msglst))
+			return false;
 	}
-	return TRUE;
+	return true;
 }
 
-static BOOL fastdownctx_object_record_foldermessagesnodelprops(
-	DOUBLE_LIST *pflow_list, const FOLDER_MESSAGES *pfldmsgs)
+bool fxdown_flow_list::record_foldermessagesnodelprops(const FOLDER_MESSAGES *pfldmsgs)
 {
 	if (NULL != pfldmsgs->pfai_msglst) {
-		if (FALSE == fastdownctx_object_record_messagelist(
-			pflow_list, pfldmsgs->pfai_msglst)) {
-			return FALSE;
-		}
+		if (!record_messagelist(pfldmsgs->pfai_msglst))
+			return false;
 	}
 	if (NULL != pfldmsgs->pnormal_msglst) {
-		if (FALSE == fastdownctx_object_record_messagelist(
-			pflow_list, pfldmsgs->pnormal_msglst)) {
-			return FALSE;
-		}
+		if (!record_messagelist(pfldmsgs->pnormal_msglst))
+			return false;
 	}
-	return TRUE;
+	return true;
 }
 
-static BOOL fastdownctx_object_record_foldercontent(
-	DOUBLE_LIST *pflow_list, const FOLDER_CONTENT *pfldctnt)
+bool fxdown_flow_list::record_foldercontent(const FOLDER_CONTENT *pfldctnt)
 {
 	if (pfldctnt->proplist.has(META_TAG_NEWFXFOLDER))
-		return fastdownctx_object_record_flow_node(
-						pflow_list, FUNC_ID_PROPLIST,
-						(void*)&pfldctnt->proplist);
-	if (FALSE == fastdownctx_object_record_flow_node(
-		pflow_list, FUNC_ID_PROPLIST, (void*)&pfldctnt->proplist)) {
-		return FALSE;
-	}
-	if (FALSE == fastdownctx_object_record_foldermessages(
-		pflow_list, &pfldctnt->fldmsgs)) {
-		return FALSE;	
-	}
-	if (FALSE == fastdownctx_object_record_flow_node(pflow_list,
-		FUNC_ID_UINT32, (void*)META_TAG_FXDELPROP)) {
-		return FALSE;
-	}
-	if (FALSE == fastdownctx_object_record_flow_node(pflow_list,
-		FUNC_ID_UINT32, (void*)PROP_TAG_CONTAINERHIERARCHY)) {
-		return FALSE;
-	}
+		return record_node(FUNC_ID_PROPLIST, &pfldctnt->proplist);
+	if (!record_node(FUNC_ID_PROPLIST, &pfldctnt->proplist) ||
+	    !record_foldermessages(&pfldctnt->fldmsgs) ||
+	    !record_tag(META_TAG_FXDELPROP) ||
+	    !record_tag(PROP_TAG_CONTAINERHIERARCHY))
+		return false;
 	for (const auto &f : pfldctnt->psubflds)
-		if (!fastdownctx_object_record_subfolder(pflow_list, &f))
-			return FALSE;	
-	return TRUE;
+		if (!record_subfolder(&f))
+			return false;
+	return true;
 }
 
-static BOOL fastdownctx_object_record_foldercontentnodelprops(
-	DOUBLE_LIST *pflow_list, const FOLDER_CONTENT *pfldctnt)
+bool fxdown_flow_list::record_foldercontentnodelprops(const FOLDER_CONTENT *pfldctnt)
 {
-	if (FALSE == fastdownctx_object_record_flow_node(
-		pflow_list, FUNC_ID_PROPLIST, (void*)&pfldctnt->proplist)) {
-		return FALSE;
-	}
-	if (FALSE == fastdownctx_object_record_foldermessagesnodelprops(
-		pflow_list, &pfldctnt->fldmsgs)) {
-		return FALSE;
-	}
+	if (!record_node(FUNC_ID_PROPLIST, &pfldctnt->proplist) ||
+	    !record_foldermessagesnodelprops(&pfldctnt->fldmsgs))
+		return false;
 	for (const auto &f : pfldctnt->psubflds)
-		if (!fastdownctx_object_record_subfoldernodelprops(pflow_list, &f))
-			return FALSE;
-	return TRUE;
+		if (!record_subfoldernodelprops(&f))
+			return false;
+	return true;
 }
 
-static BOOL fastdownctx_object_record_subfoldernodelprops(
-	DOUBLE_LIST *pflow_list, const FOLDER_CONTENT *pfldctnt)
+bool fxdown_flow_list::record_subfoldernodelprops(const FOLDER_CONTENT *pfldctnt)
 {
-	if (FALSE == fastdownctx_object_record_flow_node(
-		pflow_list, FUNC_ID_UINT32, (void*)STARTSUBFLD)) {
-		return FALSE;
-	}
-	if (FALSE == fastdownctx_object_record_foldercontentnodelprops(
-		pflow_list, pfldctnt)) {
-		return FALSE;
-	}
-	if (FALSE == fastdownctx_object_record_flow_node(
-		pflow_list, FUNC_ID_UINT32, (void*)ENDFOLDER)) {
-		return FALSE;
-	}
-	return TRUE;
+	return record_tag(STARTSUBFLD) &&
+	       record_foldercontentnodelprops(pfldctnt) &&
+	       record_tag(ENDFOLDER);
 }
 
-static BOOL fastdownctx_object_record_subfolder(
-	DOUBLE_LIST *pflow_list, const FOLDER_CONTENT *pfldctnt)
+bool fxdown_flow_list::record_subfolder(const FOLDER_CONTENT *pfldctnt)
 {
-	if (FALSE == fastdownctx_object_record_flow_node(
-		pflow_list, FUNC_ID_UINT32, (void*)STARTSUBFLD)) {
-		return FALSE;
-	}
-	if (FALSE == fastdownctx_object_record_foldercontent(
-		pflow_list, pfldctnt)) {
-		return FALSE;
-	}
-	if (FALSE == fastdownctx_object_record_flow_node(
-		pflow_list, FUNC_ID_UINT32, (void*)ENDFOLDER)) {
-		return FALSE;
-	}
-	return TRUE;
+	return record_tag(STARTSUBFLD) && record_foldercontent(pfldctnt) &&
+	       record_tag(ENDFOLDER);
 }
 
 BOOL fastdownctx_object::make_messagecontent(MESSAGE_CONTENT *pmsgctnt)
@@ -234,91 +152,54 @@ BOOL fastdownctx_object::make_state(ICS_STATE *pstate)
 	return TRUE;
 }
 
+static bool is_message(const flow_node &n) { return n.first == FUNC_ID_MESSAGE; }
+
 BOOL fastdownctx_object::make_foldercontent(BOOL b_subfolders,
     std::unique_ptr<FOLDER_CONTENT> &&fc)
 {
 	auto pctx = this;
-	DOUBLE_LIST_NODE *pnode;
 	
-	if (!fastdownctx_object_record_flow_node(&pctx->flow_list,
-	    FUNC_ID_PROPLIST, &fc->proplist))
-		return FALSE;
-	if (!fastdownctx_object_record_foldermessages(&pctx->flow_list, &fc->fldmsgs))
+	if (!flow_list.record_node(FUNC_ID_PROPLIST, &fc->proplist) ||
+	    !flow_list.record_foldermessages(&fc->fldmsgs))
 		return FALSE;	
 	if (TRUE == b_subfolders) {
-		if (FALSE == fastdownctx_object_record_flow_node(
-			&pctx->flow_list, FUNC_ID_UINT32,
-			(void*)META_TAG_FXDELPROP)) {
+		if (!flow_list.record_tag(META_TAG_FXDELPROP) ||
+		    !flow_list.record_tag(PROP_TAG_CONTAINERHIERARCHY))
 			return FALSE;
-		}
-		if (FALSE == fastdownctx_object_record_flow_node(
-			&pctx->flow_list, FUNC_ID_UINT32,
-			(void*)PROP_TAG_CONTAINERHIERARCHY)) {
-			return FALSE;
-		}
 		for (const auto &f : fc->psubflds)
-			if (!fastdownctx_object_record_subfolder(&pctx->flow_list, &f))
+			if (!flow_list.record_subfolder(&f))
 				return FALSE;	
 	}
 	pctx->pfldctnt = std::move(fc);
 	pctx->progress_steps = 0;
-	pctx->total_steps = 0;
-	for (pnode=double_list_get_head(&pctx->flow_list); NULL!=pnode;
-		pnode=double_list_get_after(&pctx->flow_list, pnode)) {
-		if (FUNC_ID_MESSAGE == ((FAST_FLOW_NODE*)
-			pnode->pdata)->func_id) {
-			pctx->total_steps ++;
-		}
-	}
+	total_steps = std::count_if(flow_list.cbegin(), flow_list.cend(), is_message);
 	return TRUE;
 }
 	
 BOOL fastdownctx_object::make_topfolder(std::unique_ptr<FOLDER_CONTENT> &&fc)
 {
 	auto pctx = this;
-	DOUBLE_LIST_NODE *pnode;
 	
-	if (FALSE == fastdownctx_object_record_flow_node(
-		&pctx->flow_list, FUNC_ID_UINT32, (void*)STARTTOPFLD)) {
+	if (!flow_list.record_tag(STARTTOPFLD) ||
+	    !flow_list.record_foldercontentnodelprops(fc.get()) ||
+	    !flow_list.record_tag(ENDFOLDER))
 		return FALSE;
-	}
-	if (!fastdownctx_object_record_foldercontentnodelprops(&pctx->flow_list, fc.get()))
-		return FALSE;
-	if (FALSE == fastdownctx_object_record_flow_node(
-		&pctx->flow_list, FUNC_ID_UINT32, (void*)ENDFOLDER)) {
-		return FALSE;
-	}
 	pctx->pfldctnt = std::move(fc);
 	pctx->progress_steps = 0;
-	pctx->total_steps = 0;
-	for (pnode=double_list_get_head(&pctx->flow_list); NULL!=pnode;
-		pnode=double_list_get_after(&pctx->flow_list, pnode)) {
-		if (FUNC_ID_MESSAGE == ((FAST_FLOW_NODE*)
-			pnode->pdata)->func_id) {
-			pctx->total_steps ++;
-		}
-	}
+	total_steps = std::count_if(flow_list.cbegin(), flow_list.cend(), is_message);
 	return TRUE;
 }
 
 BOOL fastdownctx_object::make_messagelist(BOOL chginfo, EID_ARRAY *msglst)
 {
 	auto pctx = this;
-	DOUBLE_LIST_NODE *pnode;
 	
-	if (!fastdownctx_object_record_messagelist(&pctx->flow_list, msglst))
+	if (!flow_list.record_messagelist(msglst))
 		return FALSE;
 	pctx->b_chginfo = chginfo;
 	pctx->pmsglst = msglst;
 	pctx->progress_steps = 0;
-	pctx->total_steps = 0;
-	for (pnode=double_list_get_head(&pctx->flow_list); NULL!=pnode;
-		pnode=double_list_get_after(&pctx->flow_list, pnode)) {
-		if (FUNC_ID_MESSAGE == ((FAST_FLOW_NODE*)
-			pnode->pdata)->func_id) {
-			pctx->total_steps ++;
-		}
-	}
+	total_steps = std::count_if(flow_list.cbegin(), flow_list.cend(), is_message);
 	return TRUE;
 }
 
@@ -328,11 +209,9 @@ static BOOL fastdownctx_object_get_buffer_internal(fastdownctx_object *pctx,
 	BOOL b_last;
 	uint16_t len;
 	uint16_t len1;
-	FAST_FLOW_NODE *pflow;
-	DOUBLE_LIST_NODE *pnode;
 	MESSAGE_CONTENT *pmsgctnt;
 	
-	if (0 == double_list_get_nodes_num(&pctx->flow_list)) {
+	if (pctx->flow_list.size() == 0) {
 		if (!pctx->pstream->read_buffer(pbuff, plen, pb_last))
 			return FALSE;	
 		if (NULL == pctx->pmsglst && NULL == pctx->pfldctnt) {
@@ -353,38 +232,31 @@ static BOOL fastdownctx_object_get_buffer_internal(fastdownctx_object *pctx,
 		}
 	}
 	len1 = *plen - len;
-	while ((pnode = double_list_pop_front(&pctx->flow_list)) != nullptr) {
-		pflow = (FAST_FLOW_NODE*)pnode->pdata;
-		switch (pflow->func_id) {
+	while (pctx->flow_list.size() > 0) {
+		auto [func_id, param] = pctx->flow_list.front();
+		pctx->flow_list.pop_front();
+		switch (func_id) {
 		case FUNC_ID_UINT32:
-			if (!pctx->pstream->write_uint32(reinterpret_cast<uintptr_t>(pflow->pparam))) {
-				free(pnode->pdata);
+			if (!pctx->pstream->write_uint32(reinterpret_cast<uintptr_t>(param)))
 				return FALSE;
-			}
 			break;
 		case FUNC_ID_PROPLIST:
-			if (!pctx->pstream->write_proplist(static_cast<TPROPVAL_ARRAY *>(pflow->pparam))) {
-				free(pnode->pdata);
+			if (!pctx->pstream->write_proplist(static_cast<const TPROPVAL_ARRAY *>(param)))
 				return FALSE;
-			}
 			break;
 		case FUNC_ID_MESSAGE: {
 			auto pinfo = emsmdb_interface_get_emsmdb_info();
 			if (pctx->pstream->plogon->check_private()) {
 				if (!exmdb_client_read_message(pctx->pstream->plogon->get_dir(),
 				    nullptr, pinfo->cpid,
-				    *static_cast<uint64_t *>(pflow->pparam), &pmsgctnt)) {
-					free(pnode->pdata);
+				    *static_cast<const uint64_t *>(param), &pmsgctnt))
 					return FALSE;
-				}
 			} else {
 				auto rpc_info = get_rpc_info();
 				if (!exmdb_client_read_message(pctx->pstream->plogon->get_dir(),
 				    rpc_info.username, pinfo->cpid,
-				    *static_cast<uint64_t *>(pflow->pparam), &pmsgctnt)) {
-					free(pnode->pdata);
+				    *static_cast<const uint64_t *>(param), &pmsgctnt))
 					return FALSE;
-				}
 			}
 			if (NULL == pmsgctnt) {
 				continue;
@@ -407,24 +279,21 @@ static BOOL fastdownctx_object_get_buffer_internal(fastdownctx_object *pctx,
 					PR_ENTRYID, PR_ORIGINAL_ENTRYID);
 			}
 			if (!pctx->pstream->write_message(pmsgctnt)) {
-				free(pnode->pdata);
 				return FALSE;
 			}
 			pctx->progress_steps ++;
 			break;
 		}
 		default:
-			free(pnode->pdata);
 			return FALSE;
 		}
-		free(pnode->pdata);
 		if (pctx->pstream->total_length() > len1)
 			break;
 	}
 	if (!pctx->pstream->read_buffer(static_cast<char *>(pbuff) + len, &len1, &b_last))
 		return FALSE;
 	*plen = len + len1;
-	*pb_last = double_list_get_nodes_num(&pctx->flow_list) == 0 && b_last ? TRUE : false;
+	*pb_last = pctx->flow_list.size() == 0 && b_last ? TRUE : false;
 	return TRUE;
 }
 
@@ -461,7 +330,6 @@ fastdownctx_object::create(logon_object *plogon, uint8_t string_option)
 	pctx->pstream = ftstream_producer::create(plogon, string_option);
 	if (pctx->pstream == nullptr)
 		return NULL;
-	double_list_init(&pctx->flow_list);
 	pctx->pmsglst = NULL;
 	return pctx;
 }
@@ -469,11 +337,6 @@ fastdownctx_object::create(logon_object *plogon, uint8_t string_option)
 fastdownctx_object::~fastdownctx_object()
 {
 	auto pctx = this;
-	DOUBLE_LIST_NODE *pnode;
-	
-	while ((pnode = double_list_pop_front(&pctx->flow_list)) != nullptr)
-		free(pnode->pdata);
-	double_list_free(&pctx->flow_list);
 	if (NULL != pctx->pmsglst) {
 		eid_array_free(pctx->pmsglst);
 	}
--- exch/emsmdb/fastdownctx_object.h
+++ exch/emsmdb/fastdownctx_object.h
@@ -1,14 +1,29 @@
 #pragma once
 #include <cstdint>
+#include <list>
 #include <memory>
 #include <gromox/mapi_types.hpp>
 
 struct ATTACHMENT_CONTENT;
 struct FOLDER_CONTENT;
+struct FOLDER_MESSAGES;
 struct FTSTREAM_PRODUCER;
 struct ICS_STATE;
 struct logon_object;
 struct MESSAGE_CONTENT;
+using flow_node = std::pair<uint8_t, const void *>;
+
+struct fxdown_flow_list : public std::list<flow_node> {
+	bool record_node(uint8_t, const void * = nullptr);
+	bool record_tag(uint32_t);
+	bool record_messagelist(EID_ARRAY *);
+	bool record_foldermessages(const FOLDER_MESSAGES *);
+	bool record_foldermessagesnodelprops(const FOLDER_MESSAGES *);
+	bool record_foldercontent(const FOLDER_CONTENT *);
+	bool record_foldercontentnodelprops(const FOLDER_CONTENT *);
+	bool record_subfoldernodelprops(const FOLDER_CONTENT *);
+	bool record_subfolder(const FOLDER_CONTENT *);
+};
 
 struct fastdownctx_object final {
 	protected:
@@ -31,6 +46,6 @@ struct fastdownctx_object final {
 	BOOL b_back = false, b_last = false, b_chginfo = false;
 	EID_ARRAY *pmsglst = nullptr;
 	std::unique_ptr<FOLDER_CONTENT> pfldctnt;
-	DOUBLE_LIST flow_list{};
+	fxdown_flow_list flow_list;
 	uint32_t total_steps = 0, progress_steps = 0;
 };
