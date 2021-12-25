211377efd73b0e6503e0d628f1bc61f647994750
C. M. Palmerston <cmp@gromox.com>
2021-12-25 15:18:06 +0100

    adaptor: delete daemon
    alias_resolve(4gx) basically does the same as adaptor(8gx), namely periodically
    downloading data from MySQL. Even if adaptor(8gx) used to historically service
    two daemons (delivery & delivery-queue/smtp), that does not really justify the
    extra daemon.

--- tools/adaptor/engine.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <gromox/common_types.hpp>
-extern void engine_init(const char *domainlist_path, const char *aliasaddress_path);
-extern int engine_run();
-extern void engine_stop();
-extern void engine_trig();
