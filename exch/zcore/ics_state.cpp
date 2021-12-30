99d299d5e4af3e10b72475ca0fe51b314a9871a5
Jan Engelhardt <jengelh@inai.de>
2021-12-30 20:46:06 +0100

    zcore: automatic memory release within ics_state_serialize

--- exch/zcore/ics_state.cpp
+++ exch/zcore/ics_state.cpp
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only WITH linking exception
 #include <cstdint>
+#include <memory>
 #include "common_util.h"
 #include <gromox/ext_buffer.hpp>
 #include "ics_state.h"
@@ -75,8 +76,11 @@ void ics_state_free(ICS_STATE *pstate)
 
 BINARY* ics_state_serialize(ICS_STATE *pstate)
 {
+	struct mdel {
+		inline void operator()(BINARY *x) { rop_util_free_binary(x); }
+		inline void operator()(TPROPVAL_ARRAY *x) { tpropval_array_free(x); }
+	};
 	EXT_PUSH ext_push;
-	TPROPVAL_ARRAY *pproplist;
 	static constexpr uint8_t bin_buff[8]{};
 	static constexpr BINARY fake_bin = {gromox::arsizeof(bin_buff), {deconst(bin_buff)}};
 	
@@ -91,70 +95,35 @@ BINARY* ics_state_serialize(ICS_STATE *pstate)
 		    pstate->pseen->check_empty())
 			return deconst(&fake_bin);
 	}
-	pproplist = tpropval_array_init();
+	std::unique_ptr<TPROPVAL_ARRAY, mdel> pproplist(tpropval_array_init());
 	if (NULL == pproplist) {
 		return NULL;
 	}
-	
-	auto pbin = pstate->pgiven->serialize();
-	if (NULL == pbin) {
-		tpropval_array_free(pproplist);
+	std::unique_ptr<BINARY, mdel> ser(pstate->pgiven->serialize());
+	if (ser == nullptr || pproplist->set(MetaTagIdsetGiven1, ser.get()) != 0)
 		return NULL;
-	}
-	if (pproplist->set(MetaTagIdsetGiven1, pbin) != 0) {
-		rop_util_free_binary(pbin);
-		tpropval_array_free(pproplist);
+	ser.reset(pstate->pseen->serialize());
+	if (ser == nullptr || pproplist->set(MetaTagCnsetSeen, ser.get()) != 0)
 		return NULL;
-	}
-	rop_util_free_binary(pbin);
-	
-	pbin = pstate->pseen->serialize();
-	if (NULL == pbin) {
-		tpropval_array_free(pproplist);
-		return NULL;
-	}
-	if (pproplist->set(MetaTagCnsetSeen, pbin) != 0) {
-		rop_util_free_binary(pbin);
-		tpropval_array_free(pproplist);
-		return NULL;
-	}
-	rop_util_free_binary(pbin);
+	ser.reset();
 	
 	if (ICS_TYPE_CONTENTS == pstate->type) {
-		pbin = pstate->pseen_fai->serialize();
-		if (NULL == pbin) {
-			tpropval_array_free(pproplist);
+		decltype(ser) s(pstate->pseen_fai->serialize());
+		if (s == nullptr || pproplist->set(MetaTagCnsetSeenFAI, s.get()) != 0)
 			return NULL;
-		}
-		if (pproplist->set(MetaTagCnsetSeenFAI, pbin) != 0) {
-			rop_util_free_binary(pbin);
-			tpropval_array_free(pproplist);
-			return NULL;
-		}
-		rop_util_free_binary(pbin);
 	}
 	
 	if (ICS_TYPE_CONTENTS == pstate->type &&
 	    !pstate->pread->check_empty()) {
-		pbin = pstate->pread->serialize();
-		if (NULL == pbin) {
-			tpropval_array_free(pproplist);
+		decltype(ser) s(pstate->pread->serialize());
+		if (s == nullptr || pproplist->set(MetaTagCnsetRead, s.get()) != 0)
 			return NULL;
-		}
-		if (pproplist->set(MetaTagCnsetRead, pbin) != 0) {
-			rop_util_free_binary(pbin);
-			tpropval_array_free(pproplist);
-			return NULL;
-		}
-		rop_util_free_binary(pbin);
 	}
 	if (!ext_push.init(nullptr, 0, 0) ||
-	    ext_push.p_tpropval_a(pproplist) != EXT_ERR_SUCCESS) {
-		tpropval_array_free(pproplist);
+	    ext_push.p_tpropval_a(pproplist.get()) != EXT_ERR_SUCCESS)
 		return NULL;
-	}
-	tpropval_array_free(pproplist);
-	pbin = cu_alloc<BINARY>();
+	pproplist.reset();
+	auto pbin = cu_alloc<BINARY>();
 	pbin->cb = ext_push.m_offset;
 	pbin->pv = common_util_alloc(pbin->cb);
 	if (pbin->pv == nullptr) {
