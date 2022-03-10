b86de4a55d940ff14b1d08504b5d6db34d96818c
Jan Engelhardt <jengelh@inai.de>
2022-03-10 01:40:50 +0100

    service: emit log messages to stderr only

--- exch/http/service.cpp
+++ exch/http/service.cpp
@@ -159,7 +159,7 @@ static int service_load_library(const char *path)
 	auto it = std::find_if(g_list_plug.cbegin(), g_list_plug.cend(),
 	          [&](const SVC_PLUG_ENTITY &p) { return p.file_name == path; });
 	if (it != g_list_plug.cend()) {
-		printf("[service]: %s is already loaded by service module\n", path);
+		fprintf(stderr, "[service]: %s is already loaded by service module\n", path);
 		return PLUGIN_ALREADY_LOADED;
 	}
 	SVC_PLUG_ENTITY plug;
@@ -167,15 +167,15 @@ static int service_load_library(const char *path)
 	if (plug.handle == nullptr && strchr(path, '/') == nullptr)
 		plug.handle = dlopen((g_init_path + "/"s + path).c_str(), RTLD_LAZY);
 	if (plug.handle == nullptr) {
-		printf("[service]: error loading %s: %s\n", fake_path, dlerror());
-		printf("[service]: the plugin %s is not loaded\n", fake_path);
+		fprintf(stderr, "[service]: error loading %s: %s\n", fake_path, dlerror());
+		fprintf(stderr, "[service]: the plugin %s is not loaded\n", fake_path);
 		return PLUGIN_FAIL_OPEN;
 	}
 	plug.lib_main = reinterpret_cast<decltype(plug.lib_main)>(dlsym(plug.handle, "SVC_LibMain"));
 	if (plug.lib_main == nullptr) {
-		printf("[service]: error finding the SVC_LibMain function in %s\n",
+		fprintf(stderr, "[service]: error finding the SVC_LibMain function in %s\n",
 				fake_path);
-		printf("[service]: the plugin %s is not loaded\n", fake_path);
+		fprintf(stderr, "[service]: the plugin %s is not loaded\n", fake_path);
 		return PLUGIN_NO_MAIN;
 	}
 	plug.file_name = path;
@@ -206,11 +206,11 @@ SVC_PLUG_ENTITY::~SVC_PLUG_ENTITY()
 	PLUGIN_MAIN func;
 	auto plib = this;
 	if (plib->ref_count > 0) {
-		printf("Unbalanced refcount on %s\n", plib->file_name.c_str());
+		fprintf(stderr, "Unbalanced refcount on %s\n", plib->file_name.c_str());
 		return;
 	}
 	if (plib->file_name.size() > 0)
-		printf("[service]: unloading %s\n", plib->file_name.c_str());
+		fprintf(stderr, "[service]: unloading %s\n", plib->file_name.c_str());
 	func = (PLUGIN_MAIN)plib->lib_main;
 	if (func != nullptr && plib->completed_init)
 		/* notify the plugin that it will be unloaded */
@@ -346,7 +346,7 @@ void *service_query(const char *service_name, const char *module, const std::typ
 			"ip_filter_add", "ip_filter_judge", "ndr_stack_alloc"};
 		if (std::none_of(excl, &excl[GX_ARRAY_SIZE(excl)],
 		    [&](const char *s) { return strcmp(service_name, s) == 0; }))
-			printf("[service]: dlname \"%s\" not found\n", service_name);
+			fprintf(stderr, "[service]: dlname \"%s\" not found\n", service_name);
 		return NULL;
 	}
 	auto &pservice = *node;
