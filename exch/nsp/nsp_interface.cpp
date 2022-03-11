e00b036732600d9aeb95b8525a91d8cd510ecf78
Jan Engelhardt <jengelh@inai.de>
2022-03-11 01:01:19 +0100

    nsp: remove EXIT gotos from nsp_interface_seek_entries

--- exch/nsp/nsp_interface.cpp
+++ exch/nsp/nsp_interface.cpp
@@ -1080,8 +1080,8 @@ int nsp_interface_seek_entries(NSPI_HANDLE handle, uint32_t reserved,
 			prow = common_util_proprowset_enlarge(*pprows);
 			if (NULL == prow || NULL ==
 				common_util_propertyrow_init(prow)) {
-				result = ecMAPIOOM;
-				goto EXIT_SEEK_ENTRIES;
+				*pprows = nullptr;
+				return ecMAPIOOM;
 			}
 			result = nsp_interface_fetch_row(pnode1, TRUE,
 			         pstat->codepage, pproptags, prow);
@@ -1090,13 +1090,14 @@ int nsp_interface_seek_entries(NSPI_HANDLE handle, uint32_t reserved,
 		}
 		
 		if (0 == tmp_minid) {
-			result = ecNotFound;
-			goto EXIT_SEEK_ENTRIES;
+			*pprows = nullptr;
+			return ecNotFound;
 		}
 		
 		pstat->total_rec = (*pprows)->crows;
 		pstat->cur_rec = tmp_minid;
 		pstat->num_pos = row;
+		return ecSuccess;
 	} else {
 		const SIMPLE_TREE_NODE *pnode = nullptr;
 		if (0 == pstat->container_id) {
@@ -1105,21 +1106,21 @@ int nsp_interface_seek_entries(NSPI_HANDLE handle, uint32_t reserved,
 		} else {
 			pnode = ab_tree_minid_to_node(pbase.get(), pstat->container_id);
 			if (NULL == pnode) {
-				result = ecInvalidBookmark;
-				goto EXIT_SEEK_ENTRIES;
+				*pprows = nullptr;
+				return ecInvalidBookmark;
 			}
 			nsp_interface_position_in_table(pstat,
 				pnode, &start_pos, &last_row, &total);
 			auto pnode1 = pnode->get_child();
 			if (NULL == pnode1) {
-				result = ecNotFound;
-				goto EXIT_SEEK_ENTRIES;
+				*pprows = nullptr;
+				return ecNotFound;
 			}
 		}
 		
 		if (0 == total) {
-			result = ecNotFound;
-			goto EXIT_SEEK_ENTRIES;
+			*pprows = nullptr;
+			return ecNotFound;
 		}
 		size_t row = 0;
 		if (0 == pstat->container_id) {
@@ -1132,20 +1133,20 @@ int nsp_interface_seek_entries(NSPI_HANDLE handle, uint32_t reserved,
 				prow = common_util_proprowset_enlarge(*pprows);
 				if (NULL == prow ||
 				    NULL == common_util_propertyrow_init(prow)) {
-					result = ecMAPIOOM;
-					goto EXIT_SEEK_ENTRIES;
+					*pprows = nullptr;
+					return ecMAPIOOM;
 				}
 				if (nsp_interface_fetch_row(ptr,
 				    TRUE, pstat->codepage, pproptags,
 				    prow) != ecSuccess) {
-					result = ecError;
-					goto EXIT_SEEK_ENTRIES;
+					*pprows = nullptr;
+					return ecError;
 				}
 				break;
 			}
 			if (row == pbase->gal_list.size()) {
-				result = ecNotFound;
-				goto EXIT_SEEK_ENTRIES;
+				*pprows = nullptr;
+				return ecNotFound;
 			}
 			pstat->cur_rec = ab_tree_get_node_minid(pbase->gal_list[row]);
 		} else {
@@ -1166,32 +1167,26 @@ int nsp_interface_seek_entries(NSPI_HANDLE handle, uint32_t reserved,
 				prow = common_util_proprowset_enlarge(*pprows);
 				if (NULL == prow ||
 				    NULL == common_util_propertyrow_init(prow)) {
-					result = ecMAPIOOM;
-					goto EXIT_SEEK_ENTRIES;
+					*pprows = nullptr;
+					return ecMAPIOOM;
 				}
 				if (nsp_interface_fetch_row(pnode1,
 				    TRUE, pstat->codepage, pproptags,
 				    prow) != ecSuccess) {
-					result = ecError;
-					goto EXIT_SEEK_ENTRIES;
+					*pprows = nullptr;
+					return ecError;
 				}
 				break;
 			} while ((pnode1 = pnode1->get_sibling()) != nullptr);
 			if (NULL == pnode1) {
-				result = ecNotFound;
-				goto EXIT_SEEK_ENTRIES;
+				*pprows = nullptr;
+				return ecNotFound;
 			}
 			pstat->cur_rec = ab_tree_get_node_minid(pnode1);
 		}
 		pstat->num_pos = row;
+		return ecSuccess;
 	}
-	
-	result = ecSuccess;
-
- EXIT_SEEK_ENTRIES:
-	if (result != ecSuccess)
-		*pprows = NULL;
-	return result;
 }
 
 static BOOL nsp_interface_match_node(const SIMPLE_TREE_NODE *pnode,
