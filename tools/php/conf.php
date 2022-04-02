a202e33a1bb5b8fb9e30169c81163bbbf3f9d439
Victor Tesla <victor.tesla@grommunio.com>
2022-04-02 15:39:38 +0200

    delayed-submit: replace read of sa.cfg by mysql_adaptor.cfg
    sa.cfg has not existed in a long time.

--- tools/php/conf.php
+++ tools/php/conf.php
@@ -7,7 +7,7 @@ function get_athena_config()
 	if ($sysconf) {
 		return $sysconf;
 	}
-	$sysconf = parse_ini_file("/etc/gromox/sa.cfg", false, INI_SCANNER_RAW);
+	$sysconf = parse_ini_file("/etc/gromox/mysql_adaptor.cfg", false, INI_SCANNER_RAW);
 	if (!isset($sysconf)) {
 		die("cannot find config.ini file");
 	}
