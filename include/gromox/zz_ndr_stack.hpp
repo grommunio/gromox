cf0e73e37a5580c0d0fb5b6149daa450338f1610
François Châteauneuf <fcn@gromox.com>
2022-04-03 03:25:57 +0200

    lib: delete unused lines from zz_ndr_stack.hpp

--- include/gromox/zz_ndr_stack.hpp
+++ include/gromox/zz_ndr_stack.hpp
@@ -5,5 +5,3 @@
  */
 template<typename T> T *ndr_stack_anew(unsigned int dir) { return static_cast<T *>(ndr_stack_alloc(dir, sizeof(T))); }
 template<typename T> T *ndr_stack_anew(unsigned int dir, size_t elem) { return static_cast<T *>(ndr_stack_alloc(dir, sizeof(T) * elem)); }
-//template<typename T> static inline T *ndr_stack_anew(unsigned int dir) { return static_cast<T *>(ndr_stack_alloc(dir, sizeof(T))); }
-//template<typename T> static inline T *ndr_stack_anew(unsigned int dir, size_t n) { return static_cast<T *>(ndr_stack_alloc(dir, n * sizeof(T))); }
