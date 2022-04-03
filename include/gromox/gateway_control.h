d5dbe80847c4c354b2db42181a75f002e1f79db9
François Châteauneuf <fcn@gromox.com>
2022-04-03 03:14:20 +0200

    lib: delete unused gateway_control.h

--- include/gromox/gateway_control.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <gromox/common_types.hpp>
-#include <gromox/defs.h>
-#define NOTIFY_DELIVERY		0x00000002
-
-void gateway_control_init(const char *path);
-extern int gateway_control_run();
-void gateway_control_notify(const char *command, int control_mask);
