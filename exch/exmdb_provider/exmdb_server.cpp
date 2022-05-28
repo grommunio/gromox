6b707a35ade2a14833fdc4d16879d7869547ac2c
Jan Engelhardt <jengelh@inai.de>
2022-05-28 22:02:10 +0200

    exmdb_provider: automatic memory management for g_env_key

--- exch/exmdb_provider/exmdb_server.cpp
+++ exch/exmdb_provider/exmdb_server.cpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only WITH linking exception
+#include <memory>
 #include <gromox/svc_common.h>
 #include <gromox/util.hpp>
 #include "common_util.h"
@@ -19,8 +20,15 @@ using ENVIRONMENT_CONTEXT = env_context;
 
 static thread_local const char *g_id_key;
 static thread_local const char *g_public_username_key;
-static thread_local ENVIRONMENT_CONTEXT *g_env_key;
 static alloc_limiter<ENVIRONMENT_CONTEXT> g_ctx_allocator;
+
+namespace {
+struct envctx_delete {
+	void operator()(env_context *x) const { g_ctx_allocator->put(x); }
+};
+}
+
+static thread_local std::unique_ptr<env_context, envctx_delete> g_env_key;
 
 void (*exmdb_server_event_proc)(const char *dir,
 	BOOL b_table, uint32_t notify_id, const DB_NOTIFY *pdb_notify);
@@ -34,21 +42,17 @@ int exmdb_server_run()
 void exmdb_server_build_env(unsigned int flags, const char *dir)
 {
 	common_util_build_tls();
-	auto pctx = g_ctx_allocator.get();
+	std::unique_ptr<env_context, envctx_delete> pctx(g_ctx_allocator.get());
 	pctx->b_local = flags & EM_LOCAL;
 	pctx->b_private = flags & EM_PRIVATE;
 	pctx->dir = dir;
 	pctx->account_id = -1;
-	g_env_key = pctx;
+	g_env_key = std::move(pctx);
 }
 
 void exmdb_server_free_environment()
 {
-	auto pctx = g_env_key;
-	if (pctx == nullptr)
-		return;
-	g_env_key = nullptr;
-	g_ctx_allocator.put(pctx);
+	g_env_key.reset();
 }
 
 void exmdb_server_set_remote_id(const char *remote_id)
@@ -58,7 +62,7 @@ void exmdb_server_set_remote_id(const char *remote_id)
 
 ALLOC_CONTEXT* exmdb_server_get_alloc_context()
 {
-	auto pctx = g_env_key;
+	auto pctx = g_env_key.get();
 	if (pctx == nullptr || pctx->b_local)
 		return NULL;
 	return &pctx->alloc_ctx;
@@ -84,31 +88,28 @@ const char* exmdb_server_get_public_username()
 	invoking exmdb_server_build_environment before! */
 BOOL exmdb_server_check_private()
 {
-	auto pctx = g_env_key;
-	return pctx->b_private;
+	return g_env_key->b_private;
 }
 
 /* can not be called in local rpc thread without
 	invoking exmdb_server_build_environment before! */
 const char* exmdb_server_get_dir()
 {
-	auto pctx = g_env_key;
-	return pctx->dir;
+	return g_env_key->dir;
 }
 
 /* can not be called in local rpc thread without
 	invoking exmdb_server_build_environment before! */
 void exmdb_server_set_dir(const char *dir)
 {
-	auto pctx = g_env_key;
-	pctx->dir = dir;
+	g_env_key->dir = dir;
 }
 
 int exmdb_server_get_account_id()
 {
 	int account_id;
 	
-	auto pctx = g_env_key;
+	auto pctx = g_env_key.get();
 	if (pctx->account_id < 0) {
 		if (pctx->b_private) {
 			if (common_util_get_id_from_maildir(pctx->dir, &account_id))
@@ -123,7 +124,7 @@ int exmdb_server_get_account_id()
 
 const GUID* exmdb_server_get_handle()
 {
-	auto pctx = g_env_key;
+	auto pctx = g_env_key.get();
 	if (pctx == nullptr || !pctx->b_local)
 		return NULL;
 	return common_util_get_handle();
