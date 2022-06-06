4ec730b71fc73eca616333c686595dbb98a90339
François Châteauneuf <fcn@gromox.com>
2022-06-06 23:16:08 +0200

    user_filter: switch to stdlib containers

--- exch/user_filter/temp_list.cpp
+++ exch/user_filter/temp_list.cpp
@@ -3,9 +3,10 @@
 #include <ctime>
 #include <fcntl.h>
 #include <mutex>
+#include <string>
 #include <unistd.h>
+#include <unordered_map>
 #include <libHX/string.h>
-#include <gromox/str_hash.hpp>
 #include <gromox/util.hpp>
 #include "grey_list.h"
 #include "str_filter.h"
@@ -14,7 +15,7 @@
 static size_t temp_list_collect_string_entry();
 
 /* private global variable */
-static std::unique_ptr<STR_HASH_TABLE> g_string_hash;
+static std::unordered_map<std::string, time_t> g_string_hash;
 static std::mutex g_string_mutex_lock;
 static size_t g_size;
 static BOOL				g_case_sensitive;
@@ -27,14 +28,8 @@ void temp_list_init(BOOL case_sensitive, size_t size)
 
 int temp_list_run()
 {
-	if (g_size <= 0) {
-		g_string_hash = NULL;
-		return 0;
-	}
-	g_string_hash = STR_HASH_TABLE::create(g_size, sizeof(time_t), nullptr);
-	if (NULL == g_string_hash) {
-		return -1;
-	}
+	if (g_size <= 0)
+		g_string_hash.clear();
 	return 0;
 }
 
@@ -59,9 +54,8 @@ BOOL temp_list_add_string(const char *str, int interval)
 	time_t when;
 	char temp_string[256];
 
-	if (NULL == g_string_hash || NULL == str) {
+	if (str == nullptr)
 		return FALSE;
-	}
 	strncpy(temp_string, str, sizeof(temp_string));
 	temp_string[sizeof(temp_string) - 1] = '\0';
 	if (!g_case_sensitive)
@@ -72,12 +66,24 @@ BOOL temp_list_add_string(const char *str, int interval)
 	std::lock_guard sm_hold(g_string_mutex_lock);
 	time(&current_time);
 	when = current_time + interval;
-	if (g_string_hash->add(temp_string, &when) > 0)
-		return TRUE;
+	try {
+		if (g_string_hash.size() >= g_size) {
+			auto pair = g_string_hash.emplace(temp_string, when);
+			if (pair.second)
+				return TRUE;
+		}
+	} catch (const std::bad_alloc &) {
+	}
 	if (0 == temp_list_collect_string_entry()) {
 		return FALSE;
 	}
-	return g_string_hash->add(temp_string, &when) > 0 ? TRUE : false;
+	if (g_string_hash.size() >= g_size) try {
+		auto pair = g_string_hash.emplace(temp_string, when);
+		if (pair.second)
+			return TRUE;
+	} catch (const std::bad_alloc &) {
+	}
+	return false;
 }
 
 /*
@@ -92,17 +98,18 @@ BOOL temp_list_remove_string(const char *str)
 {
 	char temp_string[256];
 	
-	if (NULL == g_string_hash || NULL == str) {
+	if (str == nullptr)
 		return TRUE;
-	}
 	strncpy(temp_string, str, sizeof(temp_string));
 	temp_string[sizeof(temp_string) - 1] = '\0';
 	if (!g_case_sensitive)
 		HX_strlower(temp_string);
 	std::lock_guard sm_hold(g_string_mutex_lock);
-	if (g_string_hash->query1(temp_string) == nullptr)
+	auto iter = g_string_hash.find(temp_string);
+	if (iter == g_string_hash.end())
 		return TRUE;
-	return g_string_hash->remove(temp_string) == 1 ? TRUE : false;
+	g_string_hash.erase(temp_string);
+	return TRUE;
 }
 
 /*
@@ -119,24 +126,20 @@ BOOL temp_list_query(const char *str)
 	time_t current_time;
 	char temp_string[256];
 	
-	if (NULL == g_string_hash || NULL == str)	{
+	if (str == nullptr)
 		return FALSE;
-	}
 	strncpy(temp_string, str, sizeof(temp_string));
 	temp_string[sizeof(temp_string) - 1] = '\0';
 	if (!g_case_sensitive)
 		HX_strlower(temp_string);
 	std::lock_guard sm_hold(g_string_mutex_lock);
-	auto pwhen = g_string_hash->query<time_t>(temp_string);
-	if (NULL == pwhen) {
+	auto iter = g_string_hash.find(temp_string);
+	if (iter == g_string_hash.end())
 		return FALSE; /* not found */
-	}
-	
 	time(&current_time);
-	if (current_time <= *pwhen) {
+	if (current_time <= iter->second)
 		return TRUE; /* found, in temp list */
-	}
-	g_string_hash->remove(temp_string);
+	g_string_hash.erase(temp_string);
 	return FALSE; /* is overdue */
 }
 
@@ -148,22 +151,23 @@ BOOL temp_list_query(const char *str)
  */
 static size_t temp_list_collect_string_entry()
 {
-	time_t *pwhen;
 	time_t current_time;
 
 	time(&current_time);
+#if __cplusplus >= 202000L
+	return std::erase_if(g_string_hash, [&](auto &&e) { return current_time > e.second; });
+#else
 	size_t collected_num = 0;
-	auto iter = g_string_hash->make_iter();
-	for (str_hash_iter_begin(iter); !str_hash_iter_done(iter);
-		str_hash_iter_forward(iter)) {
-		pwhen = (time_t*)str_hash_iter_get_value(iter, NULL);
-		if (current_time > *pwhen) {
-			str_hash_iter_remove(iter);
+	for (auto iter = g_string_hash.begin(); iter != g_string_hash.end(); ) {
+		if (current_time > iter->second) {
+			iter = g_string_hash.erase(iter);
 			collected_num++;
+		} else {
+			++iter;
 		}
 	}
-	str_hash_iter_free(iter);
 	return collected_num;
+#endif
 }
 
 /*
@@ -179,9 +183,8 @@ BOOL temp_list_echo(const char *str, time_t *puntil)
 {
 	char temp_string[256];
 
-	if (NULL == g_string_hash || NULL == str || NULL == puntil) {
+	if (str == nullptr || puntil == nullptr)
 		return FALSE;
-	}
 	strncpy(temp_string, str, sizeof(temp_string));
 	temp_string[sizeof(temp_string) - 1] = '\0';
 	if (!g_case_sensitive)
@@ -189,11 +192,10 @@ BOOL temp_list_echo(const char *str, time_t *puntil)
 	std::lock_guard sm_hold(g_string_mutex_lock);
 	/* first remove the overdue items */
 	temp_list_collect_string_entry();
-	auto pwhen = g_string_hash->query<time_t>(temp_string);
-	if (NULL == pwhen) {
+	auto iter = g_string_hash.find(temp_string);
+	if (iter == g_string_hash.end())
 		return FALSE;
-	}
-	*puntil = *pwhen;
+	*puntil = iter->second;
 	return TRUE;
 }
 
@@ -204,9 +206,8 @@ BOOL temp_list_dump(const char *path)
 	struct tm time_buff;
 	time_t current_time;
 
-	if (NULL == g_string_hash || NULL == path) {
+	if (path == nullptr)
 		return FALSE;
-	}
 	fd = open(path, O_WRONLY|O_CREAT|O_TRUNC, DEF_MODE);
 	if (-1 == fd) {
 		return FALSE;
@@ -214,24 +215,20 @@ BOOL temp_list_dump(const char *path)
 
 	std::unique_lock sm_hold(g_string_mutex_lock);
 	time(&current_time);
-	auto iter = g_string_hash->make_iter();
-	for (str_hash_iter_begin(iter); !str_hash_iter_done(iter);
-		str_hash_iter_forward(iter)) {
-		auto pwhen = static_cast<time_t *>(str_hash_iter_get_value(iter, temp_string));
-		if (current_time >= *pwhen) {
-			str_hash_iter_remove(iter);
+	for (auto iter = g_string_hash.begin(); iter != g_string_hash.end(); ) {
+		if (current_time >= iter->second) {
+			iter = g_string_hash.erase(iter);
 		} else {
 			len = strlen(temp_string);
 			temp_string[len] = '\t';
 			len ++;
 			len += strftime(temp_string + len, 512 - len, "%Y/%m/%d %H:%M:%S",
-					localtime_r(pwhen, &time_buff));
+			       localtime_r(&iter->second, &time_buff));
 			temp_string[len] = '\n';
 			len ++;
 			write(fd, temp_string, len);
 		}
 	}
-	str_hash_iter_free(iter);
 	sm_hold.unlock();
 	close(fd);
 	return TRUE;
