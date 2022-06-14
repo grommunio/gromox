4a3303a6cb5d51c6eb0873ccb6a48bb1068689e8
François Châteauneuf <fcn@gromox.com>
2022-06-14 05:41:56 +0200

    lib: remove unused source files system_log*

--- include/gromox/system_log.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <gromox/defs.h>
-
-void system_log_init(const char *path);
-extern int system_log_run();
-extern void system_log_info(const char *format, ...);
-extern void system_log_stop();
-extern void system_log_free();
