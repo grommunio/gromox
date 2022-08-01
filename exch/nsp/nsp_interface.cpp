04db7a5a1fba2bdc24a8c5637d73733af7a4ec6d
Jan Engelhardt <jengelh@inai.de>
2022-08-01 22:16:16 +0200

    nsp: show DT_DISTLIST icon for mlists
    Fixes: gromox-0~666
    References: GXL-293

--- exch/nsp/nsp_interface.cpp
+++ exch/nsp/nsp_interface.cpp
@@ -168,11 +168,14 @@ static uint32_t nsp_interface_fetch_property(const SIMPLE_TREE_NODE *pnode,
 		                 MAPI_MAILUSER;
 		return ecSuccess;
 	case PR_DISPLAY_TYPE:
+		/* The PR_DISPLAY_TYPE value in MariaDB is not directly used */
 		pprop->value.l = node_type == abnode_type::mlist ? DT_DISTLIST : DT_MAILUSER;
 		return ecSuccess;
 	case PR_DISPLAY_TYPE_EX:
+		/* The PR_DISPLAY_TYPE_EX value in MariaDB is not directly used */
 		pprop->value.l = node_type == abnode_type::room ? DT_ROOM :
 		                 node_type == abnode_type::equipment ? DT_EQUIPMENT :
+		                 node_type == abnode_type::mlist ? DT_DISTLIST :
 		                 DT_MAILUSER | DTE_FLAG_ACL_CAPABLE;
 		return ecSuccess;
 	case PR_MAPPING_SIGNATURE:
