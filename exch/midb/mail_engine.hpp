c10a74c7659d9887bb5ee233b78b0eea83a0a584
Jan Engelhardt <jengelh@inai.de>
2022-08-15 13:41:06 +0200

    midb: make midb_cache_interval/midb_reload_interval reloadable

--- exch/midb/mail_engine.hpp
+++ exch/midb/mail_engine.hpp
@@ -6,8 +6,9 @@ enum {
 	MIDB_UPGRADE_AUTO,
 };
 
-extern void mail_engine_init(const char *dfl_cset, const char *org_name, size_t table_size, BOOL async, BOOL wal, uint64_t mmap_size, int cache_interval, int mime_num);
+extern void mail_engine_init(const char *dfl_cset, const char *org_name, size_t table_size, BOOL async, BOOL wal, uint64_t mmap_size, int mime_num);
 extern int mail_engine_run();
 extern void mail_engine_stop();
 
 extern unsigned int g_midb_schema_upgrades;
+extern unsigned int g_midb_cache_interval, g_midb_reload_interval;
