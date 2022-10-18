f962774136fbb4de39f3f0e9c7447315c49e37cd
Jan Engelhardt <jengelh@inai.de>
2022-10-18 21:27:30 +0200

    nsp: dump properties at end of nsp_interface_get_props RPC

--- exch/nsp/nsp_interface.cpp
+++ exch/nsp/nsp_interface.cpp
@@ -1935,6 +1935,8 @@ int nsp_interface_get_props(NSPI_HANDLE handle, uint32_t flags,
 	if (result != ecSuccess) {
 		if (result != ecWarnWithErrors)
 			*pprows = nullptr;
+		NSP_ROWSET rs = {*pprows != nullptr ? 1U : 0U, *pprows};
+		nsp_trace(__func__, 1, pstat, nullptr, &rs);
 		return result;
 	}
 	if (!b_proptags) {
@@ -1959,7 +1961,8 @@ int nsp_interface_get_props(NSPI_HANDLE handle, uint32_t flags,
 	}
 	if (result != ecSuccess && result != ecWarnWithErrors)
 		*pprows = NULL;
-	nsp_trace(__func__, 1, pstat);
+	NSP_ROWSET rs = {*pprows != nullptr ? 1U : 0U, *pprows};
+	nsp_trace(__func__, 1, pstat, nullptr, &rs);
 	return result;
 }
 
