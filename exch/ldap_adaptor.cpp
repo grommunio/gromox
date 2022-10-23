430c67ca7b82c495287600dcc67599e7e1a273f9
Jan Engelhardt <jengelh@inai.de>
2022-10-23 16:21:45 +0200

    ldap_adaptor: untangle login3 cases for empty URI handling

--- exch/ldap_adaptor.cpp
+++ exch/ldap_adaptor.cpp
@@ -252,10 +252,12 @@ BOOL ldap_adaptor_login3(const char *user, const char *pass, const sql_meta_resu
 		g_conn_pool.resize(0);
 		g_conn_pool.clear();
 	}
-	auto conn = m.ldap_uri.empty() ?
-	            make_conn(g_ldap_host.c_str(), g_bind_user.c_str(),
-	            g_bind_pass.c_str(), true) :
-	            make_conn(m.ldap_uri.c_str(), m.ldap_binddn.c_str(),
+	if (m.ldap_uri.empty()) {
+		auto conn = make_conn(g_ldap_host.c_str(), g_bind_user.c_str(),
+		            g_bind_pass.c_str(), true);
+		return ldaplogin_host(conn, conn, user, pass, m.ldap_basedn);
+	}
+	auto conn = make_conn(m.ldap_uri.c_str(), m.ldap_binddn.c_str(),
 	            m.ldap_bindpw.c_str(), true);
 	return ldaplogin_host(conn, conn, user, pass, m.ldap_basedn);
 }
