ff5cd8e58632a16e06b3b0804c2176f0f4defb39
Jan Engelhardt <jengelh@inai.de>
2022-12-05 01:26:14 +0100

    bounce_producer: dedup/rewrite subst generation of mda bounce_producer

--- include/gromox/bounce_gen.hpp
+++ include/gromox/bounce_gen.hpp
@@ -2,8 +2,13 @@
 #include <string>
 #include <gromox/defs.h>
 struct ATTACHMENT_LIST;
+struct MAIL;
 struct tarray_set;
 namespace gromox {
 extern GX_EXPORT std::string bounce_gen_rcpts(const tarray_set &, const char *sep);
 extern GX_EXPORT std::string bounce_gen_attachs(const ATTACHMENT_LIST &, const char *sep);
+extern GX_EXPORT std::string bounce_gen_attachs(const MAIL &, const char *cset, const char *sep);
+extern GX_EXPORT std::string bounce_gen_thrindex(const MAIL &);
+extern GX_EXPORT std::string bounce_gen_charset(const MAIL &);
+extern GX_EXPORT std::string bounce_gen_subject(const MAIL &, const char *cset);
 }
