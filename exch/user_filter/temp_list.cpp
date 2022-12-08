015e9f6bdbacff502ed18d1bbd81ddbf44c7bed1
Jan Engelhardt <jengelh@inai.de>
2022-12-08 14:40:25 +0100

    user_filter: delete unused functions

--- exch/user_filter/temp_list.cpp
+++ exch/user_filter/temp_list.cpp
@@ -86,32 +86,6 @@ BOOL temp_list_add_string(const char *str, int interval)
 }
 
 /*
- *	remove the specified string from the temp list
- *	@param	
- *		str		string
- *	@return	 
- *		TRUE	success
- *		FALSE	fail
- */
-BOOL temp_list_remove_string(const char *str)
-{
-	char temp_string[256];
-	
-	if (str == nullptr)
-		return TRUE;
-	strncpy(temp_string, str, sizeof(temp_string));
-	temp_string[sizeof(temp_string) - 1] = '\0';
-	if (!g_case_sensitive)
-		HX_strlower(temp_string);
-	std::lock_guard sm_hold(g_string_mutex_lock);
-	auto iter = g_string_hash.find(temp_string);
-	if (iter == g_string_hash.end())
-		return TRUE;
-	g_string_hash.erase(temp_string);
-	return TRUE;
-}
-
-/*
  *	query if the specified string is in the 
  *	temp list
  *	@param	
@@ -168,66 +142,3 @@ static size_t temp_list_collect_string_entry()
 	return collected_num;
 #endif
 }
-
-/*
- *  enumerate each item in temp list hash table
- *	@param
- *		str [in]		string
- *		puntil [out]	buffer for saving until time
- *	@return
- *		TRUE			found
- *		FALSE			found nothing
- */
-BOOL temp_list_echo(const char *str, time_t *puntil)
-{
-	char temp_string[256];
-
-	if (str == nullptr || puntil == nullptr)
-		return FALSE;
-	strncpy(temp_string, str, sizeof(temp_string));
-	temp_string[sizeof(temp_string) - 1] = '\0';
-	if (!g_case_sensitive)
-		HX_strlower(temp_string);
-	std::lock_guard sm_hold(g_string_mutex_lock);
-	/* first remove the overdue items */
-	temp_list_collect_string_entry();
-	auto iter = g_string_hash.find(temp_string);
-	if (iter == g_string_hash.end())
-		return FALSE;
-	*puntil = iter->second;
-	return TRUE;
-}
-
-BOOL temp_list_dump(const char *path)
-{
-	int fd;
-	struct tm time_buff;
-	time_t current_time;
-
-	if (path == nullptr)
-		return FALSE;
-	fd = open(path, O_WRONLY|O_CREAT|O_TRUNC, DEF_MODE);
-	if (-1 == fd) {
-		return FALSE;
-	}
-
-	std::unique_lock sm_hold(g_string_mutex_lock);
-	time(&current_time);
-	for (auto iter = g_string_hash.begin(); iter != g_string_hash.end(); ) {
-		if (current_time >= iter->second) {
-			iter = g_string_hash.erase(iter);
-			continue;
-		}
-		char ts[64];
-		strftime(ts, std::size(ts), "%Y-%m-%d %H:%M:%S", localtime_r(&iter->second, &time_buff));
-		if (HXio_fullwrite(fd, iter->first.c_str(), iter->first.size()) < 0 ||
-		    HXio_fullwrite(fd, "\t", 1) < 0 ||
-		    HXio_fullwrite(fd, ts, strlen(ts)) < 0 ||
-		    HXio_fullwrite(fd, "\n", 1) < 0)
-			break;
-	}
-	sm_hold.unlock();
-	close(fd);
-	return TRUE;
-}
-
