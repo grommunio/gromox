4be5027d1cb28a3c040d497f418bc71e50a5b837
Jan Engelhardt <jengelh@inai.de>
2022-12-08 14:40:25 +0100

    user_filter: delete unused functions

--- exch/user_filter/user_filter.hpp
+++ exch/user_filter/user_filter.hpp
@@ -24,19 +24,12 @@ extern void audit_filter_init(BOOL case_sensitive, int audit_num, int audit_inte
 extern void audit_filter_stop();
 extern BOOL audit_filter_query(const char *str);
 extern BOOL audit_filter_judge(const char *str);
-extern BOOL audit_filter_echo(const char *str, time_t *first_access, time_t *last_access, int *times);
-extern BOOL audit_filter_dump(const char *path);
-extern BOOL audit_filter_remove_string(const char *str);
 
 extern void grey_list_init(BOOL case_sensitive, const char *path, int growing_num);
 extern int grey_list_run();
 extern void grey_list_free();
 extern int grey_list_refresh();
 extern int grey_list_query(const char *str, BOOL count);
-extern BOOL grey_list_add_string(const char *str, int times, int interval);
-extern BOOL grey_list_remove_string(const char *str);
-extern BOOL grey_list_dump(const char *path);
-extern BOOL grey_list_echo(const char *str, int *times, int *interval);
 
 extern void str_filter_init(const char *module_name, BOOL case_sensitive, int audit_num, int audit_interval, int audit_times, int temp_list_size, const char *list_path, int growing_num);
 extern void str_filter_free();
@@ -52,6 +45,3 @@ extern int temp_list_run();
 extern void temp_list_free();
 extern BOOL temp_list_add_string(const char *str, int interval);
 extern BOOL temp_list_query(const char *str);
-extern BOOL temp_list_remove_string(const char *str);
-extern BOOL temp_list_dump(const char *path);
-extern BOOL temp_list_echo(const char *str, time_t *puntil);
