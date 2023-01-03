1a7d4414b04a57936d220b0d584d7ece34877db3
Jan Engelhardt <jengelh@inai.de>
2023-01-03 14:39:32 +0100

    dbop: add private store schema 8 with definite messages.is_deleted column
    References: GXL-337 (GX-22), GXF-193

--- lib/dbop_sqlite.cpp
+++ lib/dbop_sqlite.cpp
@@ -14,6 +14,7 @@
  * INDEX names should be suffixed by the schema number, to facilitate
  * dbop_sqlite_upgrade (old table+index exists simultaneously with new table).
  */
+#define TABLE_END {}
 
 namespace gromox {
 
@@ -238,7 +239,7 @@ static constexpr char tbl_pvt_msgs_0[] =
 "CREATE INDEX parent_assoc_index ON messages(parent_fid, is_associated);"
 "CREATE INDEX parent_read_assoc_index ON messages(parent_fid, read_state, is_associated);";
 
-static constexpr char tbl_pvt_msgs_7[] =
+static constexpr char tbl_pvt_msgs_8[] =
 "CREATE TABLE messages ("
 "  message_id INTEGER PRIMARY KEY,"
 "  parent_fid INTEGER,"
@@ -254,13 +255,13 @@ static constexpr char tbl_pvt_msgs_7[] =
 "  mid_string TEXT DEFAULT NULL,"
 "  FOREIGN KEY (parent_fid) REFERENCES folders (folder_id) ON DELETE CASCADE ON UPDATE CASCADE,"
 "  FOREIGN KEY (parent_attid) REFERENCES attachments (attachment_id) ON DELETE CASCADE ON UPDATE CASCADE);"
-"CREATE INDEX pid_messages_index7 ON messages(parent_fid);"
-"CREATE INDEX attid_messages_index7 ON messages(parent_attid);"
-"CREATE INDEX assoc_index7 ON messages(is_associated);"
-"CREATE INDEX parent_assoc_index7 ON messages(parent_fid, is_associated);"
-"CREATE INDEX parent_read_assoc_index7 ON messages(parent_fid, read_state, is_associated);";
+"CREATE INDEX pid_messages_index8 ON messages(parent_fid);"
+"CREATE INDEX attid_messages_index8 ON messages(parent_attid);"
+"CREATE INDEX assoc_index8 ON messages(is_associated);"
+"CREATE INDEX parent_assoc_index8 ON messages(parent_fid, is_associated);"
+"CREATE INDEX parent_read_assoc_index8 ON messages(parent_fid, read_state, is_associated);";
 
-static constexpr char tbl_pvt_msgs_move7[] =
+static constexpr char tbl_pvt_msgs_move8[] =
 "INSERT INTO messages SELECT message_id, parent_fid, parent_attid, 0 AS is_deleted, is_associated, change_number, read_cn, read_state, message_size, group_id, timer_id, mid_string FROM u0";
 
 static constexpr char tbl_pvt_recvfld_0[] =
@@ -359,7 +360,7 @@ static constexpr tbl_init tbl_pvt_init_0[] = {
 	{"receive_table", tbl_pvt_recvfld_0},
 	{"search_scopes", tbl_pvt_searchscopes_0},
 	{"search_result", tbl_pvt_searchresult_0},
-	{},
+	TABLE_END,
 };
 
 static constexpr tbl_init tbl_pvt_init_top[] = {
@@ -377,11 +378,11 @@ static constexpr tbl_init tbl_pvt_init_top[] = {
 	{"attachments", tbl_attach_0},
 	{"attachment_properties", tbl_atxprops_6},
 	{"folders", tbl_pvt_folders_0},
-	{"messages", tbl_pvt_msgs_0},
+	{"messages", tbl_pvt_msgs_8},
 	{"receive_table", tbl_pvt_recvfld_0},
 	{"search_scopes", tbl_pvt_searchscopes_0},
 	{"search_result", tbl_pvt_searchresult_0},
-	{},
+	TABLE_END,
 };
 
 static constexpr tbl_init tbl_pub_init_0[] = {
@@ -403,7 +404,7 @@ static constexpr tbl_init tbl_pub_init_0[] = {
 	{"read_states", tbl_pub_readst_0},
 	{"read_cns", tbl_pub_readcn_0},
 	{"replca_mapping", tbl_pub_replmap_0},
-	{},
+	TABLE_END,
 };
 
 static constexpr tbl_init tbl_pub_init_top[] = {
@@ -425,7 +426,7 @@ static constexpr tbl_init tbl_pub_init_top[] = {
 	{"read_states", tbl_pub_readst_0},
 	{"read_cns", tbl_pub_readcn_0},
 	{"replca_mapping", tbl_pub_replmap_0},
-	{},
+	TABLE_END,
 };
 
 static constexpr char tbl_midb_folders_0[] =
@@ -487,7 +488,7 @@ static constexpr tbl_init tbl_midb_init_0[] = {
 	{"folders", tbl_midb_folders_0},
 	{"messages", tbl_midb_msgs_0},
 	{"mapping", tbl_midb_mapping_0},
-	{},
+	TABLE_END,
 };
 
 static constexpr tbl_init tbl_midb_init_top[] = {
@@ -495,9 +496,13 @@ static constexpr tbl_init tbl_midb_init_top[] = {
 	{"folders", tbl_midb_folders_0},
 	{"messages", tbl_midb_msgs_0},
 	{"mapping", tbl_midb_mapping_0},
-	{},
+	TABLE_END,
 };
 
+/*
+ * Because sqlite does not support ALTER TABLE CHANGE COLUMN statements, we are
+ * but left with recreating the entire table and issuing a move(copy) command.
+ */
 static constexpr tblite_upgradefn tbl_pvt_upgrade_list[] = {
 	{1, nullptr, "configurations", tbl_config_1, tbl_config_move1},
 	{2, nullptr, "store_properties", tbl_storeprops_2, tbl_storeprops_move2},
@@ -507,10 +512,11 @@ static constexpr tblite_upgradefn tbl_pvt_upgrade_list[] = {
 	{6, nullptr, "attachment_properties", tbl_atxprops_6, tbl_atxprops_move6},
 	/*
 	 * Some AAPI versions generated schema 0 databases with an is_deleted column.
-	 * Recreate that table to make that column official.
+	 * Some dbop_sqlite versions generated schema 7 databases without
+	 * an is_deleted column. Make it right.
 	 */
-	{7, nullptr, "messages", tbl_pvt_msgs_7, tbl_pvt_msgs_move7},
-	{},
+	{8, nullptr, "messages", tbl_pvt_msgs_8, tbl_pvt_msgs_move8},
+	TABLE_END,
 };
 
 static constexpr tblite_upgradefn tbl_pub_upgrade_list[] = {
@@ -520,12 +526,12 @@ static constexpr tblite_upgradefn tbl_pub_upgrade_list[] = {
 	{4, nullptr, "message_properties", tbl_msgprops_4, tbl_msgprops_move4},
 	{5, nullptr, "recipients_properties", tbl_rcptprops_5, tbl_rcptprops_move5},
 	{6, nullptr, "attachment_properties", tbl_atxprops_6, tbl_atxprops_move6},
-	{},
+	TABLE_END,
 };
 
 static constexpr tblite_upgradefn tbl_midb_upgrade_list[] = {
 	{1, nullptr, "configurations", tbl_config_1, tbl_config_move1},
-	{},
+	TABLE_END,
 };
 
 static char kind_to_char(sqlite_kind k)
