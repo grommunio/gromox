16eb13ced6c6f3b0698f2f9dc4268b94f93807f4
Jan Engelhardt <jengelh@inai.de>
2023-01-15 14:33:52 +0100

    emsmdb: avoid potential subtraction underflow when streaming
    References: GXL-345

--- exch/emsmdb/oxcprpt.cpp
+++ exch/emsmdb/oxcprpt.cpp
@@ -1041,7 +1041,10 @@ ec_error_t rop_readstream(uint16_t byte_count, uint32_t max_byte_count,
 		buffer_size = byte_count;
 	}
 	emsmdb_interface_get_rop_left(&max_rop);
-	max_rop -= 16;
+	if (max_rop >= 16)
+		max_rop -= 16;
+	else
+		max_rop = 0;
 	if (buffer_size > max_rop) {
 		buffer_size = max_rop;
 	}
