07789b352ffd016832d76ac972fdc7b0c9fe8e4e
Jan Engelhardt <jengelh@inai.de>
2023-02-22 11:11:14 +0100

    mapi_lib: add const qualifiers to propval_size function

--- include/gromox/propval.hpp
+++ include/gromox/propval.hpp
@@ -4,7 +4,7 @@
 
 extern GX_EXPORT void *propval_dup(uint16_t type, const void *);
 void propval_free(uint16_t type, void *pvalue);
-extern GX_EXPORT uint32_t propval_size(uint16_t type, void *) __attribute__((nonnull(2)));
+extern GX_EXPORT uint32_t propval_size(uint16_t type, const void *pvalue) __attribute__((nonnull(2)));
 extern GX_EXPORT int propval_compare(const void *, const void *, uint16_t proptype) __attribute__((nonnull(1,2)));
 extern GX_EXPORT bool propval_compare_relop(relop, uint16_t proptype, const void *, const void *) __attribute__((nonnull(3,4)));
 extern GX_EXPORT int SVREID_compare(const SVREID *, const SVREID *);
