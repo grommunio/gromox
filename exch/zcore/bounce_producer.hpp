a40cbf0357033568695ddebacf795678c3ca38f3
François Châteauneuf <fcn@gromox.com>
2023-03-08 09:20:14 +0100

    exch,mda: remove unused old bounce_producer prototypes

--- exch/zcore/bounce_producer.hpp
+++ exch/zcore/bounce_producer.hpp
@@ -4,5 +4,4 @@
 struct MAIL;
 struct MESSAGE_CONTENT;
 
-extern int bounce_producer_run(const char *, const char *, const char *);
 extern BOOL zcore_bouncer_make(const char *username, MESSAGE_CONTENT *brief, const char *bounce_type, MAIL *);
--- mda/exmdb_local/exmdb_local.hpp
+++ mda/exmdb_local/exmdb_local.hpp
@@ -20,7 +20,6 @@ extern void auto_response_reply(const char *user_home, const char *from, const c
 extern void bounce_audit_init(int audit_num, int audit_interval);
 extern BOOL bounce_audit_check(const char *audit_string);
 
-extern int bounce_producer_run(const char *, const char *, const char *);
 extern bool exml_bouncer_make(const char *from, const char *rcpt, MAIL *orig, time_t orig_time, const char *bounce_type, MAIL *cur);
 
 extern int (*exmdb_local_check_domain)(const char *domainname);
