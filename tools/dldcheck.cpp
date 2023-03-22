e87fca03bf328a5a8af3fa795219ad9bba5f001a
Jan Engelhardt <jengelh@inai.de>
2023-03-22 15:42:58 +0100

    dldcheck: skip use of cstring and libHX
    HX_dirname allocates a string which we failed to free. It is possible to do without.

--- tools/dldcheck.cpp
+++ tools/dldcheck.cpp
@@ -3,13 +3,11 @@
 // This file is part of Gromox.
 #include <cstdio>
 #include <cstdlib>
-#include <cstring>
 #include <dlfcn.h>
 #include <fcntl.h>
 #include <string>
 #include <unistd.h>
 #include <libHX/defs.h>
-#include <libHX/string.h>
 #include <sys/stat.h>
 #undef EXIT_FAILURE
 #define EXIT_FAILURE 2
@@ -20,14 +18,16 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	auto stamp = argv[1];
 	for (argv += 2; *argv != nullptr; ++argv) {
-		auto file = *argv;
-		auto z = strlen(file);
-		auto so = z >= 3 && strcmp(&file[z-3], ".la") == 0 ?
-		          std::string(HX_dirname(file)) + "/.libs/" +
-		          std::string(file, z - 3) + ".so" : std::string(file);
-		auto h = dlopen(so.c_str(), RTLD_NOW);
+		std::string file = *argv;
+		auto z = file.size();
+		if (z >= 3 && file.compare(z - 3, z, ".la") == 0) {
+			file.replace(z - 3, 3, ".so");
+			auto pos = file.find_last_of('/');
+			file.insert(pos != file.npos ? pos + 1 : 0, ".libs/");
+		}
+		auto h = dlopen(file.c_str(), RTLD_NOW);
 		if (h == nullptr) {
-			fprintf(stderr, "dlopen %s: %s\n", so.c_str(), dlerror());
+			fprintf(stderr, "dlopen %s: %s\n", file.c_str(), dlerror());
 			return EXIT_FAILURE;
 		}
 		dlclose(h);
