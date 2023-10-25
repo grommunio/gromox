0f3da59855f80d8be1939dfc1c51e6d8403ec88a
Jan Engelhardt <jengelh@inai.de>
2023-10-25 11:03:16 +0200

    delivery-queue: better error reporting for invalid vs. non-existing users
    Valid addresses to non-existing recipients leads to response message
    516, "invalid user". I was thinking about adding a new response
    message "unknown user" (537).
    
    Since the RCPT addresses are fed through vmime's parser, invalid
    addresses will be translated to "invalid@invalid" before
    delivery-queue sees it. So rather than add string 537, just reword
    516.

--- mda/smtp/resource.cpp
+++ mda/smtp/resource.cpp
@@ -71,7 +71,7 @@ static constexpr std::pair<unsigned int, const char *> g_default_code_table[] = 
 	{513, "530 Authentication required", },
 	{514, "534 Authentication mechanism is too weak"},
 	{515, "538 Encryption required for requested authentication mechanism"},
-	{516, "550 invalid user - <email_addr>"},
+	{516, "550 unknown user - <email_addr>"},
 	{517, "452 Mailbox <email_addr> is full"},
 	{518, "550 access denied to you"},
 	{519, "550 Access to Mailbox <email_addr>  is denied"},
