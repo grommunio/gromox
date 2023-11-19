647575faca0894a3993f100ba80a220125511396
Jan Engelhardt <jengelh@inai.de>
2023-11-19 23:23:49 +0100

    ews: consolidate EX->SMTP resolution code (4/3)

--- include/gromox/usercvt.hpp
+++ include/gromox/usercvt.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <functional>
 #include <string>
 #include <gromox/defs.h>
 #include <gromox/mapierr.hpp>
@@ -7,7 +8,7 @@ struct BINARY;
 
 namespace gromox {
 
-using cvt_id2user = ec_error_t (*)(int, std::string &);
+using cvt_id2user = std::function<ec_error_t(int, std::string &)>;
 extern GX_EXPORT ec_error_t cvt_essdn_to_username(const char *emaddr, const char *org, cvt_id2user, std::string &);
 extern GX_EXPORT ec_error_t cvt_essdn_to_username(const char *emaddr, const char *org, cvt_id2user, char *, size_t);
 extern GX_EXPORT bool emsab_to_email(EXT_PULL &, const char *org, cvt_id2user, char *addr, size_t adsize);
