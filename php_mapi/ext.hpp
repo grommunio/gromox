bf72694999fefb1ecc2140ea8a831865d06b0795
Jan Engelhardt <jengelh@inai.de>
2023-12-11 02:06:33 +0100

    php_mapi: remove two unused defines

--- php_mapi/ext.hpp
+++ php_mapi/ext.hpp
@@ -12,8 +12,6 @@
 #undef asprintf
 
 #define SYNC_DELETES_FLAG_HARDDELETE				0x02
-#define ROOT_HANDLE 0
-#define INVALID_HANDLE 0xFFFFFFFF
 #define SYNC_NEW_MESSAGE 0x800
 #define SYNC_SOFT_DELETE 0x01
 
