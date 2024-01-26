0a60f6616f7c85d28bdfc7dbf933e94443ea20c3
Julia Schroeder <julia.schroeder@grommunio.com>
2024-01-26 08:17:10 +0100

    ews: fix "missing field 'pname' initializer"

--- exch/ews/namedtags.hpp
+++ exch/ews/namedtags.hpp
@@ -10,33 +10,32 @@
 static const PROPERTY_NAME NtGlobalObjectId = {MNID_ID, PSETID_MEETING, PidLidGlobalObjectId, deconst("GlobalObjectId")};
 static const PROPERTY_NAME NtCategories = {MNID_STRING, PS_PUBLIC_STRINGS, 0, deconst("Keywords")};
 
-
 /* PSETID_Address */
-static const PROPERTY_NAME NtImAddress1 = {MNID_ID, PSETID_ADDRESS, PidLidInstantMessagingAddress};
-static const PROPERTY_NAME NtEmailAddress1 = {MNID_ID, PSETID_ADDRESS, PidLidEmail1EmailAddress};
-static const PROPERTY_NAME NtEmailAddress2 = {MNID_ID, PSETID_ADDRESS, PidLidEmail2EmailAddress};
-static const PROPERTY_NAME NtEmailAddress3 = {MNID_ID, PSETID_ADDRESS, PidLidEmail3EmailAddress};
+static const PROPERTY_NAME NtImAddress1 = {MNID_ID, PSETID_ADDRESS, PidLidInstantMessagingAddress, nullptr};
+static const PROPERTY_NAME NtEmailAddress1 = {MNID_ID, PSETID_ADDRESS, PidLidEmail1EmailAddress, nullptr};
+static const PROPERTY_NAME NtEmailAddress2 = {MNID_ID, PSETID_ADDRESS, PidLidEmail2EmailAddress, nullptr};
+static const PROPERTY_NAME NtEmailAddress3 = {MNID_ID, PSETID_ADDRESS, PidLidEmail3EmailAddress, nullptr};
 
 /* PSETID_Task */
-static const PROPERTY_NAME NtTaskStatus = {MNID_ID, PSETID_TASK, PidLidTaskStatus};
-static const PROPERTY_NAME NtPercentComplete = {MNID_ID, PSETID_TASK, PidLidPercentComplete};
-static const PROPERTY_NAME NtTaskStartDate = {MNID_ID, PSETID_TASK, PidLidTaskStartDate};
-static const PROPERTY_NAME NtTaskDueDate = {MNID_ID, PSETID_TASK, PidLidTaskDueDate};
-static const PROPERTY_NAME NtTaskDateCompleted = {MNID_ID, PSETID_TASK, PidLidTaskDateCompleted};
-static const PROPERTY_NAME NtTaskActualEffort = {MNID_ID, PSETID_TASK, PidLidTaskActualEffort};
-static const PROPERTY_NAME NtTaskEstimatedEffort = {MNID_ID, PSETID_TASK, PidLidTaskEstimatedEffort};
-static const PROPERTY_NAME NtTaskRecurrence = {MNID_ID, PSETID_TASK, PidLidTaskRecurrence};
-static const PROPERTY_NAME NtTaskComplete = {MNID_ID, PSETID_TASK, PidLidTaskComplete};
-static const PROPERTY_NAME NtTaskOwner = {MNID_ID, PSETID_TASK, PidLidTaskOwner};
-static const PROPERTY_NAME NtTaskFRecurring = {MNID_ID, PSETID_TASK, PidLidTaskFRecurring};
+static const PROPERTY_NAME NtTaskStatus = {MNID_ID, PSETID_TASK, PidLidTaskStatus, nullptr};
+static const PROPERTY_NAME NtPercentComplete = {MNID_ID, PSETID_TASK, PidLidPercentComplete, nullptr};
+static const PROPERTY_NAME NtTaskStartDate = {MNID_ID, PSETID_TASK, PidLidTaskStartDate, nullptr};
+static const PROPERTY_NAME NtTaskDueDate = {MNID_ID, PSETID_TASK, PidLidTaskDueDate, nullptr};
+static const PROPERTY_NAME NtTaskDateCompleted = {MNID_ID, PSETID_TASK, PidLidTaskDateCompleted, nullptr};
+static const PROPERTY_NAME NtTaskActualEffort = {MNID_ID, PSETID_TASK, PidLidTaskActualEffort, nullptr};
+static const PROPERTY_NAME NtTaskEstimatedEffort = {MNID_ID, PSETID_TASK, PidLidTaskEstimatedEffort, nullptr};
+static const PROPERTY_NAME NtTaskRecurrence = {MNID_ID, PSETID_TASK, PidLidTaskRecurrence, nullptr};
+static const PROPERTY_NAME NtTaskComplete = {MNID_ID, PSETID_TASK, PidLidTaskComplete, nullptr};
+static const PROPERTY_NAME NtTaskOwner = {MNID_ID, PSETID_TASK, PidLidTaskOwner, nullptr};
+static const PROPERTY_NAME NtTaskFRecurring = {MNID_ID, PSETID_TASK, PidLidTaskFRecurring, nullptr};
 
 /* PSETID_Appointment */
 static const PROPERTY_NAME NtAppointmentSequence = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentSequence, deconst("AppointmentSequence")};
 static const PROPERTY_NAME NtBusyStatus = {MNID_ID, PSETID_APPOINTMENT, PidLidBusyStatus, deconst("BusyStatus")};
 static const PROPERTY_NAME NtLocation = {MNID_ID, PSETID_APPOINTMENT, PidLidLocation, deconst("Location")};
 static const PROPERTY_NAME NtAppointmentReplyTime = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentReplyTime, deconst("AppointmentReplyTime")};
-static const PROPERTY_NAME NtAppointmentStartWhole = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentStartWhole};
-static const PROPERTY_NAME NtAppointmentEndWhole = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentEndWhole};
+static const PROPERTY_NAME NtAppointmentStartWhole = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentStartWhole, nullptr};
+static const PROPERTY_NAME NtAppointmentEndWhole = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentEndWhole, nullptr};
 static const PROPERTY_NAME NtAppointmentSubType = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentSubType, deconst("AppointmentSubType")};
 static const PROPERTY_NAME NtAppointmentRecur = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentRecur, deconst("AppointmentRecur")};
 static const PROPERTY_NAME NtAppointmentStateFlags = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentStateFlags, deconst("AppointmentStateFlags")};
@@ -47,11 +46,11 @@ static const PROPERTY_NAME NtRecurring = {MNID_ID, PSETID_APPOINTMENT, PidLidRec
 static const PROPERTY_NAME NtAppointmentNotAllowPropose = {MNID_ID, PSETID_APPOINTMENT, PidLidAppointmentNotAllowPropose, deconst("AppointmentNotAllowPropose")};
 
 /* PSETID_Common */
-static const PROPERTY_NAME NtReminderDelta = {MNID_ID, PSETID_COMMON, PidLidReminderDelta};
-static const PROPERTY_NAME NtReminderTime = {MNID_ID, PSETID_COMMON, PidLidReminderTime};
-static const PROPERTY_NAME NtReminderSet = {MNID_ID, PSETID_COMMON, PidLidReminderSet};
+static const PROPERTY_NAME NtReminderDelta = {MNID_ID, PSETID_COMMON, PidLidReminderDelta, nullptr};
+static const PROPERTY_NAME NtReminderTime = {MNID_ID, PSETID_COMMON, PidLidReminderTime, nullptr};
+static const PROPERTY_NAME NtReminderSet = {MNID_ID, PSETID_COMMON, PidLidReminderSet, nullptr};
 static const PROPERTY_NAME NtCommonStart = {MNID_ID, PSETID_COMMON, PidLidCommonStart, deconst("CommonStart")};
 static const PROPERTY_NAME NtCommonEnd = {MNID_ID, PSETID_COMMON, PidLidCommonEnd, deconst("CommonEnd")};
-static const PROPERTY_NAME NtMileage = {MNID_ID, PSETID_COMMON, PidLidMileage};
-static const PROPERTY_NAME NtBilling = {MNID_ID, PSETID_COMMON, PidLidBilling};
-static const PROPERTY_NAME NtCompanies = {MNID_ID, PSETID_COMMON, PidLidCompanies};
+static const PROPERTY_NAME NtMileage = {MNID_ID, PSETID_COMMON, PidLidMileage, nullptr};
+static const PROPERTY_NAME NtBilling = {MNID_ID, PSETID_COMMON, PidLidBilling, nullptr};
+static const PROPERTY_NAME NtCompanies = {MNID_ID, PSETID_COMMON, PidLidCompanies, nullptr};
