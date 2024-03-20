grommunio/gromox

commit 8bda598892a4c716b36b9296982b85e1a55b0f8a

imap: diagnose discrepancies between size reported in MJSON vs. file size

mra/imap/imap_parser.cpp
@@ -814,7 +814,8 @@ static tproc_status ps_stat_wrdat(imap_context *pcontext)
 		len = 64 * 1024;
 	auto read_len = read(pcontext->message_fd, pcontext->write_buff, len);
 	if (read_len != len) {
-		imap_parser_log_info(pcontext, LV_WARN, "failed to read message file");
+		imap_parser_log_info(pcontext, LV_WARN, "W-5200: %s: wanted %d bytes, got %zd",
+			pcontext->file_path.c_str(), len, read_len);
 		/* IMAP_CODE_2180012: * BAD internal error: fail to read file */
 		size_t string_length = 0;
 		auto imap_reply_str = resource_get_imap_code(1812, 1, &string_length);
@@ -937,7 +938,7 @@ static tproc_status ps_end_processing(imap_context *pcontext,
 static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 {
 	int len;
-	int read_len;
+	ssize_t read_len;
 	int line_length;
 	char *last_line;
 	char *ptr, *ptr1;
@@ -974,9 +975,10 @@ static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 				*ptr = '\0';
 				*ptr1 = '\0';
 				pcontext->close_fd();
+				pcontext->file_path.clear();
 				try {
-					auto eml_path = std::string(pcontext->maildir) + "/eml/" + (last_line + 8);
-					pcontext->message_fd = open(eml_path.c_str(), O_RDONLY);
+					pcontext->file_path = std::string(pcontext->maildir) + "/eml/" + (last_line + 8);
+					pcontext->message_fd = open(pcontext->file_path.c_str(), O_RDONLY);
 				} catch (const std::bad_alloc &) {
 					mlog(LV_ERR, "E-1466: ENOMEM");
 				}
@@ -994,8 +996,10 @@ static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 					read_len = read(pcontext->message_fd, pcontext->write_buff +
 					           pcontext->write_length, len);
 					if (read_len != len) {
-						imap_parser_log_info(pcontext, LV_WARN, "failed to read message file");
+						imap_parser_log_info(pcontext, LV_WARN, "W-5201: %s: wanted %d bytes, got %zd",
+							pcontext->file_path.c_str(), len, read_len);
 						pcontext->close_fd();
+						pcontext->file_path.clear();
 						return IMAP_RETRIEVE_ERROR;
 					}
 					pcontext->current_len += len;
@@ -1017,9 +1021,10 @@ static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 				*ptr = '\0';
 				*ptr1 = '\0';
 				pcontext->close_fd();
+				pcontext->file_path.clear();
 				try {
-					auto rfc_path = std::string(pcontext->maildir) + "/tmp/imap.rfc822/" + (last_line + 10);
-					pcontext->message_fd = open(rfc_path.c_str(), O_RDONLY);
+					pcontext->file_path = std::string(pcontext->maildir) + "/tmp/imap.rfc822/" + (last_line + 10);
+					pcontext->message_fd = open(pcontext->file_path.c_str(), O_RDONLY);
 				} catch (const std::bad_alloc &) {
 					mlog(LV_ERR, "E-1467: ENOMEM");
 				}
@@ -1037,8 +1042,10 @@ static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 					read_len = read(pcontext->message_fd, pcontext->write_buff +
 					           pcontext->write_length, len);
 					if (read_len != len) {
-						imap_parser_log_info(pcontext, LV_WARN, "failed to read message file");
+						imap_parser_log_info(pcontext, LV_WARN, "W-5202: %s: wanted %d bytes, got %zd",
+							pcontext->file_path.c_str(), len, read_len);
 						pcontext->close_fd();
+						pcontext->file_path.clear();
 						return IMAP_RETRIEVE_ERROR;
 					}
 					pcontext->current_len += len;

commit 0a6c72babfaddc95558940aa07828f7b649a1fb5

imap: fix eml files getting lost
file_path was always unlinked on destruction, even when readonly
operations were performed. A safeguard is needed, hereby added by
evaluating the flags used at the time of open.

Trigger was:

	>A SELECT INBOX
	>A FETCH 1 (RFC822)
	>LOGOUT

Fixes: 8bda59889 [master-only]
References: GXL-498

mra/imap/imap.hpp
@@ -79,6 +79,7 @@ struct content_array final : public XARRAY {
 
 /**
  * @mid:        midstr
+ * @open_mode:  controls unlinking of @file_path upon destruction
  * @file_path:  absolute path in filesystem, built from midstr
  * @message_fd:	feckin descriptor
  * @b_modify:	flag indicating that other clients concurrently modified the mailbox
@@ -101,7 +102,7 @@ struct imap_context final : public schedule_context {
 
 	GENERIC_CONNECTION connection;
 	std::string mid, file_path;
-	int message_fd = -1;
+	int message_fd = -1, open_mode = 0;
 	iproto_stat proto_stat = iproto_stat::none;
 	isched_stat sched_stat = isched_stat::none;
 	char *write_buff = nullptr;

mra/imap/imap_cmd_parser.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only WITH linking exception
-// SPDX-FileCopyrightText: 2020–2021 grommunio GmbH
+// SPDX-FileCopyrightText: 2020–2024 grommunio GmbH
 // This file is part of Gromox.
 /* 
  * collection of functions for handling the imap command
@@ -2368,9 +2368,10 @@ static int imap_cmd_parser_append_begin2(int argc, char **argv,
 	pcontext->mid = fmt::format("{}.{}.{}",
 	                time(nullptr), imap_parser_get_sequence_ID(),
 	                znul(g_config_file->get_value("host_id")));
+	pcontext->open_mode = O_CREAT | O_RDWR | O_TRUNC;
 	pcontext->file_path = fmt::format("{}/tmp/{}",
 	                      pcontext->maildir, pcontext->mid);
-	wrapfd fd = open(pcontext->file_path.c_str(), O_CREAT | O_RDWR | O_TRUNC, FMODE_PRIVATE);
+	wrapfd fd = open(pcontext->file_path.c_str(), pcontext->open_mode, FMODE_PRIVATE);
 	if (fd.get() < 0)
 		return 1909 | DISPATCH_BREAK;
 	std::string buf;
@@ -2476,11 +2477,7 @@ static int imap_cmd_parser_append_end2(int argc, char **argv,
 	if (fd.get() < 0 || !imail.to_file(fd.get())) {
 		imail.clear();
 		pbuff.reset();
-		if (remove(pcontext->file_path.c_str()) < 0 && errno != ENOENT)
-			mlog(LV_WARN, "W-1345: remove %s: %s",
-				pcontext->file_path.c_str(), strerror(errno));
-		pcontext->mid.clear();
-		pcontext->file_path.clear();
+		pcontext->unlink_file();
 		if (remove(eml_path.c_str()) < 0 && errno != ENOENT)
 			mlog(LV_WARN, "W-1346: remove %s: %s",
 			        eml_path.c_str(), strerror(errno));
@@ -2492,10 +2489,7 @@ static int imap_cmd_parser_append_end2(int argc, char **argv,
 	}
 	imail.clear();
 	pbuff.reset();
-	if (remove(pcontext->file_path.c_str()) < 0 && errno != ENOENT)
-		mlog(LV_WARN, "W-1336: remove %s: %s",
-			pcontext->file_path.c_str(), strerror(errno));
-	pcontext->file_path.clear();
+	pcontext->unlink_file();
 	auto ssr = system_services_insert_mail(pcontext->maildir, temp_name,
 	           pcontext->mid.c_str(), flag_buff, tmp_time, &errnum);
 	auto ret = m2icode(ssr, errnum);

mra/imap/imap_parser.cpp
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only WITH linking exception
+// SPDX-FileCopyrightText: 2021–2024 grommunio GmbH
+// This file is part of Gromox.
 /* imap parser is a module, which first read data from socket, parses the imap 
  * commands and then do the corresponding action. 
  */ 
@@ -814,8 +816,7 @@ static tproc_status ps_stat_wrdat(imap_context *pcontext)
 		len = 64 * 1024;
 	auto read_len = read(pcontext->message_fd, pcontext->write_buff, len);
 	if (read_len != len) {
-		imap_parser_log_info(pcontext, LV_WARN, "W-5200: %s: wanted %d bytes, got %zd",
-			pcontext->file_path.c_str(), len, read_len);
+		imap_parser_log_info(pcontext, LV_WARN, "failed to read message file");
 		/* IMAP_CODE_2180012: * BAD internal error: fail to read file */
 		size_t string_length = 0;
 		auto imap_reply_str = resource_get_imap_code(1812, 1, &string_length);
@@ -938,7 +939,7 @@ static tproc_status ps_end_processing(imap_context *pcontext,
 static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 {
 	int len;
-	ssize_t read_len;
+	int read_len;
 	int line_length;
 	char *last_line;
 	char *ptr, *ptr1;
@@ -982,10 +983,10 @@ static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 				*ptr = '\0';
 				*ptr1 = '\0';
 				pcontext->close_fd();
-				pcontext->file_path.clear();
 				try {
 					pcontext->file_path = std::string(pcontext->maildir) + "/eml/" + (last_line + 8);
-					pcontext->message_fd = open(pcontext->file_path.c_str(), O_RDONLY);
+					pcontext->open_mode = O_RDONLY;
+					pcontext->message_fd = open(pcontext->file_path.c_str(), pcontext->open_mode);
 					if (pcontext->message_fd < 0)
 						mlog(LV_ERR, "E-1741: %s: %s", pcontext->file_path.c_str(), strerror(errno));
 				} catch (const std::bad_alloc &) {
@@ -1006,10 +1007,8 @@ static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 					read_len = read(pcontext->message_fd, pcontext->write_buff +
 					           pcontext->write_length, len);
 					if (read_len != len) {
-						imap_parser_log_info(pcontext, LV_WARN, "W-5201: %s: wanted %d bytes, got %zd",
-							pcontext->file_path.c_str(), len, read_len);
+						imap_parser_log_info(pcontext, LV_WARN, "failed to read message file");
 						pcontext->close_fd();
-						pcontext->file_path.clear();
 						return IMAP_RETRIEVE_ERROR;
 					}
 					pcontext->current_len += len;
@@ -1031,10 +1030,10 @@ static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 				*ptr = '\0';
 				*ptr1 = '\0';
 				pcontext->close_fd();
-				pcontext->file_path.clear();
 				try {
 					pcontext->file_path = std::string(pcontext->maildir) + "/tmp/imap.rfc822/" + (last_line + 10);
-					pcontext->message_fd = open(pcontext->file_path.c_str(), O_RDONLY);
+					pcontext->open_mode = O_RDONLY;
+					pcontext->message_fd = open(pcontext->file_path.c_str(), pcontext->open_mode);
 					if (pcontext->message_fd < 0)
 						mlog(LV_ERR, "E-1742: %s: %s", pcontext->file_path.c_str(), strerror(errno));
 				} catch (const std::bad_alloc &) {
@@ -1055,10 +1054,8 @@ static int imap_parser_wrdat_retrieve(imap_context *pcontext)
 					read_len = read(pcontext->message_fd, pcontext->write_buff +
 					           pcontext->write_length, len);
 					if (read_len != len) {
-						imap_parser_log_info(pcontext, LV_WARN, "W-5202: %s: wanted %d bytes, got %zd",
-							pcontext->file_path.c_str(), len, read_len);
+						imap_parser_log_info(pcontext, LV_WARN, "failed to read message file");
 						pcontext->close_fd();
-						pcontext->file_path.clear();
 						return IMAP_RETRIEVE_ERROR;
 					}
 					pcontext->current_len += len;
@@ -1438,7 +1435,7 @@ static void imap_parser_context_clear(imap_context *pcontext)
 	pcontext->sched_stat = isched_stat::none;
 	pcontext->close_fd();
 	pcontext->mid[0] = '\0';
-	pcontext->file_path[0] = '\0';
+	pcontext->file_path.clear();
 	pcontext->write_buff = NULL;
 	pcontext->write_length = 0;
 	pcontext->write_offset = 0;
@@ -1475,7 +1472,7 @@ void imap_context::close_fd()
 
 void imap_context::unlink_file()
 {
-	if (file_path.empty())
+	if (file_path.empty() || !(open_mode & O_CREAT))
 		return;
 	if (remove(file_path.c_str()) < 0 && errno != ENOENT)
 		mlog(LV_WARN, "W-1342: remove %s: %s",