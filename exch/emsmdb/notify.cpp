609ef0b66f0eaa29ef945b6a033ce82115ed790f
Jan Engelhardt <jengelh@inai.de>
2024-04-30 18:01:32 +0200

    emsmdb: merge subscription_object.cpp into notify.cpp

--- exch/emsmdb/notify.cpp
+++ exch/emsmdb/notify.cpp
@@ -5,10 +5,12 @@
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <memory>
 #include <gromox/defs.h>
 #include <gromox/ext_buffer.hpp>
 #include <gromox/rop_util.hpp>
 #include "common_util.h"
+#include "emsmdb_interface.h"
 #include "exmdb_client.h"
 #include "logon_object.h"
 #include "notify_response.h"
@@ -17,9 +19,44 @@
 #include "rop_funcs.hpp"
 #include "rop_ids.hpp"
 #include "rop_processor.h"
-#include "subscription_object.h"
 
 using namespace gromox;
+
+std::unique_ptr<subscription_object>
+subscription_object::create(logon_object *plogon, uint8_t logon_id,
+    uint16_t notification_types, BOOL b_whole, uint64_t folder_id,
+    uint64_t message_id)
+{
+	std::unique_ptr<subscription_object> psub;
+	try {
+		psub.reset(new subscription_object);
+	} catch (const std::bad_alloc &) {
+		return NULL;
+	}
+	if (!emsmdb_interface_get_cxh(&psub->cxh))
+		return NULL;
+	psub->plogon = plogon;
+	psub->logon_id = logon_id;
+	if (!exmdb_client::subscribe_notification(plogon->get_dir(),
+	    notification_types, b_whole, folder_id, message_id, &psub->sub_id))
+		return NULL;
+	return psub;
+}
+
+void subscription_object::set_handle(uint32_t h)
+{
+	auto psub = this;
+	psub->handle = h;
+	emsmdb_interface_add_subscription_notify(psub->plogon->get_dir(),
+		psub->sub_id, psub->handle, psub->logon_id, &psub->cxh.guid);
+}
+
+subscription_object::~subscription_object()
+{
+	auto psub = this;
+	exmdb_client::unsubscribe_notification(psub->plogon->get_dir(), psub->sub_id);
+	emsmdb_interface_remove_subscription_notify(psub->plogon->get_dir(), psub->sub_id);
+}
 
 notify_response *notify_response::create(uint32_t handle, uint8_t logon_id) try
 {
