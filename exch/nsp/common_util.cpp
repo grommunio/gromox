5fd7f80e78c3ce3d696f2b0fcd592231a3e488c8
Jan Engelhardt <jengelh@inai.de>
2024-05-03 17:45:02 +0200

    build: resolve compile failures on gcc 12
    exch/nsp/common_util.cpp:300:27: error: ambiguous overload for
    'operator==' (operand types are 'GUI' and 'const FLATUID')
      300 |         if (g_server_guid == muidEMSAB)
    
    This is no problem in gcc-11 (with -std=c++17) or gcc-13 (with
    -std=c++20).

--- exch/nsp/common_util.cpp
+++ exch/nsp/common_util.cpp
@@ -297,9 +297,13 @@ BOOL common_util_load_file(const char *path, BINARY *pbin)
 int common_util_run()
 {
 	g_server_guid = GUID::random_new();
-	if (g_server_guid == muidEMSAB)
+	/*
+	 * Work around odd behavior of gcc-12 in conjunction with -std=c++20
+	 * and call a.op==(b) instead of a==b.
+	 */
+	if (g_server_guid.operator==(muidEMSAB))
 		g_server_guid = GUID::random_new();
-	if (g_server_guid == muidEMSAB) {
+	if (g_server_guid.operator==(muidEMSAB)) {
 		mlog(LV_ERR, "nsp: unlucky random number generator");
 		return -1;
 	}
