491a5f4b11c7108ea2ea9c86f81c11b7aaf66037
François Châteauneuf <fcn@gromox.com>
2024-05-17 08:20:59 +0200

    build: list some more missing dist files in Makefile.am
    (distcheck still is not fully supported)

--- tools/mpak.pl
+++ tools/mpak.pl
@@ -2,9 +2,11 @@
 # SPDX-License-Identifier: MIT
 use strict;
 use warnings;
+my $dir = shift(@ARGV);
 my $dsize = scalar(@ARGV) * 64;
 my $ofs = 12 + $dsize;
 print "PACK", pack("VV", 12, $dsize);
+@ARGV = map { "$dir/$_" } @ARGV;
 for (@ARGV) {
 	my $n = $_;
 	$n =~ s{^.*/}{};
