3d75ae485756106b61be0938be1b50aa0cee2e14
Jan Engelhardt <jengelh@inai.de>
2024-08-06 07:20:35 +0200

    exmdb_local: replace get_user_lang by mysql_adaptor_meta

--- mda/exmdb_local/exmdb_local.hpp
+++ mda/exmdb_local/exmdb_local.hpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <gromox/common_types.hpp>
 #include <gromox/hook_common.h>
+#include <gromox/mysql_adaptor.hpp>
 
 enum class delivery_status {
 	ok, no_user, mailbox_full, perm_fail, temp_fail, bounce_sent,
@@ -17,7 +18,7 @@ extern BOOL bounce_audit_check(const char *audit_string);
 extern bool exml_bouncer_make(const char *from, const char *rcpt, MAIL *orig, time_t orig_time, const char *bounce_type, MAIL *cur);
 
 extern int (*exmdb_local_check_domain)(const char *domainname);
-extern bool (*exmdb_local_get_lang)(const char *username, char *lang, size_t);
+extern decltype(mysql_adaptor_meta) *exmdb_local_meta;
 extern BOOL (*exmdb_local_check_same_org2)(const char *domainname1, const char *domainname2);
 
 extern void cache_queue_init(const char *path, int scan_interval, int retrying_times);
