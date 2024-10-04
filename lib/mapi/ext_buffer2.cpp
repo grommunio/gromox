52ed6c15313ee12e7b2adef706505336c96aad55
Jan Engelhardt <jengelh@inai.de>
2024-10-04 14:11:44 +0200

    mapi_lib: augment EXT_PULL/PUSH with support for some std::vector-based types

--- lib/mapi/ext_buffer2.cpp
+++ lib/mapi/ext_buffer2.cpp
@@ -1,9 +1,64 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2023–2024 grommunio GmbH
 // This file is part of Gromox.
+#include <limits>
+#include <string>
+#include <vector>
 #include <gromox/ext_buffer.hpp>
 #define TRY(expr) do { pack_result klfdv{expr}; if (klfdv != pack_result::success) return klfdv; } while (false)
 using namespace gromox;
+
+#define AP(T, t, C, c) \
+	pack_result EXT_PUSH::p_ ## t ## _a(const std::vector<T> &r) \
+	{ \
+		if (r.size() > std::numeric_limits<C>::max()) \
+			return pack_result::format; \
+		TRY(p_ ## c(r.size())); \
+		for (size_t i = 0; i < r.size(); ++i) \
+			TRY(p_ ## t(r[i])); \
+		return pack_result::success; \
+	}
+
+#define AN(T, t) \
+	pack_result EXT_PULL::g_ ## t ## _an(std::vector<T> *r, size_t count) try \
+	{ \
+		r->resize(count); \
+		if (count == 0) \
+			return pack_result::success; \
+		for (size_t i = 0; i < count; ++i) \
+			TRY(g_ ## t(&(*r)[i])); \
+		return pack_result::success; \
+	} catch (const std::bad_alloc &) { \
+		return pack_result::alloc; \
+	}
+
+#define A(T, t, C, c) \
+	AN(T, t) \
+	pack_result EXT_PULL::g_ ## t ## _a(std::vector<T> *r) \
+	{ \
+		C count; \
+		TRY(g_ ## c(&count)); \
+		return count != 0 ? g_ ## t ## _an(r, count) : pack_result::success; \
+	}
+
+A(uint16_t, uint16, uint32_t, uint32)
+AP(uint16_t, uint16, uint32_t, uint32)
+A(uint32_t, uint32, uint32_t, uint32)
+AP(uint32_t, uint32, uint32_t, uint32)
+A(uint64_t, uint64, uint32_t, uint32)
+AP(uint64_t, uint64, uint32_t, uint32)
+A(float, float, uint32_t, uint32)
+AP(float, float, uint32_t, uint32)
+A(double, double, uint32_t, uint32)
+AP(double, double, uint32_t, uint32)
+A(GUID, guid, uint32_t, uint32)
+AP(GUID, guid, uint32_t, uint32)
+A(std::string, str, uint32_t, uint32)
+A(std::string, wstr, uint32_t, uint32)
+
+#undef A
+#undef AN
+#undef AP
 
 pack_result EXT_PULL::g_fb(freebusy_event *fb_event)
 {
