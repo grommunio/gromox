a18fde1bc7677709d68fb4175c2169a4205966a2
Jan Engelhardt <jengelh@inai.de>
2024-11-18 18:18:53 +0100

    smtp: deindent smtp_cmd_handler_rcpt

--- mda/smtp/smtp_cmd_handler.cpp
+++ mda/smtp/smtp_cmd_handler.cpp
@@ -196,25 +196,21 @@ int smtp_cmd_handler_rcpt(const char* cmd_line, int line_length,
 	if (pcontext->last_cmd != T_MAIL_CMD && pcontext->last_cmd != T_RCPT_CMD)
 		return 507; /* bad sequence */
 
-	{
-		snprintf(buff, std::size(buff), "%s@%s", email_addr.local_part,
-				email_addr.domain);
-		if (!system_services_check_user(buff, g_rcpt_delimiter.c_str())) {
-			/* 550 invalid user - <email_addr> */
-			smtp_reply_str = resource_get_smtp_code(516, 1, &string_length);
-			smtp_reply_str2 = resource_get_smtp_code(516, 2, &string_length);
-			string_length = gx_snprintf(reason, std::size(reason),
-			                "%s<%s>%s", smtp_reply_str, buff,
-			                smtp_reply_str2);
-			pcontext->connection.write(reason, string_length);
-			mlog(LV_NOTICE, "remote=[%s] from=<%s> to=<%s>  RCPT address is invalid",
-				pcontext->connection.client_ip,
-				pcontext->menv.from, buff);
-			return DISPATCH_CONTINUE;
-		}
-	}
 	snprintf(buff, std::size(buff), "%s@%s", email_addr.local_part,
-		email_addr.domain);
+			email_addr.domain);
+	if (!system_services_check_user(buff, g_rcpt_delimiter.c_str())) {
+		/* 550 invalid user - <email_addr> */
+		smtp_reply_str = resource_get_smtp_code(516, 1, &string_length);
+		smtp_reply_str2 = resource_get_smtp_code(516, 2, &string_length);
+		string_length = gx_snprintf(reason, std::size(reason),
+				"%s<%s>%s", smtp_reply_str, buff,
+				smtp_reply_str2);
+		pcontext->connection.write(reason, string_length);
+		mlog(LV_NOTICE, "remote=[%s] from=<%s> to=<%s>  RCPT address is invalid",
+			pcontext->connection.client_ip,
+			pcontext->menv.from, buff);
+		return DISPATCH_CONTINUE;
+	}
 	pcontext->menv.rcpt_to.push_back(buff);
 	pcontext->last_cmd = T_RCPT_CMD;
 	return 205; /* 250 OK */
