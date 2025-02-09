771a5bda2864cbd7caea3ed42e7aa75aef0da9fc
Jan Engelhardt <jengelh@inai.de>
2025-02-09 14:41:32 +0100

    dnsbl_filter: add c-ares backend

--- exch/dnsbl_filter.cpp
+++ exch/dnsbl_filter.cpp
@@ -15,7 +15,9 @@
 #include <gromox/scope.hpp>
 #include <gromox/svc_common.h>
 #include <gromox/util.hpp>
-#ifdef HAVE_LDNS
+#if defined(HAVE_CARES)
+#	include <ares.h>
+#elif defined(HAVE_LDNS)
 #	include <ldns/ldns.h>
 #elif defined(HAVE_RES_NQUERYDOMAIN)
 #	include <netdb.h>
@@ -26,6 +28,59 @@
 using namespace gromox;
 static std::string g_zone_suffix;
 DECLARE_SVC_API(,);
+
+#ifdef HAVE_CARES
+static std::string abinp_to_str(const ares_dns_rr_t *rr, ares_dns_rr_key_t key)
+{
+	std::string out;
+	size_t cnt = ares_dns_rr_get_abin_cnt(rr, key);
+	for (size_t i = 0; i < cnt; ++i) {
+		size_t len;
+		auto str = ares_dns_rr_get_abin(rr, key, i, &len);
+		if (!out.empty())
+			out += ' ';
+		out.append(reinterpret_cast<const char *>(str), len);
+	}
+	return out;
+}
+
+static void ares_cb_a(void *blocked, ares_status_t status, size_t timeouts,
+    const ares_dns_record_t *rsp)
+{
+	if (status != ARES_SUCCESS || rsp == nullptr)
+		return;
+	for (size_t i = 0; i < ares_dns_record_rr_cnt(rsp, ARES_SECTION_ANSWER); ++i) {
+		auto rr = ares_dns_record_rr_get_const(rsp, ARES_SECTION_ANSWER, i);
+		auto rtype = ares_dns_rr_get_type(rr);
+		if (rtype == ARES_REC_TYPE_A)
+			*static_cast<bool *>(blocked) = true;
+	}
+}
+
+static void ares_cb_txt(void *reasonp, ares_status_t status, size_t timeouts,
+    const ares_dns_record_t *rsp)
+{
+	if (status != ARES_SUCCESS || rsp == nullptr)
+		return;
+	auto &reason = *static_cast<std::string *>(reasonp);
+	for (size_t i = 0; i < ares_dns_record_rr_cnt(rsp, ARES_SECTION_ANSWER); ++i) {
+		auto rr = ares_dns_record_rr_get_const(rsp, ARES_SECTION_ANSWER, i);
+		auto rtype = ares_dns_rr_get_type(rr);
+		if (rtype != ARES_REC_TYPE_TXT)
+			continue;
+		size_t keycnt = 0;
+		auto keys = ares_dns_rr_get_keys(rtype, &keycnt);
+		if (keycnt < 1)
+			continue;
+		auto dtype = ares_dns_rr_key_datatype(keys[0]);
+		if (dtype == ARES_DATATYPE_ABINP) {
+			if (!reason.empty())
+				reason += "; ";
+			reason += abinp_to_str(rr, keys[0]);
+		}
+	}
+}
+#endif
 
 /**
  * An empty string is returned when there is nothing to complain about.
@@ -51,7 +106,47 @@ static bool dnsbl_check(const char *src, std::string &reason) try
 	}
 	dotrep += g_zone_suffix;
 
-#if defined(HAVE_LDNS)
+#if defined(HAVE_CARES)
+	ares_channel_t *channel = nullptr;
+	auto cl_1 = make_scope_exit([&]() { ares_destroy(channel); });
+	ares_options opts{};
+	opts.evsys = ARES_EVSYS_DEFAULT;
+	auto status = ares_init_options(&channel, &opts, ARES_OPT_EVENT_THREAD);
+	if (status != ARES_SUCCESS)
+		return true;
+	ares_dns_record_t *req = nullptr;
+	auto cl_2 = make_scope_exit([&]() { ares_dns_record_destroy(req); });
+	status = ares_dns_record_create(&req, 0, ARES_FLAG_RD,
+	         ARES_OPCODE_QUERY, ARES_RCODE_NOERROR);
+	if (status != ARES_SUCCESS)
+		return true;
+	status = ares_dns_record_query_add(req, dotrep.c_str(),
+	         ARES_REC_TYPE_A, ARES_CLASS_IN);
+	if (status != ARES_SUCCESS)
+		return true;
+	bool blocked = false;
+	status = ares_send_dnsrec(channel, req, ares_cb_a, &blocked, nullptr);
+	if (status != ARES_SUCCESS)
+		return true;
+	ares_queue_wait_empty(channel, -1);
+	if (!blocked)
+		return true;
+	reason = "Host in blocklist (no reason text available)";
+	ares_dns_record_destroy(req);
+	req = nullptr;
+	status = ares_dns_record_create(&req, 0, ARES_FLAG_RD,
+	         ARES_OPCODE_QUERY, ARES_RCODE_NOERROR);
+	if (status != ARES_SUCCESS)
+		return false;
+	status = ares_dns_record_query_add(req, dotrep.c_str(),
+	         ARES_REC_TYPE_TXT, ARES_CLASS_IN);
+	if (status != ARES_SUCCESS)
+		return false;
+	status = ares_send_dnsrec(channel, req, ares_cb_txt, &reason, nullptr);
+	if (status == ARES_SUCCESS)
+		ares_queue_wait_empty(channel, -1);
+	return false;
+#elif defined(HAVE_LDNS)
 	auto dname = ldns_dname_new_frm_str(dotrep.c_str());
 	if (dname == nullptr) {
 		mlog(LV_ERR, "E-1251: ENOMEM");
@@ -193,8 +288,21 @@ static bool dnsbl_check(const char *src, std::string &reason) try
 
 BOOL SVC_dnsbl_filter(enum plugin_op reason, const struct dlfuncs &data)
 {
+	if (reason == PLUGIN_FREE) {
+#ifdef HAVE_CARES
+		ares_library_cleanup();
+#endif
+		return TRUE;
+	}
 	if (reason != PLUGIN_INIT)
 		return TRUE;
+#ifdef HAVE_CARES
+	auto status = ares_library_init(ARES_LIB_INIT_ALL);
+	if (status != ARES_SUCCESS) {
+		mlog(LV_ERR, "ares_library_init failed");
+		return false;
+	}
+#endif
 	LINK_SVC_API(data);
 	// TODO: dnsbl_client=<apikey>.authbl.dq.spamhaus.net=127.0.0.20
 	// dnsbl_client=zen.spamhaus.org
