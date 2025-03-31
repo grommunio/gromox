a5d64695c56c55415bc3454047393c0d1b91403e
Sergey Komarov <sergey.kom@computools.com>
2025-03-31 14:38:49 +0300

    lib: Fixed indefinite wait CID 1558692: (#1 of 1): Indefinite wait (BAD_CHECK_OF_WAIT_COND) dead_wait: A wait is performed without ensuring that the condition is not already satisfied while holding lock mtx. This can cause a deadlock if the notification happens before the lock is acquired.
    -Replaced incorrect usage of a local mutex in the notify listener wait
    loop with a shared mutex inside the ag.

--- lib/exmdb_client.cpp
+++ lib/exmdb_client.cpp
@@ -363,11 +363,8 @@ static int launch_notify_listener(remote_svr &srv) try
 	 * Wait for the notify thread to be up before allowing
 	 * current thread to send any commands.
 	 */
-	while (ag.startup_wait) {
-		std::mutex mtx;
-		std::unique_lock lk(mtx);
-		ag.startup_cv.wait(lk);
-	}
+	std::unique_lock lk(ag.startup_mutex);
+	ag.startup_cv.wait(lk, [&ag] { return !ag.startup_wait; });
 	return 0;
 } catch (const std::bad_alloc &) {
 	mlog(LV_ERR, "exmdb_client: failed to allocate memory for exmdb");
