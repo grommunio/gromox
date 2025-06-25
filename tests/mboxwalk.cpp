70df2df0a5b4f62af3464d274da5e5eb73a5ee8f
Jan Engelhardt <jengelh@inai.de>
2025-06-25 19:55:51 +0200

    tests: showcase codepage use in mboxwalk

--- tests/mboxwalk.cpp
+++ tests/mboxwalk.cpp
@@ -37,7 +37,51 @@ static int do_message(const TPROPVAL_ARRAY &row, unsigned int depth)
 	printf("msg %llu: \"%s\"\n",
 		static_cast<unsigned long long>(rop_util_get_gc_value(*msg_id)), znul(subject));
 
-	/* Obtain full message (bypass limits of content tables) */
+	/*
+	 * Table cells have a value length limit. If there is potential
+	 * truncation (strlen(x) >= 255), then:
+	 *
+	 * - A table value should only be used for _displaying_.
+	 *   It should not be stored or be used for data processing that
+	 *   requires the full value.
+	 *
+	 * - Obtain the complete value with another call, like so:
+	 */
+	static constexpr proptag_t qtags1[] = {PR_SUBJECT, PR_SUBJECT_A};
+	static constexpr PROPTAG_ARRAY qtags = {std::size(qtags1), deconst(qtags1)};
+	TPROPVAL_ARRAY values{};
+	if (!exmdb_client->get_message_properties(g_storedir, nullptr, CP_UTF8,
+	    *msg_id, &qtags, &values)) {
+		printf("get_msg_props RPC failed\n");
+		return -1;
+	}
+	/*
+	 * In Windows/MSMAPI, values of PT_UNICODE properties (such
+	 * as PR_SUBJECT/PR_SUBJECT_W) are encoded as wchar_t. In the
+	 * exmdb protocol, the encoding for PT_UNICODE is always
+	 * UTF-8. [So if your virtual terminal does not have the
+	 * right combination set, this printf call will output some
+	 * garbage on umlauts/etc.]
+	 */
+	subject = values.get<const char>(PR_SUBJECT);
+	printf("%-*sPR_SUBJECT: %s\n", depth * 4, "", subject ? subject : "(nullptr)");
+
+	/*
+	 * The values of PT_STRING8 properties (PR_SUBJECT_A) are
+	 * encoded whatever codepage is requested, e.g. `CP_UTF8` above.
+	 *
+	 * Because everything on Linux is UTF-8 these days and
+	 * PT_UNICODE serves the need just fine, not much thought was
+	 * given to the use of codepages, and you may see `CP_ACP`
+	 * instead in the source code too.
+	 */
+	subject = values.get<const char>(PR_SUBJECT_A);
+	printf("%-*sPR_SUBJECT_A: %s\n", depth * 4, "", subject ? subject : "(nullptr)");
+
+	/*
+	 * In the following, as an example, we obtain the full message instead
+	 * (complete with attachments etc.):
+	 */
 	MESSAGE_CONTENT *content = nullptr;
 	if (!exmdb_client->read_message(g_storedir, nullptr, CP_ACP,
 	    *msg_id, &content)) {
@@ -45,7 +89,10 @@ static int do_message(const TPROPVAL_ARRAY &row, unsigned int depth)
 		return -1;
 	}
 	auto cleanup_0 = HX::make_scope_exit([&]() { message_content_free(content); });
-	// content->proplist.get<const char>(PR_SUBJECT); // full subject
+	subject = content->proplist.get<const char>(PR_SUBJECT);
+	printf("%-*sPR_SUBJECT (full msg): %s\n", depth * 4, "", subject ? subject : "(nullptr)");
+	subject = content->proplist.get<const char>(PR_SUBJECT_A);
+	printf("%-*sPR_SUBJECT_A (full msg): %s\n", depth * 4, "", subject ? subject : "(nullptr)");
 
 	return 0;
 }
