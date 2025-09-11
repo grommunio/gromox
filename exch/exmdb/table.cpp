c40fe131675eb075d95902bcefd3b70a277e6a28
Jan Engelhardt <jengelh@inai.de>
2025-09-12 00:30:48 +0200

    exmdb: delete unnecessary memdb transaction restart from table_load_content_table

--- exch/exmdb/table.cpp
+++ exch/exmdb/table.cpp
@@ -938,11 +938,6 @@ static BOOL table_load_content_table(db_conn_ptr &pdb, db_base_wr_ptr &dbase,
 	pstmt.finalize();
 	pstmt1.finalize();
 	if (NULL != psorts) {
-		if (psort_transact.commit() != SQLITE_OK)
-			return false;
-		psort_transact = gx_sql_begin(psqlite, txn_mode::write);
-		if (!psort_transact)
-			return false;
 		snprintf(sql_string, std::size(sql_string), "INSERT INTO t%u "
 			    "(inst_id, row_type, row_stat, parent_id, depth, "
 			    "count, inst_num, value, extremum, prev_id) VALUES"
