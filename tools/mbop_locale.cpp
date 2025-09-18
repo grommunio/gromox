367ec1206520610642c95e081e41e4b97fc327ac
Jan Engelhardt <jengelh@inai.de>
2025-09-18 11:40:08 +0200

    mbop/locale: add -T option to test set_locale performance

--- tools/mbop_locale.cpp
+++ tools/mbop_locale.cpp
@@ -16,24 +16,47 @@ using namespace gromox;
 namespace set_locale {
 
 static constexpr HXoption g_options_table[] = {
+	{{}, 'T', HXTYPE_NONE, {}, {}, {}, 0, "Run EXRPC performance test"},
 	{{}, 'l', HXTYPE_STRING, {}, {}, {}, 0, "XPG/POSIX-style locale code (e.g. ja_JP)", "CODE"},
 	{nullptr, 'v', HXTYPE_NONE, &global::g_verbose_mode, {}, {}, 0, "Verbose mode"},
 	MBOP_AUTOHELP,
 	HXOPT_TABLEEND,
 };
 
-static int set_names(const char *lang);
+static int set_names(const char *lang, size_t &);
+
+static void do_perftest(const char *lang)
+{
+	size_t fcount = 0;
+	auto t_start = tp_now();
+	while (true) {
+		if (set_names(lang, fcount) != 0)
+			/* ignore */;
+		auto now = tp_now();
+		auto delta = now - t_start;
+		if (delta >= std::chrono::seconds(1)) {
+			auto d = std::chrono::duration_cast<std::chrono::microseconds>(delta) / fcount;
+			fprintf(stderr, "\r\e[2K%llu µs\e[K", static_cast<unsigned long long>(d.count()));
+			t_start = now;
+			fcount = 0;
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
 	const char *g_language = nullptr;
+	bool run_perftest = false;
 	HXopt6_auto_result result;
 	if (HX_getopt6(g_options_table, argc, argv, &result, HXOPT_USAGEONERR |
 	    HXOPT_ITER_OPTS) != HXOPT_ERR_SUCCESS || g_exit_after_optparse)
 		return EXIT_PARAM;
-	for (int i = 0; i < result.nopts; ++i)
+	for (int i = 0; i < result.nopts; ++i) {
 		if (result.desc[i]->sh == 'l')
 			g_language = result.oarg[i];
+		else if (result.desc[i]->sh == 'T')
+			run_perftest = true;
+	}
 	if (g_language == nullptr) {
 		mbop_fprintf(stderr, "You need to specify the -l option\n");
 		return EXIT_PARAM;
@@ -49,10 +72,16 @@ int main(int argc, char **argv)
 		fprintf(stderr, "No folder name translations for locale \"%s\" available.\n", g_language);
 		return EXIT_SUCCESS;
 	}
-	return set_names(lang) == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+
+	if (run_perftest) {
+		do_perftest(lang);
+		return EXIT_SUCCESS;
+	}
+	size_t ignored = 0;
+	return set_names(lang, ignored) == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
-static int set_names(const char *lang)
+static int set_names(const char *lang, size_t &fcount)
 {
 	unsigned int start_gcv = 1;
 	unsigned int end_gcv   = g_public_folder ? PUBLIC_FID_UNASSIGNED_START : PRIVATE_FID_UNASSIGNED_START;
@@ -81,6 +110,7 @@ static int set_names(const char *lang)
 			mbop_fprintf(stderr, "set_folder_props failed\n");
 			return EXIT_FAILURE;
 		}
+		++fcount;
 	}
 	return EXIT_SUCCESS;
 }
