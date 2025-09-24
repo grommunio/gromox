572a1a4675e126011d0e441097e97b010da5b1ae
BigBearHub-commits <bigbearhub29@gmail.com>
2025-09-24 23:36:44 +0700

    Update dldcheck.cpp

--- tools/dldcheck.cpp
+++ tools/dldcheck.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2023 grommunio GmbH
-// This file is part of Gromox.
+// This file is part of Gromox. 
 #include <cstdio>
 #include <cstdlib>
 #include <dlfcn.h>
