e31a60057c76c5a79f472d81ad2849280c35fe92
BigBearHub-commits <bigbearhub29@gmail.com>
2025-09-24 23:42:49 +0700

    Update mkshared.cpp

--- tools/mkshared.cpp
+++ tools/mkshared.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2021–2024 grommunio GmbH
-// This file is part of Gromox.
+// This file is part of Gromox. 
 #ifdef HAVE_CONFIG_H
 #	include "config.h"
 #endif
