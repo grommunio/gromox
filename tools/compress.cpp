510a8b278b84ac2325710ec755fc822408423246
BigBearHub-commits <bigbearhub29@gmail.com>
2025-09-24 23:33:24 +0700

    Update compress.cpp

--- tools/compress.cpp
+++ tools/compress.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2024 grommunio GmbH
-// This file is part of Gromox.
+// This file is part of Gromox. 
 #include <cerrno>
 #include <cstdio>
 #include <cstdlib>
