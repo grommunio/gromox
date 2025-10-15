4b0ba0a7c31cfdcf1acd7adb3c685840d1b3c9b4
Jan Engelhardt <jengelh@inai.de>
2025-10-15 23:54:45 +0200

    timer: delete support for reading old timer_acl.txt

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -330,14 +330,6 @@ int main(int argc, char **argv)
 	auto hosts_allow = pconfig->get_value("timer_hosts_allow");
 	if (hosts_allow != nullptr)
 		g_acl_list = gx_split(hosts_allow, ' ');
-	auto err = list_file_read_fixedstrings("timer_acl.txt",
-	           pconfig->get_value("config_file_path"), g_acl_list);
-	if (err == ENOENT) {
-	} else if (err != 0) {
-		printf("[system]: list_file_initd timer_acl.txt: %s\n", strerror(err));
-		g_notify_stop = true;
-		return EXIT_FAILURE;
-	}
 	std::sort(g_acl_list.begin(), g_acl_list.end());
 	g_acl_list.erase(std::remove(g_acl_list.begin(), g_acl_list.end(), ""), g_acl_list.end());
 	g_acl_list.erase(std::unique(g_acl_list.begin(), g_acl_list.end()), g_acl_list.end());
