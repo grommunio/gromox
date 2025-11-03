390b25f13185302b06462ff9c8833cd1fcf82747
Jan Engelhardt <jengelh@inai.de>
2025-11-03 10:42:13 +0100

    mapi_lib: proper error code for g_str() running over misformatted input buffer

--- lib/mapi/ext_buffer.cpp
+++ lib/mapi/ext_buffer.cpp
@@ -177,7 +177,7 @@ pack_result EXT_PULL::g_str(char **ppstr)
 		return pack_result::bufsize;
 	auto len = strnlen(&m_cdata[m_offset], m_data_size - m_offset);
 	if (len + 1 > m_data_size - m_offset)
-		return pack_result::bufsize;
+		return pack_result::format;
 	len ++;
 	*ppstr = anew<char>(len);
 	if (*ppstr == nullptr)
@@ -192,7 +192,7 @@ pack_result EXT_PULL::g_str(std::string *out) try
 		return pack_result::bufsize;
 	auto len = strnlen(&m_cdata[m_offset], m_data_size - m_offset);
 	if (len + 1 > m_data_size - m_offset)
-		return pack_result::bufsize;
+		return pack_result::format;
 	out->resize(len);
 	/* Copy with, and advance over, NUL */
 	memcpy(out->data(), &m_udata[m_offset], len + 1);
