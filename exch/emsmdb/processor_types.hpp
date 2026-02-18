2d71b6d2ef168d142c1417dbcf9e6a8c702daa97
Jan Engelhardt <jengelh@inai.de>
2026-02-18 17:51:33 +0100

    emsmdb: add member initializers to EMSMDB/ROP structs

--- exch/emsmdb/processor_types.hpp
+++ exch/emsmdb/processor_types.hpp
@@ -4,6 +4,11 @@
 #include <vector>
 #include <gromox/mapi_types.hpp>
 
+/*
+ * Because there is no EMSMDB client in Gromox (just the server part),
+ * there is no view_t mechanism like there is in zcore_rpc.hpp.
+ */
+
 struct rop_request {
 	rop_request() = default; /* Prevent use of direct-list-init */
 	virtual ~rop_request() = default;
@@ -26,73 +31,70 @@ struct rop_response {
 using ROP_RESPONSE = rop_response;
 
 struct PARTIAL_COMPLETION_RESPONSE final : public rop_response {
-	uint8_t partial_completion;
+	uint8_t partial_completion = false;
 };
 
 struct TABLE_STATUS_RESPONSE final : public rop_response {
-	uint8_t table_status;
+	uint8_t table_status = 0;
 };
 
 struct PROBLEM_RESPONSE final : public rop_response {
-	PROBLEM_ARRAY problems;
+	PROBLEM_ARRAY problems{};
 };
 
 struct LOGON_REQUEST final : public rop_request {
-	uint8_t logon_flags;
-	uint32_t open_flags;
-	uint32_t store_stat;
-	char *pessdn;
+	uint8_t logon_flags = 0;
+	uint32_t open_flags = 0, store_stat = 0;
+	char *pessdn = nullptr;
 };
 
 struct LOGON_PMB_RESPONSE final : public rop_response {
-	uint8_t logon_flags;
-	uint64_t folder_ids[13];
-	uint8_t response_flags;
-	GUID mailbox_guid;
-	uint16_t replid;
-	GUID replguid;
-	LOGON_TIME logon_time;
-	uint64_t gwart_time;
-	uint32_t store_stat;
+	uint8_t logon_flags = 0, response_flags = 0;
+	uint64_t folder_ids[13]{};
+	GUID mailbox_guid{};
+	uint16_t replid = 0;
+	GUID replguid{};
+	LOGON_TIME logon_time{};
+	uint64_t gwart_time = 0;
+	uint32_t store_stat = 0;
 };
 
 struct LOGON_PF_RESPONSE final : public rop_response {
-	uint8_t logon_flags;
-	uint64_t folder_ids[13];
-	uint16_t replid;
-	GUID replguid;
-	GUID per_user_guid;
+	uint8_t logon_flags = 0;
+	uint64_t folder_ids[13]{};
+	uint16_t replid = 0;
+	GUID replguid{}, per_user_guid{};
 };
 
 struct LOGON_REDIRECT_RESPONSE final : public rop_response {
-	uint8_t logon_flags;
-	char pserver_name[1024];
+	uint8_t logon_flags = 0;
+	char pserver_name[1024]{};
 };
 
 struct GETRECEIVEFOLDER_REQUEST final : public rop_request {
-	char *pstr_class;
+	char *pstr_class = nullptr;
 };
 
 struct GETRECEIVEFOLDER_RESPONSE final : public rop_response {
-	uint64_t folder_id;
+	uint64_t folder_id = 0;
 	std::string pstr_class;
 };
 
 struct SETRECEIVEFOLDER_REQUEST final : public rop_request {
-	uint64_t folder_id;
-	char * pstr_class;
+	uint64_t folder_id = 0;
+	char *pstr_class = nullptr;
 };
 
 struct GETRECEIVEFOLDERTABLE_RESPONSE final : public rop_response {
-	PROPROW_SET rows;
+	PROPROW_SET rows{};
 };
 
 struct GETSTORESTAT_RESPONSE final : public rop_response {
-	uint32_t stat;
+	uint32_t stat = 0;
 };
 
 struct GETOWNINGSERVERS_REQUEST final : public rop_request {
-	uint64_t folder_id;
+	uint64_t folder_id = 0;
 };
 
 struct GETOWNINGSERVERS_RESPONSE final : public rop_response {
@@ -100,15 +102,15 @@ struct GETOWNINGSERVERS_RESPONSE final : public rop_response {
 };
 
 struct PUBLICFOLDERISGHOSTED_REQUEST final : public rop_request {
-	uint64_t folder_id;
+	uint64_t folder_id = 0;
 };
 
 struct PUBLICFOLDERISGHOSTED_RESPONSE final : public rop_response {
-	GHOST_SERVER *pghost;
+	GHOST_SERVER *pghost = nullptr;
 };
 
 struct LONGTERMIDFROMID_REQUEST final : public rop_request {
-	uint64_t id;
+	uint64_t id = 0;
 };
 
 struct LONGTERMIDFROMID_RESPONSE final : public rop_response {
@@ -120,11 +122,11 @@ struct IDFROMLONGTERMID_REQUEST final : public rop_request {
 };
 
 struct IDFROMLONGTERMID_RESPONSE final : public rop_response {
-	uint64_t id;
+	uint64_t id = 0;
 };
 
 struct GETPERUSERLONGTERMIDS_REQUEST final : public rop_request {
-	GUID guid;
+	GUID guid{};
 };
 
 struct GETPERUSERLONGTERMIDS_RESPONSE final : public rop_response {
@@ -136,318 +138,287 @@ struct GETPERUSERGUID_REQUEST final : public rop_request {
 };
 
 struct GETPERUSERGUID_RESPONSE final : public rop_response {
-	GUID guid;
+	GUID guid{};
 };
 
 struct READPERUSERINFORMATION_REQUEST final : public rop_request {
 	LONG_TERM_ID long_folder_id;
-	uint8_t reserved;
-	uint32_t data_offset;
-	uint16_t max_data_size;
+	uint32_t data_offset = 0;
+	uint16_t max_data_size = 0;
+	uint8_t reserved = 0;
 };
 
 struct READPERUSERINFORMATION_RESPONSE final : public rop_response {
-	uint8_t has_finished;
-	BINARY data;
+	uint8_t has_finished = false;
+	BINARY data{};
 };
 
 struct WRITEPERUSERINFORMATION_REQUEST final : public rop_request {
 	LONG_TERM_ID long_folder_id;
-	uint8_t has_finished;
-	uint32_t offset;
-	BINARY data;
-	GUID *pguid;
+	uint8_t has_finished = 0;
+	uint32_t offset = 0;
+	BINARY data{};
+	GUID *pguid = nullptr;
 };
 
 struct OPENFOLDER_REQUEST final : public rop_request {
-	uint8_t ohindex;
-	uint64_t folder_id;
-	uint8_t open_flags;
+	uint8_t ohindex = 0, open_flags = 0;
+	uint64_t folder_id = 0;
 };
 
 struct OPENFOLDER_RESPONSE final : public rop_response {
-	uint8_t has_rules;
-	GHOST_SERVER *pghost;
+	uint8_t has_rules = false;
+	GHOST_SERVER *pghost = nullptr;
 };
 
 struct CREATEFOLDER_REQUEST final : public rop_request {
-	uint8_t ohindex;
-	uint8_t folder_type;
-	uint8_t use_unicode;
-	uint8_t open_existing;
-	uint8_t reserved;
-	char *pfolder_name;
-	char *pfolder_comment;
+	uint8_t ohindex = 0, folder_type = 0, use_unicode = false;
+	uint8_t open_existing = false, reserved =0;
+	char *pfolder_name = nullptr, *pfolder_comment = nullptr;
 };
 
 struct CREATEFOLDER_RESPONSE final : public rop_response {
-	uint64_t folder_id;
-	uint8_t is_existing;
-	uint8_t has_rules;
-	GHOST_SERVER *pghost;
+	uint64_t folder_id = 0;
+	uint8_t is_existing = false, has_rules = false;
+	GHOST_SERVER *pghost = nullptr;
 };
 
 struct DELETEFOLDER_REQUEST final : public rop_request {
-	uint8_t flags;
-	uint64_t folder_id;
+	uint8_t flags = 0;
+	uint64_t folder_id = 0;
 };
 
 struct SETSEARCHCRITERIA_REQUEST final : public rop_request {
-	RESTRICTION *pres;
-	EID_ARRAY folder_ids;
-	uint32_t search_flags;
+	RESTRICTION *pres = nullptr;
+	EID_ARRAY folder_ids{};
+	uint32_t search_flags = 0;
 };
 
 struct GETSEARCHCRITERIA_REQUEST final : public rop_request {
-	uint8_t use_unicode;
-	uint8_t include_restriction;
-	uint8_t include_folders;
+	uint8_t use_unicode = false, include_restriction = false;
+	uint8_t include_folders = false;
 };
 
 struct GETSEARCHCRITERIA_RESPONSE final : public rop_response {
-	RESTRICTION *pres;
-	uint8_t logon_id;
-	EID_ARRAY folder_ids;
-	uint32_t search_status;
+	uint8_t logon_id = 0;
+	uint32_t search_status = 0;
+	RESTRICTION *pres = nullptr;
+	EID_ARRAY folder_ids{};
 };
 
 struct MOVECOPYMESSAGES_REQUEST final : public rop_request {
-	uint8_t dhindex;
-	EID_ARRAY message_ids;
-	uint8_t want_asynchronous;
-	uint8_t want_copy;
+	uint8_t dhindex = 0, want_asynchronous = 0, want_copy = 0;
+	EID_ARRAY message_ids{};
 };
 
 /**
  * Alternate response for ropMoveCopyMessages, ropMoveFolder, ropCopyFolder.
  */
 struct NULL_DST_RESPONSE final : public rop_response {
-	uint32_t dhindex;
-	uint8_t partial_completion;
+	uint32_t dhindex = 0;
+	uint8_t partial_completion = false;
 };
 
 /**
  * Alternate response for ropCopyProperties, ropCopyTo.
  */
 struct NULL_DST1_RESPONSE final : public rop_response {
-	uint32_t dhindex;
+	uint32_t dhindex = 0;
 };
 
 struct MOVEFOLDER_REQUEST final : public rop_request {
-	uint8_t dhindex;
-	uint8_t want_asynchronous;
-	uint8_t use_unicode;
-	uint64_t folder_id;
-	char *pnew_name;
+	uint8_t dhindex = 0, want_asynchronous = false, use_unicode = false;
+	uint64_t folder_id = 0;
+	char *pnew_name = nullptr;
 };
 
 struct COPYFOLDER_REQUEST final : public rop_request {
-	uint8_t dhindex;
-	uint8_t want_asynchronous;
-	uint8_t want_recursive;
-	uint8_t use_unicode;
-	uint64_t folder_id;
-	char *pnew_name;
+	uint8_t dhindex = 0, want_asynchronous = false, want_recursive = false;
+	uint8_t use_unicode = false;
+	uint64_t folder_id = 0;
+	char *pnew_name = nullptr;
 };
 
 struct EMPTYFOLDER_REQUEST final : public rop_request {
-	uint8_t want_asynchronous;
-	uint8_t want_delete_associated;
+	uint8_t want_asynchronous = false, want_delete_associated = false;
 };
 
 struct HARDDELETEMESSAGESANDSUBFOLDERS_REQUEST final : public rop_request {
-	uint8_t want_asynchronous;
-	uint8_t want_delete_associated;
+	uint8_t want_asynchronous = false, want_delete_associated = false;
 };
 
 struct DELETEMESSAGES_REQUEST final : public rop_request {
-	uint8_t want_asynchronous;
-	uint8_t notify_non_read;
-	EID_ARRAY message_ids;
+	uint8_t want_asynchronous = false, notify_non_read = false;
+	EID_ARRAY message_ids{};
 };
 
 struct HARDDELETEMESSAGES_REQUEST final : public rop_request {
-	uint8_t want_asynchronous;
-	uint8_t notify_non_read;
-	EID_ARRAY message_ids;
+	uint8_t want_asynchronous = false, notify_non_read = false;
+	EID_ARRAY message_ids{};
 };
 
 struct GETHIERARCHYTABLE_REQUEST final : public rop_request {
-	uint8_t ohindex;
-	uint8_t table_flags;
+	uint8_t ohindex = 0, table_flags = 0;
 };
 
 struct GETHIERARCHYTABLE_RESPONSE final : public rop_response {
-	uint32_t row_count;
+	uint32_t row_count = 0;
 };
 
 struct GETCONTENTSTABLE_REQUEST final : public rop_request {
-	uint8_t ohindex;
-	uint8_t table_flags;
+	uint8_t ohindex = 0, table_flags = 0;
 };
 
 struct GETCONTENTSTABLE_RESPONSE final : public rop_response {
-	uint32_t row_count;
+	uint32_t row_count = 0;
 };
 
 struct SETCOLUMNS_REQUEST final : public rop_request {
-	uint8_t table_flags;
+	uint8_t table_flags = 0;
 	proptag_vector proptags;
 };
 
 struct SORTTABLE_REQUEST final : public rop_request {
-	uint8_t table_flags;
-	SORTORDER_SET sort_criteria;
+	uint8_t table_flags = 0;
+	SORTORDER_SET sort_criteria{};
 };
 
 struct RESTRICT_REQUEST final : public rop_request {
-	uint8_t res_flags;
-	RESTRICTION *pres;
+	uint8_t res_flags = 0;
+	RESTRICTION *pres = nullptr;
 };
 
 struct QUERYROWS_REQUEST final : public rop_request {
-	uint8_t flags;
-	uint8_t forward_read;
-	uint16_t row_count;
+	uint8_t flags = 0, forward_read = 0;
+	uint16_t row_count = 0;
 };
 
 struct QUERYROWS_RESPONSE final : public rop_response {
-	uint8_t seek_pos;
-	uint16_t count;
-	BINARY bin_rows;
+	uint8_t seek_pos = 0;
+	uint16_t count = 0;
+	BINARY bin_rows{};
 };
 
 struct QUERYPOSITION_RESPONSE final : public rop_response {
-	uint32_t numerator;
-	uint32_t denominator;
+	uint32_t numerator = 0, denominator = 0;
 };
 
 struct SEEKROW_REQUEST final : public rop_request {
-	uint8_t seek_pos;
-	int32_t offset;
-	uint8_t want_moved_count;
+	uint8_t seek_pos = 0, want_moved_count = false;
+	int32_t offset = 0;
 };
 
 struct SEEKROW_RESPONSE final : public rop_response {
-	uint8_t has_soughtless;
-	int32_t offset_sought;
+	uint8_t has_soughtless = false;
+	int32_t offset_sought = 0;
 };
 
 struct SEEKROWBOOKMARK_REQUEST final : public rop_request {
-	BINARY bookmark;
-	int32_t offset;
-	uint8_t want_moved_count;
+	BINARY bookmark{};
+	int32_t offset = 0;
+	uint8_t want_moved_count = false;
 };
 
 struct SEEKROWBOOKMARK_RESPONSE final : public rop_response {
-	uint8_t row_invisible;
-	uint8_t has_soughtless;
-	uint32_t offset_sought;
+	uint8_t row_invisible = false, has_soughtless = false;
+	uint32_t offset_sought = 0;
 };
 
 struct SEEKROWFRACTIONAL_REQUEST final : public rop_request {
-	uint32_t numerator;
-	uint32_t denominator;
+	uint32_t numerator = 0, denominator = 0;
 };
 
 struct CREATEBOOKMARK_RESPONSE final : public rop_response {
-	BINARY bookmark;
+	BINARY bookmark{};
 };
 
 struct QUERYCOLUMNSALL_RESPONSE final : public rop_response {
-	PROPTAG_ARRAY proptags;
+	PROPTAG_ARRAY proptags{};
 };
 
 struct FINDROW_REQUEST final : public rop_request {
-	uint8_t flags;
-	RESTRICTION *pres;
-	uint8_t seek_pos;
-	BINARY bookmark;
+	uint8_t flags = 0, seek_pos = 0;
+	RESTRICTION *pres = nullptr;
+	BINARY bookmark{};
 };
 
 struct FINDROW_RESPONSE final : public rop_response {
-	uint8_t bookmark_invisible;
-	PROPERTY_ROW *prow;
+	uint8_t bookmark_invisible = false;
+	PROPERTY_ROW *prow = nullptr;
 	proptag_vector pcolumns;
 };
 
 struct FREEBOOKMARK_REQUEST final : public rop_request {
-	BINARY bookmark;
+	BINARY bookmark{};
 };
 
 struct EXPANDROW_REQUEST final : public rop_request {
-	uint16_t max_count;
+	uint16_t max_count = 0;
 	uint64_t category_id;
 };
 
 struct EXPANDROW_RESPONSE final : public rop_response {
-	uint32_t expanded_count;
-	uint16_t count;
-	BINARY bin_rows;
+	uint32_t expanded_count = 0;
+	uint16_t count = 0;
+	BINARY bin_rows{};
 };
 
 struct COLLAPSEROW_REQUEST final : public rop_request {
-	uint64_t category_id;
+	uint64_t category_id = 0;
 };
 
 struct COLLAPSEROW_RESPONSE final : public rop_response {
-	uint32_t collapsed_count;
+	uint32_t collapsed_count = 0;
 };
 
 struct GETCOLLAPSESTATE_REQUEST final : public rop_request {
-	uint64_t row_id;
-	uint32_t row_instance;
+	uint64_t row_id = 0;
+	uint32_t row_instance = 0;
 };
 
 struct GETCOLLAPSESTATE_RESPONSE final : public rop_response {
-	BINARY collapse_state;
+	BINARY collapse_state{};
 };
 
 struct SETCOLLAPSESTATE_REQUEST final : public rop_request {
-	BINARY collapse_state;
+	BINARY collapse_state{};
 };
 
 struct SETCOLLAPSESTATE_RESPONSE final : public rop_response {
-	BINARY bookmark;
+	BINARY bookmark{};
 };
 
 struct OPENMESSAGE_REQUEST final : public rop_request {
-	uint8_t ohindex;
-	uint16_t cpid;
-	uint64_t folder_id;
-	uint8_t open_mode_flags;
-	uint64_t message_id;
+	uint8_t ohindex = 0, open_mode_flags = 0;
+	uint16_t cpid = 0;
+	uint64_t folder_id = 0, message_id = 0;
 };
 
 struct OPENMESSAGE_RESPONSE final : public rop_response {
-	uint8_t has_named_properties;
-	TYPED_STRING subject_prefix;
-	TYPED_STRING normalized_subject;
-	uint16_t recipient_count;
-	PROPTAG_ARRAY recipient_columns;
-	uint8_t row_count;
-	OPENRECIPIENT_ROW *precipient_row;
+	uint8_t has_named_properties = false, row_count = 0;
+	uint16_t recipient_count = 0;
+	TYPED_STRING subject_prefix, normalized_subject;
+	PROPTAG_ARRAY recipient_columns{};
+	OPENRECIPIENT_ROW *precipient_row = nullptr;
 };
 
 struct CREATEMESSAGE_REQUEST final : public rop_request {
-	uint8_t ohindex;
-	uint16_t cpid;
-	uint64_t folder_id;
-	uint8_t associated_flag;
+	uint8_t ohindex = 0, associated_flag = 0;
+	uint16_t cpid = 0;
+	uint64_t folder_id = 0;
 };
 
 struct CREATEMESSAGE_RESPONSE final : public rop_response {
-	uint64_t *pmessage_id;
+	uint64_t *pmessage_id = nullptr;
 };
 
 struct SAVECHANGESMESSAGE_REQUEST final : public rop_request {
-	uint8_t ihindex2;
-	uint8_t save_flags;
+	uint8_t ihindex2 = 0, save_flags = 0;
 };
 
 struct SAVECHANGESMESSAGE_RESPONSE final : public rop_response {
-	uint8_t ihindex2;
-	uint64_t message_id;
+	uint8_t ihindex2 = 0;
+	uint64_t message_id = 0;
 };
 
 struct REMOVEALLRECIPIENTS_REQUEST final : public rop_request {
