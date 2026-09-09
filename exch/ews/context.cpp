7d2ecd1b51b630a7c95b7363ae57f2b1f2f7c1e8
Ievgen Popovych <ievgen.popovych@grommunio.com>
2026-09-09 15:56:27 +0200

    exch: EWS protocol covered by audit logs
    Reference: GLX-713

--- exch/ews/context.cpp
+++ exch/ews/context.cpp
@@ -3522,6 +3522,45 @@ void EWSContext::send(const std::string &dir, uint64_t log_msg_id,
 	           m_auth_info.username, rcpts);
 	if (err != ecSuccess)
 		throw DispatchError(E3117(err));
+
+	auto get_smtp_address = [&](proptag_t smtp_tag, proptag_t addrtype_tag,
+                            proptag_t email_tag) -> const char * {
+		auto addr = content.proplist.get<const char>(smtp_tag);
+		if (addr != nullptr)
+			return addr;
+
+		auto type = content.proplist.get<const char>(addrtype_tag);
+		if (type == nullptr || strcasecmp(type, "SMTP") == 0)
+			return content.proplist.get<const char>(email_tag);
+
+		return nullptr;
+	};
+
+	auto actor = get_auth_info(m_ctx_id).username;
+	auto subject = content.proplist.get<const char>(PR_SUBJECT);
+
+	auto representing = get_smtp_address(
+		PR_SENT_REPRESENTING_SMTP_ADDRESS,
+		PR_SENT_REPRESENTING_ADDRTYPE,
+		PR_SENT_REPRESENTING_EMAIL_ADDRESS);
+
+	auto sender = get_smtp_address(
+		PR_SENDER_SMTP_ADDRESS,
+		PR_SENDER_ADDRTYPE,
+		PR_SENDER_EMAIL_ADDRESS);
+
+	std::string representation;
+	if (actor != nullptr && representing != nullptr &&
+		strcasecmp(actor, representing) != 0) {
+		if (sender != nullptr && strcasecmp(actor, sender) == 0)
+			representation = " on behalf of "s + representing;
+		else
+			representation = " as "s + representing;
+	}
+
+	mlog(LV_NOTICE,
+		"gromox-audit: %s sent message \"%s\"%s via EWS",
+		znul(actor), znul(subject), representation.c_str());
 }
 
 /**
